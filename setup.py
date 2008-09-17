215f905b84ce44fb4b921bb2c86c4937e2966fc5
Jan Schneider <j.schneider@uib.de>
2008-09-17 15:59:46 +0000

    -

--- setup.py
+++ setup.py
@@ -4,15 +4,25 @@
 from distutils.core import setup, Extension
 
 setup (
-    name = 'python-opsi',
-    version = '3.3.0.25',
-    description = 'opsi python library',
-    long_description = 'opsi python library.',
-    author = "uib GmbH",
-    author_email = 'info@uib.de',
-    license = 'GPL',
-    url='http://www.opsi.org',
-    py_modules = [
-	'OPSI'
-    ],
+	name = 'python-opsi',
+	version = '3.3.0.25',
+	description = 'opsi python library',
+	long_description = 'opsi python library.',
+	author = "uib GmbH",
+	author_email = 'info@uib.de',
+	license = 'GPL',
+	url='http://www.opsi.org',
+	package_dir = {'': 'src'},
+	packages = [
+		'OPSI',
+		'OPSI.Backend',
+		'OPSI.System',
+		'dav',
+		'dav.element',
+		'dav.method',
+	],
+	#data_files=[
+	#	('/usr/share/locale/de/LC_MESSAGES opsi_system.mo', ['gettext/opsi_system.mo', 'gettext/opsi_ui.mo'])
+	#]
 )
+
