efeb6a035584593778480ec0ed2d4046483b5af4
Jan Schneider <j.schneider@uib.de>
2009-04-01 08:05:07 +0000

    -

--- src/OPSI/Backend/MySQL.py
+++ src/OPSI/Backend/MySQL.py
@@ -1351,18 +1351,6 @@ class MySQLBackend(DataBackend):
 			data['licensePoolId'] = licensePoolId
 			self.__mysql__.db_insert('SOFTWARE_LICENSE_TO_LICENSE_POOL', data)
 	
-	def addWindowsSoftwareIdsToLicensePool(self, windowsSoftwareIds, licensePoolId):
-		if not self._licenseManagementEnabled: raise BackendModuleDisabledError("License management module currently disabled")
-		if not re.search(LICENSE_POOL_ID_REGEX, licensePoolId):
-			raise BackendBadValueError("Bad license pool id '%s'" % licensePoolId)
-		if not licensePoolId in self.getLicensePoolIds_list():
-			raise BackendMissingDataError("License pool '%s' does not exist" % licensePoolId)
-		if not type(windowsSoftwareIds) is list:
-			windowsSoftwareIds = [ windowsSoftwareIds ]
-		for windowsSoftwareId in windowsSoftwareIds:
-			if not self.__mysql__.db_getRow('SELECT * FROM `WINDOWS_SOFTWARE_ID_TO_LICENSE_POOL` WHERE `windowsSoftwareId`="%s" AND `licensePoolId`="%s"' % (windowsSoftwareId, licensePoolId)):
-				self.__mysql__.db_insert('WINDOWS_SOFTWARE_ID_TO_LICENSE_POOL', { 'licensePoolId': licensePoolId, 'windowsSoftwareId': windowsSoftwareId })
-				
 	def setWindowsSoftwareIdsToLicensePool(self, windowsSoftwareIds, licensePoolId):
 		if not self._licenseManagementEnabled: raise BackendModuleDisabledError("License management module currently disabled")
 		if not re.search(LICENSE_POOL_ID_REGEX, licensePoolId):
