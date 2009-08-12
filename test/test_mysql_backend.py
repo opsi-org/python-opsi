2a06be11684f10fc60817bd52066d77323ff4a62
Jan Schneider <j.schneider@uib.de>
2009-08-12 13:22:16 +0000

    created mysql backend test

---
+++ test/test_mysql_backend.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+
+from OPSI.Logger import *
+from OPSI.Backend.MySQL import MySQLBackend
+from backend import *
+
+logger = Logger()
+logger.setConsoleLevel(LOG_DEBUG2)
+logger.setConsoleColor(True)
+
+
+
+mysqlBackend = MySQLBackend(username = 'opsi', password = 'opsi', database='opsi')
+bt = BackendTest(mysqlBackend)
+bt.cleanupBackend()
+bt.testObjectMethods()
+#bt.testNonObjectMethods()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
