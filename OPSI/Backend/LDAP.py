096dc6a578ec980e47d6b2be02e2a23fb1532616
Erol Ülükmen <e.ueluekmen@uib.de>
2009-08-13 11:38:10 +0000

    -

--- OPSI/Backend/LDAP.py
+++ OPSI/Backend/LDAP.py
@@ -49,7 +49,7 @@ logger = Logger()
 # ======================================================================================================
 class LDAPBackend(DataBackend):
 	
-	def __init__(self, username = '', password = '', address = 'localhost', **kwargs):
+	def __init__(self, username = 'opsi', password = 'opsi', address = 'localhost', **kwargs):
 		DataBackend.__init__(self, username, password, address, **kwargs)
 		
 		## Parse arguments
@@ -57,7 +57,40 @@ class LDAPBackend(DataBackend):
 		#	if   (option.lower() == 'database'):
 		#		self._database = value
 		#
-	
+		
+		# Default values
+		self._baseDn = 'dc=uib,dc=local'
+		self._opsiBaseDn = 'cn=opsi,' + self._baseDn
+		self._hostsContainerDn = 'cn=hosts,' + self._opsiBaseDn
+		self._groupsContainerDn = 'cn=groups,' + self._opsiBaseDn
+		self._productsContainerDn = 'cn=products,' + self._opsiBaseDn
+		self._productClassesContainerDn = 'cn=productClasses,' + self._opsiBaseDn
+		self._productStatesContainerDn = 'cn=productStates,' + self._opsiBaseDn
+		self._generalConfigsContainerDn = 'cn=generalConfigs,' + self._opsiBaseDn
+		self._networkConfigsContainerDn = 'cn=networkConfigs,' + self._opsiBaseDn
+		self._productPropertiesContainerDn = 'cn=productProperties,' + self._opsiBaseDn
+		self._hostAttributeDescription = 'opsiDescription'
+		self._hostAttributeNotes = 'opsiNotes'
+		self._hostAttributeHardwareAddress = 'opsiHardwareAddress'
+		self._hostAttributeIpAddress = 'opsiIpAddress'
+		self._createClientCommand = ''
+		self._deleteClient = True
+		self._deleteClientCommand = ''
+		self._createServerCommand = ''
+		self._deleteServer = False
+		self._deleteServerCommand = ''
+		self._clientObjectSearchFilter = ''
+		self._serverObjectSearchFilter = ''
+		self._defaultDomain = None
+		
+		logger.info(u"Connecting to ldap server '%s' as user '%s'" % (self._address, self._username))
+		self._ldap = LDAPSession(
+				host	 = self._address,
+				username = self._username, 
+				password = self._password )
+		self._ldap.connect()
+		
+		
 	def base_delete(self):
 		DataBackend.base_delete(self)
 		
@@ -222,7 +255,176 @@ class LDAPBackend(DataBackend):
 
 
 
+# ======================================================================================================
+# =                                       CLASS SESSION                                                =
+# ======================================================================================================	
 
+class LDAPSession:
+	''' This class handles the requests to a ldap server '''
+	SCOPE_SUBTREE = ldap.SCOPE_SUBTREE
+	SCOPE_BASE = ldap.SCOPE_BASE
+	
+	def __init__(self, host, username, password):
+		''' Session constructor. '''
+		self._host = host
+		self._username = username
+		self._password = password
+		self._ldap = None
+		
+		self._commandCount = 0
+		self._searchCount = 0
+		self._deleteCount = 0
+		self._addCount = 0
+		self._modifyCount = 0
+		
+	
+	def getCommandCount(self):	
+		''' Get number of all commands (requests) sent to ldap server. '''
+		return self._commandCount
+	def getSearchCount(self):
+		''' Get number of all search commands (requests) sent to ldap server. '''
+		return self._searchCount
+	def getDeleteCount(self):	
+		''' Get number of all delete commands (requests) sent to ldap server. '''
+		return self._deleteCount
+	def getAddCount(self):		
+		''' Get number of all add commands (requests) sent to ldap server. '''
+		return self._addCount
+	def getModifyCount(self):
+		''' Get number of all modify commands (requests) sent to ldap server. '''
+		return self._modifyCount
+	def getCommandStatistics(self):
+		''' Get number of all commands as dict. '''
+		return { 	'total': 	self._commandCount,
+				'search':	self._searchCount,
+				'delete':	self._deleteCount,
+				'add': 		self._addCount,
+				'modify':	self._modifyCount }
+	
+	def connect(self):
+		''' Connect to a ldap server. '''
+		self._ldap = ldap.open(self._host)
+		self._ldap.protocol_version = ldap.VERSION3
+		try:
+			self._ldap.bind_s(self._username, self._password, ldap.AUTH_SIMPLE)
+			logger.info(u'Successfully connected to LDAP-Server.')
+		except ldap.LDAPError, e:
+			logger.error(u"Bind to LDAP failed: %s" % e)
+			raise BackendIOError(u"Bind to LDAP server '%s' as '%s' failed: %s" % (self._host, self._username, e))
+	
+	def disconnect(self):
+		''' Disconnect from ldap server '''
+		if not self._ldap:
+			return
+		try:
+			self._ldap.unbind()
+		except Exception, e:
+			pass
+		
+	def search(self, baseDn, scope, filter, attributes):
+		''' This function is used to search in a ldap directory. '''
+		self._commandCount += 1
+		self._searchCount += 1
+		logger.debug(u"Searching in baseDn: %s, scope: %s, filter: '%s', attributes: '%s' " \
+					% (baseDn, scope, filter, attributes) )
+		result = []
+		try:
+			try:
+				result = self._ldap.search_s(baseDn, scope, filter, attributes)
+			except ldap.LDAPError, e:
+				if isinstance(e, ldap.SERVER_DOWN) or (e.__str__().lower().find('ldap connection invalid') != -1):
+					# Possibly timed out
+					logger.warning(u"LDAP connection possibly timed out: %s, trying to reconnect" % e)
+					self.connect()
+					result = self._ldap.search_s(baseDn, scope, filter, attributes)
+				else:
+					raise
+		except Exception, e:
+			logger.debug(u"LDAP search error %s: %s" % (e.__class__, e))
+			if (e.__class__ == ldap.NO_SUCH_OBJECT):
+				raise BackendMissingDataError(u"No results for search in baseDn: '%s', filter: '%s', scope: %s" \
+					% (baseDn, filter, scope) )
+			
+			logger.critical(u"LDAP search error %s: %s" % (e.__class__, e))
+			raise BackendIOError(u"Error searching in baseDn '%s', filter '%s', scope %s : %s" \
+					% (baseDn, filter, scope, e) )
+		if (result == []):
+			raise BackendMissingDataError(u"No results for search in baseDn: '%s', filter: '%s', scope: %s" \
+					% (baseDn, filter, scope) )
+		return result
+	
+	def delete(self, dn):
+		''' This function is used to delete an object in a ldap directory. '''
+		self._commandCount += 1
+		self._deleteCount += 1
+		logger.debug(u"Deleting Object from LDAP, dn: '%s'" % dn)
+		try:
+			try:
+				self._ldap.delete_s(dn)
+			except ldap.LDAPError, e:
+				if isinstance(e, ldap.SERVER_DOWN) or (e.__str__().lower().find('ldap connection invalid') != -1):
+					# Possibly timed out
+					logger.warning(u"LDAP connection possibly timed out: %s, trying to reconnect" % e)
+					self.connect()
+					self._ldap.delete_s(dn)
+				else:
+					raise
+		except ldap.LDAPError, e:
+			raise BackendIOError(e)
+	
+	def modifyByModlist(self, dn, old, new):
+		''' This function is used to modify an object in a ldap directory. '''
+		self._commandCount += 1
+		self._modifyCount += 1
+		
+		logger.debug(u"[old]: %s" % old)
+		logger.debug(u"[new]: %s" % new)
+		attrs = ldap.modlist.modifyModlist(old,new)
+		logger.debug(u"[change]: %s" % attrs)
+		if (attrs == []):
+			logger.debug(u"Object '%s' unchanged." % dn)
+			return
+		logger.debug(u"Modifying Object in LDAP, dn: '%s'" % dn)
+		try:
+			try:
+				self._ldap.modify_s(dn,attrs)
+			except ldap.LDAPError, e:
+				if isinstance(e, ldap.SERVER_DOWN) or (e.__str__().lower().find('ldap connection invalid') != -1):
+					# Possibly timed out
+					logger.warning(u"LDAP connection possibly timed out: %s, trying to reconnect" % e)
+					self.connect()
+					self._ldap.modify_s(dn,attrs)
+				else:
+					raise
+		except ldap.LDAPError, e:
+			raise BackendIOError(e)
+		except TypeError, e:
+			raise BackendBadValueError(e)
+		
+		
+	def addByModlist(self, dn, new):
+		''' This function is used to add an object to the ldap directory. '''
+		self._commandCount += 1
+		self._addCount += 1
+		
+		attrs = ldap.modlist.addModlist(new)
+		logger.debug(u"Adding Object to LDAP, dn: '%s'" % dn)
+		logger.debug(u"attrs: '%s'" % attrs)
+		try:
+			try:
+				self._ldap.add_s(dn,attrs)
+			except ldap.LDAPError, e:
+				if isinstance(e, ldap.SERVER_DOWN) or (e.__str__().lower().find('ldap connection invalid') != -1):
+					# Possibly timed out
+					logger.warning(u"LDAP connection possibly timed out: %s, trying to reconnect" % e)
+					self.connect()
+					self._ldap.add_s(dn,attrs)
+				else:
+					raise
+		except ldap.LDAPError, e:
+			raise BackendIOError(e)
+		except TypeError, e:
+			raise BackendBadValueError(e)
 
 
 
