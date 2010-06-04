84da8cf53aaab45b6059b7d85370645012b334ac
Jan Schneider <j.schneider@uib.de>
2010-06-04 09:27:54 +0000

    -

--- OPSI/Backend/Backend.py
+++ OPSI/Backend/Backend.py
@@ -406,10 +406,18 @@ class Backend:
 		if not result['password']:
 			raise BackendMissingDataError(u"Username '%s' not found in '%s'" % (username, OPSI_PASSWD_FILE))
 		
-		depot = self._context.host_getObjects(id = depotId)
+		backendStack = [ self._context, self, self._backend, self._backend._backend, self._backend._backend._backend, self._backend._backend._backend._backend]
+		unprotectedBackend = self._context
+		for i in range(len(backendStack)):
+			if backendStack[i].__class__.__name__.endswith('BackendAccessControl'):
+				unprotectedBackend = backendStack[i+1]
+		depot = unprotectedBackend.host_getObjects(id = depotId)
 		if not depot:
 			raise Exception(u"Depot '%s' not found in backend" % depotId)
 		depot = depot[0]
+		if not depot.opsiHostKey:
+			raise Exception(u"Host key for depot '%s' not found" % depotId)
+		
 		result['password'] = blowfishDecrypt(depot.opsiHostKey, result['password'])
 		
 		if (username == 'pcpatch'):
