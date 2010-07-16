6a6ab7675a3a8d6f1e0f583ce6d07e52e94951f0
Christian Kampka <c.kampka@uib.de>
2010-07-16 09:01:44 +0000

    use propper test skipping if python version is to low

--- tests/opsi/TypesTest.py
+++ tests/opsi/TypesTest.py
@@ -1,18 +1,22 @@
 
 from opsidevtools.unittest.lib.unittest2.case import TestCase
+from opsidevtools.unittest.lib import unittest2 as unittest
 from OPSI.Types import *
-
+import sys
 
 class TypesTestCase(TestCase):
 	
+	@unittest.skipIf(sys.version_info < (2, 6), "Class decorators are not supported in python < 2.6")
 	def test_argsDecoratorClassConstruction(self):
-		
-		raise Excpetion("produces syntax error on python 2.5")
-		#@args("somearg", "someOtherArg")
-		#class SomeClass(object):
-		#	def __init__(self, **kwargs):
-		#		pass
-		
+
+		# will raise SyntaxError on python < 2.6 so we need to create this on demand
+		exec(' \
+		@args("somearg", "someOtherArg") \
+		class SomeClass(object): \
+			def __init__(self, **kwargs): \
+				pass'
+		)
+			
 		someObj = SomeClass()
 		try:
 			self.assertIsNone(someObj.somearg, "Expected somearg to be None, but got %s instead" % someObj.somearg)
@@ -20,12 +24,14 @@ class TypesTestCase(TestCase):
 		except AttributeError, e:
 			self.fail(e)
 		
-		raise Excpetion("produces syntax error on python 2.5")
-		#@args("somearg", someOtherArg=forceInt)
-		#class SomeOtherClass(object):
-		#	def __init__(self, **kwargs):
-		#		pass
-		
+		# will raise SyntaxError on python < 2.6 so we need to create this on demand
+		exec(' \
+		@args("somearg", someOtherArg=forceInt) \
+		class SomeClass(object): \
+			def __init__(self, **kwargs): \
+				pass'
+		)
+
 		someOtherObj = SomeOtherClass(someOtherArg="5")
 		
 
@@ -36,14 +42,17 @@ class TypesTestCase(TestCase):
 		except AttributeError,e:
 			self.fail(e)
 			
+	@unittest.skipIf(sys.version_info < (2, 6), "Class decorators are not supported in python < 2.6")
 	def test_argsDecoratorWithPrivateArgs(self):
 		
-		raise Excpetion("produces syntax error on python 2.5")
-		#@args("_somearg", "_someOtherArg")
-		#class SomeClass(object):
-		#	def __init__(self, **kwargs):
-		#		pass
-		
+		# will raise SyntaxError on python < 2.6 so we need to create this on demand
+		exec(' \
+		@args("_somearg", "_someOtherArg") \
+		class SomeClass(object): \
+			def __init__(self, **kwargs): \
+				pass'
+		)
+
 		someObj = SomeClass(somearg=5)
 		try:
 			self.assertEqual(5, someObj._somearg, "Expected somearg to be %d, but got %s instead" % (5, someObj._somearg))
