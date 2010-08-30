d5da7dc83698f49cdab9bb026e293dcc656abfd2
Jan Schneider <j.schneider@uib.de>
2010-08-30 13:49:46 +0000

    -

--- rpm/build_rpm.sh
+++ rpm/build_rpm.sh
@@ -17,7 +17,7 @@ test -e $sourcedir || mkdir -p $sourcedir
 test -e $buildroot && rm -rf $buildroot
 
 cd $dir
-bash rpm/create_source.sh
+bash ./create_source.sh
 mv /tmp/${packagename}-${version}.tar.bz2 ${sourcedir}/
 cp rpm/${packagename}.spec $specdir/
 rpmbuild -ba $specdir/${packagename}.spec
