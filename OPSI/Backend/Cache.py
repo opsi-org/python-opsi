65c6e2c7c4a65e1fdec324eb9accb5018f290083
Jan Schneider <j.schneider@uib.de>
2011-01-06 12:56:09 +0000

    -

--- OPSI/Backend/Cache.py
+++ OPSI/Backend/Cache.py
@@ -1,10 +1,11 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 """
-   = = = = = = = = = = = = = = = = = = = = = = = = =
-   =   ocdlibnonfree.CacheBackend                  =
-   = = = = = = = = = = = = = = = = = = = = = = = = =
+   = = = = = = = = = = = = = = = = = =
+   =   opsi python library - Cache   =
+   = = = = = = = = = = = = = = = = = =
    
-   opsiclientd is part of the desktop management solution opsi
+   This module is part of the desktop management solution opsi
    (open pc server integration) http://www.opsi.org
    
    Copyright (C) 2010 uib GmbH
@@ -13,8 +14,22 @@
    
    All rights reserved.
    
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 2 as
+   published by the Free Software Foundation.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+   
    @copyright:	uib GmbH <info@uib.de>
    @author: Jan Schneider <j.schneider@uib.de>
+   @license: GNU General Public License version 2
 """
 
 import inspect
