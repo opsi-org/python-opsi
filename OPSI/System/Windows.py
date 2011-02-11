0fe5745009aae5ffe2130dee422312a4bffdc0e2
Jan Schneider <j.schneider@uib.de>
2011-02-11 15:57:47 +0000

    -

--- OPSI/System/Windows.py
+++ OPSI/System/Windows.py
@@ -268,14 +268,14 @@ class NetworkPerformanceCounter(threading.Thread):
 		
 	def _getStatistics(self):
 		now = time.time()
-		#for instance in self.wmi.Win32_PerfRawData_Tcpip_NetworkInterface(["BytesReceivedPersec", "BytesSentPersec"], Name = self.interface):
-		#	bytesIn = instance.BytesReceivedPersec
-		#	bytesOut = instance.BytesSentPersec
-		for instance in self.wmi.Win32_PerfRawData_Tcpip_NetworkInterface():
-			if (instance.Name == self.interface):
-				bytesIn = instance.BytesReceivedPersec
-				bytesOut = instance.BytesSentPersec
-				break
+		for instance in self.wmi.Win32_PerfRawData_Tcpip_NetworkInterface(["BytesReceivedPersec", "BytesSentPersec"], Name = self.interface):
+			bytesIn = instance.BytesReceivedPersec
+			bytesOut = instance.BytesSentPersec
+		#for instance in self.wmi.Win32_PerfRawData_Tcpip_NetworkInterface():
+		#	if (instance.Name == self.interface):
+		#		bytesIn = instance.BytesReceivedPersec
+		#		bytesOut = instance.BytesSentPersec
+		#		break
 		timeDiff = 1
 		if self._lastTime:
 			timeDiff = now - self._lastTime
