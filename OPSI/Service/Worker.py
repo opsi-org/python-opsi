ea7a3f951b8629a7ff921f4d28346acc11ed5324
Jan Schneider <j.schneider@uib.de>
2011-02-22 13:44:46 +0000

    -

--- OPSI/Service/Worker.py
+++ OPSI/Service/Worker.py
@@ -44,11 +44,11 @@ from OPSI.Service.JsonRpc import JsonRpc
 
 logger = Logger()
 
-interfacePage = u'''
-<?xml version="1.0" encoding="UTF-8"?>
+interfacePage = u'''<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>%(title)s</title>
 	<style>
 	a:link 	      { color: #555555; text-decoration: none; }
