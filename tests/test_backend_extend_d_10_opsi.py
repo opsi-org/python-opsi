5c560ff04ac67ec518a87d3dd396790c3255df33
Niko Wenselowski <n.wenselowski@uib.de>
2013-08-16 13:52:46 +0000

    Finishing a testcase for IssueID #557.
    This is now the real commit...

--- tests/test_backend_extend_d_10_opsi.py
+++ tests/test_backend_extend_d_10_opsi.py
@@ -7,20 +7,15 @@ import tempfile
 import unittest
 
 from OPSI.Backend.BackendManager import BackendManager
-from OPSI.Logger import Logger, LOG_DEBUG, LOG_DEBUG2
-from OPSI.Object import OpsiClient, LocalbootProduct, ProductOnClient, ProductDependency
+from OPSI.Object import (OpsiClient, LocalbootProduct, ProductOnClient,
+                         ProductDependency, OpsiDepotserver, ProductOnDepot,
+                         UnicodeConfig, ConfigState)
 
-
-logger = Logger()
-logger.setConsoleLevel(LOG_DEBUG2)
-logger.setConsoleColor(True)
 
 class BackendExtendedThroughOPSITestCase(unittest.TestCase):
     BACKEND_SUBFOLDER = 'data'
 
     def setUp(self):
-
-
         self.TMP_CONFIG_DIR = self._copyOriginalBackendToTemporaryLocation()
 
         self.backendManager = BackendManager(
@@ -56,7 +51,7 @@ class BackendExtendedThroughOPSITestCase(unittest.TestCase):
     @classmethod
     def _setupFileBackend(cls, targetDirectory):
         cls._patchFileBackend(targetDirectory)
-        cls._createClientTemplates(os.path.join(targetDirectory, 'baseDir'))
+        cls._createClientTemplateFolders(os.path.join(targetDirectory, 'baseDir'))
 
     @classmethod
     def _patchFileBackend(cls, backendDirectory):
@@ -78,12 +73,9 @@ config = {{
             config.write(new_configuration)
 
     @classmethod
-    def _createClientTemplates(cls, targetDirectory):
+    def _createClientTemplateFolders(cls, targetDirectory):
         templateDirectory = os.path.join(targetDirectory, 'config', 'templates')
         os.makedirs(templateDirectory)
-
-        with open(os.path.join(templateDirectory, 'pcproto.ini'), 'w') as template:
-            template.write('')
 
     @classmethod
     def _patchDispatchConfig(cls, targetDirectory):
@@ -101,9 +93,13 @@ config = {{
             id='backend-test-1.vmnat.local',
             description='Unittest Test client.'
         )
-        self.client = client
 
-        self.backendManager.host_createObjects([client])
+        depot = OpsiDepotserver(
+            id='depotserver1.some.test',
+            description='Test Depot',
+        )
+
+        self.backendManager.host_createObjects([client, depot])
 
         firstProduct = LocalbootProduct('already_installed', '1.0', '1.0')
         secondProduct = LocalbootProduct('to_install', '1.0', '1.0')
@@ -126,15 +122,52 @@ config = {{
 
         poc = ProductOnClient(
             clientId=client.id,
-            productId=secondProduct.id,
-            productType=secondProduct.getType(),
-            productVersion=secondProduct.productVersion,
-            packageVersion=secondProduct.packageVersion,
+            productId=firstProduct.id,
+            productType=firstProduct.getType(),
+            productVersion=firstProduct.productVersion,
+            packageVersion=firstProduct.packageVersion,
             installationStatus='installed',
             actionResult='successful'
         )
 
         self.backendManager.productOnClient_createObjects([poc])
+
+        firstProductOnDepot = ProductOnDepot(
+            productId=firstProduct.id,
+            productType=firstProduct.getType(),
+            productVersion=firstProduct.productVersion,
+            packageVersion=firstProduct.packageVersion,
+            depotId=depot.getId(),
+            locked=False
+        )
+
+        secondProductOnDepot = ProductOnDepot(
+            productId=secondProduct.id,
+            productType=secondProduct.getType(),
+            productVersion=secondProduct.productVersion,
+            packageVersion=secondProduct.packageVersion,
+            depotId=depot.getId(),
+            locked=False
+        )
+
+        self.backendManager.productOnDepot_createObjects([firstProductOnDepot, secondProductOnDepot])
+
+        clientConfigDepotId = UnicodeConfig(
+            id=u'clientconfig.depot.id',
+            description=u'Depotserver to use',
+            possibleValues=[],
+            defaultValues=[depot.id]
+        )
+
+        self.backendManager.config_createObjects(clientConfigDepotId)
+
+        clientDepotMappingConfigState = ConfigState(
+            configId=clientConfigDepotId.getId(),
+            objectId=client.getId(),
+            values=depot.getId()
+        )
+
+        self.backendManager.configState_createObjects(clientDepotMappingConfigState)
 
     def tearDown(self):
         if os.path.exists(self.TMP_CONFIG_DIR):
@@ -142,16 +175,51 @@ config = {{
 
         del self.backendManager
 
+    def testBackendDoesNotCreateProductsOnClientsOnItsOwn(self):
+        pocs = self.backendManager.productOnClient_getObjects()
+        self.assertEqual(
+            1,
+            len(pocs),
+            'Expected to have only one ProductOnClient but got {n} instead: '
+            '{0}'.format(pocs, n=len(pocs))
+        )
+
     def testSetProductActionRequestWithDependenciesSetsProductsToSetup(self):
         """
-        An product action request should set products to setup even if they \
-are already installed on a client.
+        An product action request should set product that are dependencies to \
+setup even if they are already installed on a client.
         """
         self.backendManager.setProductActionRequestWithDependencies(
             'to_install',
             'backend-test-1.vmnat.local',
             'setup'
         )
+
+        productsOnClient = self.backendManager.productOnClient_getObjects()
+        self.assertEqual(
+            2,
+            len(productsOnClient),
+            'Expected to have two ProductOnClients. Instead we got {n}: '
+            '{0}'.format(productsOnClient, n=len(productsOnClient))
+        )
+
+        productThatShouldBeReinstalled = None
+        for poc in productsOnClient:
+            self.assertEqual(
+                'backend-test-1.vmnat.local',
+                poc.clientId,
+                'Wrong client id. Expected it to be "{0}" but got: '
+                '{1}'.format('backend-test-1.vmnat.local', poc.clientId)
+            )
+
+            if poc.productId == 'already_installed':
+                productThatShouldBeReinstalled = poc
+
+        if productThatShouldBeReinstalled is None:
+            self.fail('Could not find a product "{0}" on the client.'.format('already_installed'))
+
+        self.assertEquals(productThatShouldBeReinstalled.productId, 'already_installed')
+        self.assertEquals(productThatShouldBeReinstalled.actionRequest, 'setup')
 
 
 if __name__ == '__main__':
