6c4df175320c61c4ce0edb3c8798cae9a44885b7
Niko Wenselowski <n.wenselowski@uib.de>
2013-08-23 14:45:16 +0000

    Added more distribution checks.

--- tests/test_posix_system_distribution.py
+++ tests/test_posix_system_distribution.py
@@ -6,8 +6,12 @@ from OPSI.System.Posix import Distribution
 
 
 class DistributionTestCase(unittest.TestCase):
+    DIST_INFO = None
     def setUp(self):
-        self.dist = Distribution()
+        if self.DIST_INFO:
+            self.dist = Distribution(distribution_information=self.DIST_INFO)
+        else:
+            self.dist = Distribution()
 
     def tearDown(self):
         self.dist
@@ -18,54 +22,80 @@ class DistributionTestCase(unittest.TestCase):
 
 
 class DebianSqueezeTestCase(DistributionTestCase):
-    def setUp(self):
-        self.dist = Distribution(distribution_information=('debian', '6.0.7', ''))
+    DIST_INFO = ('debian', '6.0.7', '')
 
     def testReadingVersionIsCorrect(self):
         self.assertEqual((6, 0, 7), self.dist.version)
 
 
 class DebianWheezyTestCase(DistributionTestCase):
-    def setUp(self):
-        self.dist = Distribution(distribution_information=('debian', '7.1', ''))
+    DIST_INFO = ('debian', '7.1', '')
 
     def testReadingVersionIsCorrect(self):
         self.assertEqual((7, 1), self.dist.version)
 
 
+class CentOS64TestCase(DistributionTestCase):
+    DIST_INFO = ('CentOS', '6.4', 'Final')
+
+    def testReadingVersionIsCorrect(self):
+        self.assertEqual((6, 4), self.dist.version)
+
+
+class OpenSuse113TestCase(DistributionTestCase):
+    DIST_INFO = ('openSUSE ', '11.3', 'i586')
+
+    def testReadingVersionIsCorrect(self):
+        self.assertEqual((11, 3), self.dist.version)
+
+
+class OpenSuse121TestCase(DistributionTestCase):
+    DIST_INFO = ('openSUSE ', '12.1', 'x86_64')
+
+    def testReadingVersionIsCorrect(self):
+        self.assertEqual((12, 1), self.dist.version)
+
+
+class RedHatEnterpriseLinux6TestCase(DistributionTestCase):
+    DIST_INFO = ('Red Hat Enterprise Linux Server', '6.4', 'Santiago')
+
+    def testReadingVersionIsCorrect(self):
+        self.assertEqual((6, 4), self.dist.version)
+
+
+class SuseLinuxEnterpriseLinuxServer11TestCase(DistributionTestCase):
+    DIST_INFO = ('SUSE Linux Enterprise Server ', '11', 'x86_64')
+
+    def testReadingVersionIsCorrect(self):
+        self.assertEqual((11, ), self.dist.version)
+
+
 class UbuntuRaringTestCase(DistributionTestCase):
-    def setUp(self):
-        self.dist = Distribution(distribution_information=('Ubuntu', '13.04', 'raring'))
+    DIST_INFO = ('Ubuntu', '13.04', 'raring')
 
     def testReadingVersionIsCorrect(self):
         self.assertEqual((13, 4), self.dist.version)
 
 
 class UbuntuPreciseTestCase(DistributionTestCase):
-    def setUp(self):
-        self.dist = Distribution(distribution_information=('Ubuntu', '12.04', 'precise'))
+    DIST_INFO = ('Ubuntu', '12.04', 'precise')
 
     def testReadingVersionIsCorrect(self):
         self.assertEqual((12, 4), self.dist.version)
 
 
 class UCSHornLeheTestCase(DistributionTestCase):
-    def setUp(self):
-        self.dist = Distribution(distribution_information=('"Univention"', '"3.0-2 errata145"', '"Horn-Lehe"'))
+    DIST_INFO = ('"Univention"', '"3.0-2 errata145"', '"Horn-Lehe"')
 
     def testReadingVersionIsCorrect(self):
         self.assertEqual((3, 0), self.dist.version)
 
 
 class UCSFinndorfTestCase(DistributionTestCase):
-    def setUp(self):
-        self.dist = Distribution(distribution_information=('"Univention"', '"3.1-1 errata163"', '"Findorff"'))
+    DIST_INFO = ('"Univention"', '"3.1-1 errata163"', '"Findorff"')
 
     def testReadingVersionIsCorrect(self):
         self.assertEqual((3, 1), self.dist.version)
-
-
-
 
 
 if __name__ == '__main__':
