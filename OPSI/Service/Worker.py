b66795d3fed81030d5d703c549300ef4f30b20ff
Niko Wenselowski <n.wenselowski@uib.de>
2013-08-27 13:18:35 +0000

    Removed wildcard imports and changed them to normal imports. This fixes a bug with opsiconfd not starting due to missing definitions in OPSI.Objects.
    Changed license from GPLv2 to AGPLv3

--- OPSI/Service/Worker.py
+++ OPSI/Service/Worker.py
@@ -1,47 +1,48 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 """
-   = = = = = = = = = = = = = = = = = = =
-   =   opsi python library - Worker    =
-   = = = = = = = = = = = = = = = = = = =
-   
-   This module is part of the desktop management solution opsi
-   (open pc server integration) http://www.opsi.org
-   
-   Copyright (C) 2010 uib GmbH
-   
-   http://www.uib.de/
-   
-   All rights reserved.
-   
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License version 2 as
-   published by the Free Software Foundation.
-   
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-   
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-   
-   @copyright:	uib GmbH <info@uib.de>
-   @author: Jan Schneider <j.schneider@uib.de>
-   @license: GNU General Public License version 2
+opsi python library - Worker
+
+This module is part of the desktop management solution opsi
+(open pc server integration) http://www.opsi.org
+
+Copyright (C) 2010, 2013 uib GmbH
+
+http://www.uib.de/
+
+All rights reserved.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+@copyright:	uib GmbH <info@uib.de>
+@author: Jan Schneider <j.schneider@uib.de>
+@author: Niko Wenselowski <n.wenselowski@uib.de>
+@license: GNU Affero General Public License version 3
 """
 
-import base64, urllib, zlib, copy
+import base64
+import urllib
+import zlib
 from twisted.internet import defer, reactor, threads
 from twisted.python import failure
 
 from OPSI.web2 import responsecode, http_headers, http, stream
 
-from OPSI.Logger import *
-from OPSI.Types import *
-from OPSI.Util import objectToHtml, toJson, fromJson
-from OPSI.Object import serialize, deserialize
+from OPSI.Logger import Logger, LOG_ERROR, LOG_INFO
+from OPSI.Types import (forceUnicode, forceList, OpsiBadRpcError,
+						OpsiAuthenticationError)
+from OPSI.Util import objectToHtml, toJson, fromJson, serialize
 from OPSI.Service.JsonRpc import JsonRpc
 
 logger = Logger()
@@ -75,7 +76,7 @@ interfacePage = u'''<?xml version="1.0" encoding="UTF-8"?>
 		var params = '';
 		var id = '"id": 1';
 		%(javascript)s
-		
+
 		function createElement(element) {
 			if (typeof document.createElementNS != 'undefined') {
 				return document.createElementNS('http://www.w3.org/1999/xhtml', element);
@@ -85,7 +86,7 @@ interfacePage = u'''<?xml version="1.0" encoding="UTF-8"?>
 			}
 			return false;
 		}
-		
+
 		function selectPath(select) {
 			path = select.value;
 			document.getElementById('json_method').firstChild.data = '"backend_getInterface"';
@@ -114,7 +115,7 @@ interfacePage = u'''<?xml version="1.0" encoding="UTF-8"?>
 					tbody.removeChild(tbody.childNodes[i]);
 				}
 			}
-			
+
 			for (i=0; i < parameters[select.value].length; i++) {
 				tr = createElement("tr");
 				td1 = createElement("td");
@@ -134,10 +135,10 @@ interfacePage = u'''<?xml version="1.0" encoding="UTF-8"?>
 			}
 			tbody.appendChild(json)
 			tbody.appendChild(button)
-			
+
 			jsonString();
 		}
-		
+
 		function onSubmit() {
 			var json = '{ "id": 1, "method": ';
 			json += document.getElementById('json_method').firstChild.data;
@@ -147,14 +148,14 @@ interfacePage = u'''<?xml version="1.0" encoding="UTF-8"?>
 			window.location.href = '/' + path + '?' + json;
 			return false;
 		}
-		
+
 		function jsonString() {
 			span = document.getElementById('json_method');
 			for (i=span.childNodes.length-1; i>=0; i--) {
 				span.removeChild(span.childNodes[i])
 			}
 			span.appendChild(document.createTextNode('"' + method + '"'));
-			
+
 			span = document.getElementById('json_params');
 			for (i=span.childNodes.length-1; i>=0; i--) {
 				span.removeChild(span.childNodes[i])
@@ -229,6 +230,7 @@ interfacePage = u'''<?xml version="1.0" encoding="UTF-8"?>
 </html>
 '''
 
+
 class WorkerOpsi:
 	def __init__(self, service, request, resource):
 		self.service   = service
@@ -239,7 +241,7 @@ class WorkerOpsi:
 		self.resource  = resource
 		self.session   = None
 		self.authRealm = 'OPSI Service'
-		
+
 	def process(self):
 		logger.info(u"Worker %s started processing" % self)
 		deferred = defer.Deferred()
@@ -253,38 +255,33 @@ class WorkerOpsi:
 		deferred.addErrback(self._errback)
 		deferred.callback(None)
 		return deferred
-	
+
 	def _getSessionHandler(self):
 		if hasattr(self.service, '_getSessionHandler'):
 			return self.service._getSessionHandler()
 		return None
-	
+
 	def _delayResult(self, seconds, result):
 		class DelayResult:
 			def __init__(self, seconds, result):
 				self.result = result
 				self.deferred = defer.Deferred()
 				reactor.callLater(seconds, self.returnResult)
-				
+
 			def returnResult(self):
 				self.deferred.callback(self.result)
 		return DelayResult(seconds, result).deferred
-	
+
 	def _errback(self, failure):
 		logger.debug2("%s._errback" % self.__class__.__name__)
-		
+
 		self._freeSession(failure)
-		
+
 		result = self._renderError(failure)
 		result = self._setCookie(result)
 		result.code = responsecode.INTERNAL_SERVER_ERROR
 		try:
 			failure.raiseException()
-
-		#except AttributeError, e:
-		#	logger.debug(e)
-		#	result.code = responsecode.NOT_FOUND
-
 		except OpsiAuthenticationError, e:
 			logger.logException(e)
 			result.code = responsecode.UNAUTHORIZED
@@ -296,7 +293,7 @@ class WorkerOpsi:
 			logger.logException(e, LOG_ERROR)
 			logger.error(failure)
 		return result
-	
+
 	def _renderError(self, failure):
 		result = http.Response()
 		result.headers.setHeader('content-type', http_headers.MimeType("text", "html", {"charset": "utf-8"}))
@@ -307,13 +304,13 @@ class WorkerOpsi:
 			error = forceUnicode(e)
 		result.stream = stream.IByteStream(stream.IByteStream(error.encode('utf-8')))
 		return result
-	
+
 	def _freeSession(self, result):
 		if self.session:
 			logger.debug(u"Freeing session %s" % self.session)
 			self.session.decreaseUsageCount()
 		return result
-	
+
 	def _getAuthorization(self):
 		(user, password) = (u'', u'')
 		logger.debug(u"Trying to get username and password from Authorization header")
@@ -322,7 +319,7 @@ class WorkerOpsi:
 			logger.debug(u"Authorization header found (type: %s)" % auth[0])
 			try:
 				encoded = auth[1]
-				
+
 				logger.confidential(u"Auth encoded: %s" % encoded)
 				parts = unicode(base64.decodestring(encoded), 'latin-1').split(':')
 				if (len(parts) > 6):
@@ -336,21 +333,21 @@ class WorkerOpsi:
 			except Exception, e:
 				logger.error(u"Bad Authorization header from '%s': %s" % (self.request.remoteAddr.host, e))
 		return (user, password)
-	
+
 	def _getCredentials(self):
 		return self._getAuthorization()
-	
+
 	def _getUserAgent(self):
 		# Get user agent
 		userAgent = None
 		try:
 			userAgent = self.request.headers.getHeader('user-agent')
-		except Exception, e:
+		except Exception:
 			logger.info(u"Client '%s' did not supply user-agent" % self.request.remoteAddr.host)
 		if not userAgent:
 			userAgent = 'unknown'
 		return userAgent
-	
+
 	def _getSessionId(self):
 		# Get session id from cookie request header
 		userAgent = self._getUserAgent()
@@ -370,67 +367,67 @@ class WorkerOpsi:
 		except Exception, e:
 			logger.error(u"Failed to get cookie from header: %s" % e)
 		return sessionId
-		
+
 	def _getSession(self, result):
 		''' This method restores a session or generates a new one. '''
 		self.session = None
-		
+
 		logger.confidential(u"Request headers: %s " % self.request.headers)
-		
+
 		userAgent = self._getUserAgent()
-		
+
 		# Get session handler
 		sessionHandler = self._getSessionHandler()
-		
+
 		# Get session id from cookie request header
 		sessionId = self._getSessionId()
-		
+
 		# Get Session object
 		self.session = sessionHandler.getSession(sessionId, self.request.remoteAddr.host)
 		if (sessionId == self.session.uid):
 			logger.info(u"Reusing session for client '%s', application '%s'" % (self.request.remoteAddr.host, userAgent))
 		elif sessionId:
 			logger.notice(u"Application '%s' on client '%s' supplied non existing session id: %s" % (userAgent, self.request.remoteAddr.host, sessionId))
-		
+
 		if sessionHandler and self.session.ip and (self.session.ip != self.request.remoteAddr.host):
 			logger.critical(u"Client ip '%s' does not match session ip '%s', deleting old session and creating a new one" \
 				% (self.request.remoteAddr.host, self.session.ip) )
 			sessionHandler.deleteSession(self.session.uid)
 			self.session = sessionHandler.getSession()
-		
+
 		# Set ip
 		self.session.ip = self.request.remoteAddr.host
-		
+
 		# Set user-agent / application
 		if self.session.userAgent and (self.session.userAgent != userAgent):
 			logger.warning(u"Application changed from '%s' to '%s' for existing session of client '%s'" \
 				% (self.session.userAgent, userAgent, self.request.remoteAddr.host))
 		self.session.userAgent = userAgent
-		
+
 		logger.confidential(u"Session id is '%s' for client '%s', application '%s'" \
 			% (self.session.uid, self.request.remoteAddr.host, self.session.userAgent))
-		
+
 		logger.confidential(u"Session content: %s" % self.session.__dict__)
 		return result
-	
+
 	def _setCookie(self, result):
 		logger.debug(u"%s._setCookie" % self)
 		if not self.session:
 			return result
-		
+
 		# Add cookie to headers
 		cookie = http_headers.Cookie(self.session.name.encode('ascii', 'replace'), self.session.uid.encode('ascii', 'replace'), path='/')
 		if not isinstance(result, http.Response):
 			result = http.Response()
 		result.headers.setHeader('set-cookie', [ cookie ] )
 		return result
-		
+
 	def _authenticate(self, result):
 		''' This function tries to authenticate a user.
 		    Raises an exception on authentication failure. '''
 		if self.session.authenticated:
 			return result
-		
+
 		try:
 			# Get authorization
 			(self.session.user, self.session.password) = self._getCredentials()
@@ -441,7 +438,7 @@ class WorkerOpsi:
 			self._getSessionHandler().deleteSession(self.session.uid)
 			raise OpsiAuthenticationError(u"Forbidden: %s" % e)
 		return result
-	
+
 	def _getQuery(self, result):
 		self.query = ''
 		if   (self.request.method == 'GET'):
@@ -449,16 +446,14 @@ class WorkerOpsi:
 		elif (self.request.method == 'POST'):
 			# Returning deferred needed for chaining
 			d = stream.readStream(self.request.stream, self._handlePostData)
-			#d.addErrback(self._errback)
 			return d
 		else:
 			raise ValueError(u"Unhandled method '%s'" % self.request.method)
 		return result
-		
+
 	def _handlePostData(self, chunk):
-		#logger.debug2(u"_handlePostData %s" % unicode(chunk, 'utf-8', 'replace'))
 		self.query += chunk
-	
+
 	def _decodeQuery(self, result):
 		try:
 			if (self.request.method == 'POST'):
@@ -474,16 +469,15 @@ class WorkerOpsi:
 					self.query = zlib.decompress(self.query)
 					self.gzip = True
 			self.query = unicode(self.query, 'utf-8')
-			
-		except (UnicodeError, UnicodeEncodeError), e:
+		except (UnicodeError, UnicodeEncodeError):
 			self.query = unicode(self.query, 'utf-8', 'replace')
 		logger.debug2(u"query: %s" % self.query)
 		return result
-	
+
 	def _processQuery(self, result):
 		logger.warning(u"Class %s should overwrite _processQuery" % self.__class__.__name__)
 		return self._decodeQuery(result)
-		
+
 	def _generateResponse(self, result):
 		if not isinstance(result, http.Response):
 			result = http.Response()
@@ -491,7 +485,7 @@ class WorkerOpsi:
 		result.headers.setHeader('content-type', http_headers.MimeType("text", "html", {"charset": "utf-8"}))
 		result.stream = stream.IByteStream("")
 		return result
-	
+
 	def _setResponse(self, result):
 		return self._generateResponse(result)
 
@@ -499,11 +493,11 @@ class WorkerOpsi:
 class WorkerOpsiJsonRpc(WorkerOpsi):
 	def __init__(self, service, request, resource):
 		WorkerOpsi.__init__(self, service, request, resource)
-		
+
 		self._callInstance = None
 		self._callInterface = None
 		self._rpcs = []
-	
+
 	def _getCallInstance(self, result):
 		logger.warning(u"Class %s should overwrite _getCallInstance" % self.__class__.__name__)
 		self._callInstance = None
@@ -516,26 +510,25 @@ class WorkerOpsiJsonRpc(WorkerOpsi):
 			raise Exception(u"Call instance not defined in %s" % self)
 		if not self._callInterface:
 			raise Exception(u"Call interface not defined in %s" % self)
-		
+
 		rpcs = []
 		try:
 			rpcs = fromJson(self.query, preventObjectCreation = True)
 			if not rpcs:
 				raise Exception(u"Got no rpcs")
-		
 		except Exception, e:
 			raise OpsiBadRpcError(u"Failed to decode rpc: %s" % e)
-		
+
 		for rpc in forceList(rpcs):
 			rpc = JsonRpc(instance = self._callInstance, interface = self._callInterface, rpc = rpc)
 			self._rpcs.append(rpc)
-		
+
 		return result
-	
+
 	def _executeRpc(self, result, rpc):
 		deferred = threads.deferToThread(rpc.execute)
 		return deferred
-		
+
 	def _executeRpcs(self, result):
 		deferred = defer.Deferred()
 		for rpc in self._rpcs:
@@ -543,7 +536,7 @@ class WorkerOpsiJsonRpc(WorkerOpsi):
 		#deferred.addErrback(self._errback)
 		deferred.callback(None)
 		return deferred
-	
+
 	def _processQuery(self, result):
 		deferred = defer.Deferred()
 		deferred.addCallback(self._decodeQuery)
@@ -553,12 +546,12 @@ class WorkerOpsiJsonRpc(WorkerOpsi):
 		#deferred.addErrback(self._errback)
 		deferred.callback(None)
 		return deferred
-		
+
 	def _generateResponse(self, result):
 		if not isinstance(result, http.Response):
 			result = http.Response()
 		result.code = responsecode.OK
-		
+
 		encoding = None
 		try:
 			if ('deflate' in self.request.headers.getHeader('Accept-Encoding')):
@@ -576,13 +569,13 @@ class WorkerOpsiJsonRpc(WorkerOpsi):
 							break
 			except Exception, e:
 				logger.error(u"Failed to get accepted mime types from header: %s" % e)
-			
+
 		if encoding:
 			result.headers.setHeader('content-type', http_headers.MimeType("gzip-application", "json", {"charset": "utf-8"}))
 			result.headers.setHeader('content-encoding', [encoding])
 		else:
 			result.headers.setHeader('content-type', http_headers.MimeType("application", "json", {"charset": "utf-8"}))
-		
+
 		response = []
 
 		for rpc in self._rpcs:
@@ -591,7 +584,7 @@ class WorkerOpsiJsonRpc(WorkerOpsi):
 			response = response[0]
 		if not response:
 			response = None
-		
+
 		if encoding:
 			# level 1 (fastest) to 9 (most compression)
 			level = 1
@@ -600,7 +593,7 @@ class WorkerOpsiJsonRpc(WorkerOpsi):
 		else:
 			result.stream = stream.IByteStream(toJson(response).encode('utf-8'))
 		return result
-	
+
 	def _renderError(self, failure):
 		result = http.Response()
 		result.headers.setHeader('content-type', http_headers.MimeType("text", "html", {"charset": "utf-8"}))
@@ -614,21 +607,20 @@ class WorkerOpsiJsonRpc(WorkerOpsi):
 		return result
 
 
-
 class MultiprocessWorkerOpsiJsonRpc(WorkerOpsiJsonRpc):
-	
-	def _processQuery(self, result):
-		
-		logger.debug(u"Using multiprocessing to handle rpc.")
-		
 
-		
+	def _processQuery(self, result):
+
+		logger.debug(u"Using multiprocessing to handle rpc.")
+
+
+
 		def cleanup(rpc):
 			if (rpc.getMethodName() == 'backend_exit'):
 				logger.notice(u"User '%s' asked to close the session" % self.session.user)
 				self._freeSession(result)
 				self.service._getSessionHandler().deleteSession(self.session.uid)
-		
+
 		def processResult(rpcs):
 			self._rpcs = rpcs
 
@@ -637,7 +629,7 @@ class MultiprocessWorkerOpsiJsonRpc(WorkerOpsiJsonRpc):
 				self._addRpcToStatistics(None, rpc)
 				self.session.setLastRpcMethod(rpc.getMethodName())
 			return rpcs
-		
+
 		def makeInstanceCall():
 			contentType = self.request.headers.getHeader('content-type')
 			contentEncoding = None
@@ -649,19 +641,20 @@ class MultiprocessWorkerOpsiJsonRpc(WorkerOpsiJsonRpc):
 				(contentEncoding == 'gzip') or (contentType and contentType.mediaType.startswith('gzip')))
 			d.addCallback(processResult)
 			return d
-		
+
 		deferred = self._getCallInstance(None)
 		deferred.addCallback(lambda x: makeInstanceCall())
-		
+
 		return deferred
+
 
 class WorkerOpsiJsonInterface(WorkerOpsiJsonRpc):
 	def __init__(self, service, request, resource):
 		WorkerOpsiJsonRpc.__init__(self, service, request, resource)
-	
+
 	def _generateResponse(self, result):
 		logger.info(u"Creating interface page")
-		
+
 		javascript  = u"var currentParams = new Array();\n"
 		javascript += u"var currentMethod = null;\n"
 		currentMethod = u''
@@ -671,7 +664,7 @@ class WorkerOpsiJsonInterface(WorkerOpsiJsonRpc):
 			for i in range(len(self._rpcs[0].params)):
 				param = self._rpcs[0].params[i]
 				javascript += u"currentParams[%d] = '%s';\n" % (i, toJson(param))
-		
+
 		selectMethod = u''
 		for method in self._callInterface:
 			javascript += u"parameters['%s'] = new Array();\n" % (method['name'])
@@ -681,7 +674,7 @@ class WorkerOpsiJsonInterface(WorkerOpsiJsonRpc):
 			if (method['name'] == currentMethod):
 				selected = u' selected="selected"'
 			selectMethod += u'<option%s>%s</option>' % (selected, method['name'])
-		
+
 		resultDiv = u'<div id="result">'
 		if isinstance(result, failure.Failure):
 			error = u'Unknown error'
@@ -699,7 +692,7 @@ class WorkerOpsiJsonInterface(WorkerOpsiJsonRpc):
 				resultDiv += objectToHtml(serialize(rpc.getResponse()))
 				resultDiv += u'</div>'
 		resultDiv += u'</div>'
-		
+
 		html = interfacePage % {
 			'path':          self.path,
 			'title':         u'opsi interface page',
@@ -708,21 +701,22 @@ class WorkerOpsiJsonInterface(WorkerOpsiJsonRpc):
 			'select_method': selectMethod,
 			'result':        resultDiv
 		}
-		
+
 		if not isinstance(result, http.Response):
 			result = http.Response()
 		result.code = responsecode.OK
 		result.stream = stream.IByteStream(html.encode('utf-8').strip())
-		
+
 		return result
-	
+
 	def _renderError(self, failure):
 		return self._generateResponse(failure)
-	
+
+
 class WorkerOpsiDAV(WorkerOpsi):
 	def __init__(self, service, request, resource):
 		WorkerOpsi.__init__(self, service, request, resource)
-	
+
 	def process(self):
 		logger.debug(u"Worker %s started processing" % self)
 		deferred = defer.Deferred()
@@ -736,7 +730,7 @@ class WorkerOpsiDAV(WorkerOpsi):
 		deferred.addErrback(self._errback)
 		deferred.callback(None)
 		return deferred
-	
+
 	def _setResponse(self, result):
 		logger.debug(u"Client requests DAV operation: %s" % self.request)
 		if not self.resource._authRequired and self.request.method not in ('GET', 'PROPFIND', 'OPTIONS', 'USERINFO', 'HEAD'):
@@ -744,9 +738,5 @@ class WorkerOpsiDAV(WorkerOpsi):
 			return http.Response(
 				code	= responsecode.FORBIDDEN,
 				stream	= "Readonly!" )
-		
+
 		return self.resource.renderHTTP_super(self.request, self)
-	
-	
-
-
