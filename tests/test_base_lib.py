3140647e92d330b2da314b489a8656891a0dceeb
Niko Wenselowski <n.wenselowski@uib.de>
2013-09-11 08:35:37 +0000

    Splitting tests into seperate modules.

--- tests/test_base_lib.py
+++ tests/test_base_lib.py
@@ -4,9 +4,7 @@
 import time
 import unittest
 
-from OPSI.Object import (OpsiError, BackendError, OpsiClient, Host,
-	getPossibleClassAttributes, OpsiConfigserver, OpsiDepotserver,
-	LocalbootProduct)
+from OPSI.Object import OpsiClient, Host
 from OPSI.Types import (forceObjectClass, forceUnicode, forceUnicodeList,
 	forceList, forceBool, forceBoolList, forceInt, forceOct,
 	forceOpsiTimestamp, forceHardwareAddress, forceHostId, forceIPAddress,
@@ -14,65 +12,6 @@ from OPSI.Types import (forceObjectClass, forceUnicode, forceUnicodeList,
 	forceFilename, forceProductVersion, forceOpsiHostKey,
 	forceInstallationStatus, forceActionRequest, forceActionProgress,
 	forceLanguageCode, forceTime, forceArchitecture, forceEmailAddress)
-
-
-class OpsiErrorTestCase(unittest.TestCase):
-	ERROR_ARGUMENT = None
-
-	def setUp(self):
-		self.error = OpsiError(self.ERROR_ARGUMENT)
-
-	def tearDown(self):
-		del self.error
-
-	def testCanBePrinted(self):
-		print(self.error)
-
-	def testCanBeCaught(self):
-		def raiseError():
-			raise self.error
-
-		self.assertRaises(OpsiError, raiseError)
-
-
-class OpsiErrorWithIntTestCase(OpsiErrorTestCase):
-	ERROR_ARGUMENT = 1
-
-
-class OpsiErrorWithBoolTestCase(OpsiErrorTestCase):
-	ERROR_ARGUMENT = True
-
-
-class OpsiErrorWithTimeTestCase(OpsiErrorTestCase):
-	ERROR_ARGUMENT = time.localtime()
-
-
-class OpsiErrorWithUnicodeStringTestCase(OpsiErrorTestCase):
-	ERROR_ARGUMENT = u'unicode string'
-
-
-class OpsiErrorWithUTF8StringTestCase(OpsiErrorTestCase):
-	ERROR_ARGUMENT = u'utf-8 string: äöüß€'.encode('utf-8')
-
-
-class OpsiErrorWithWindowsEncodedStringTestCase(OpsiErrorTestCase):
-	ERROR_ARGUMENT = u'windows-1258 string: äöüß€'.encode('windows-1258')
-
-
-class OpsiErrorWithUTF16StringTestCase(OpsiErrorTestCase):
-	ERROR_ARGUMENT = u'utf-16 string: äöüß€'.encode('utf-16'),
-
-
-class OpsiErrorWithLatin1StringTestCase(OpsiErrorTestCase):
-	ERROR_ARGUMENT = u'latin1 string: äöüß'.encode('latin-1')
-
-
-class BackendErrorTest(unittest.TestCase):
-	def testIsSubClassOfOpsiError(self):
-		def raiseError():
-			raise BackendError('Test')
-
-		self.assertRaises(OpsiError, raiseError)
 
 
 class ForceObjectClassJSONTestCase(unittest.TestCase):
@@ -364,6 +303,7 @@ class ForceInstallationStatusTestCase(unittest.TestCase):
 		self.assertRaises(ValueError, forceInstallationStatus, 'none')
 		self.assertRaises(ValueError, forceInstallationStatus, 'abc')
 
+
 class ForceActionRequestTestCase(unittest.TestCase):
 	def testForcingWithInvalidStatusRaisesExceptions(self):
 		self.assertRaises(ValueError, forceActionRequest, 'installed')
@@ -387,6 +327,7 @@ class ForceActionProgressTestCase(unittest.TestCase):
 
 	def testForcing(self):
 		self.assertEquals(forceActionProgress('installing 50%'), u'installing 50%')
+
 
 class ForceLanguageCodeTestCase(unittest.TestCase):
 	def testCasingGetsAdjusted(self):
@@ -417,132 +358,8 @@ class ForceTimeTestCase(unittest.TestCase):
 
 
 class ForceEmailAddressTestCase(unittest.TestCase):
-
 	def testForcingRequiresValidMailAddress(self):
 		self.assertRaises(ValueError, forceEmailAddress, 'infouib.de')
 
 	def testForcing(self):
 		self.assertEquals(forceEmailAddress('info@uib.de'), u'info@uib.de')
-
-
-class GetPossibleClassAttributesTestCase(unittest.TestCase):
-	def testMethod(self):
-		self.assertEquals(
-			set(getPossibleClassAttributes(Host)),
-			set(
-				[
-					'masterDepotId', 'depotLocalUrl', 'repositoryRemoteUrl',
-					'description', 'created', 'inventoryNumber', 'notes',
-					'oneTimePassword', 'isMasterDepot', 'id', 'lastSeen',
-					'maxBandwidth', 'hardwareAddress', 'networkAddress',
-					'repositoryLocalUrl', 'opsiHostKey', 'ipAddress',
-					'depotWebdavUrl', 'depotRemoteUrl', 'type'
-				]
-			)
-		)
-
-
-class OpsiConfigServerComparisonTestCase(unittest.TestCase):
-    def setUp(self):
-        self.reference = OpsiConfigserver(
-            id='configserver1.uib.local',
-            opsiHostKey='71234545689056789012123678901234',
-            depotLocalUrl='file:///opt/pcbin/install',
-            depotRemoteUrl=u'smb://configserver1/opt_pcbin/install',
-            repositoryLocalUrl='file:///var/lib/opsi/repository',
-            repositoryRemoteUrl=u'webdavs://configserver1:4447/repository',
-            description='The configserver',
-            notes='Config 1',
-            hardwareAddress=None,
-            ipAddress=None,
-            inventoryNumber='00000000001',
-            networkAddress='192.168.1.0/24',
-            maxBandwidth=10000
-        )
-
-    def tearDown(self):
-        del self.reference
-
-    def testComparingToSelf(self):
-    	obj2 = self.reference
-    	self.assertEquals(self.reference, obj2)
-
-    def testComparingToOtherObjectWithSameSettings(self):
-        obj2 = OpsiConfigserver(
-            id='configserver1.uib.local',
-            opsiHostKey='71234545689056789012123678901234',
-            depotLocalUrl='file:///opt/pcbin/install',
-            depotRemoteUrl=u'smb://configserver1/opt_pcbin/install',
-            repositoryLocalUrl='file:///var/lib/opsi/repository',
-            repositoryRemoteUrl=u'webdavs://configserver1:4447/repository',
-            description='The configserver',
-            notes='Config 1',
-            hardwareAddress=None,
-            ipAddress=None,
-            inventoryNumber='00000000001',
-            networkAddress='192.168.1.0/24',
-            maxBandwidth=10000
-        )
-
-        self.assertEquals(self.reference, obj2)
-
-    def testComparingToDepotserverFails(self):
-        obj2 = OpsiDepotserver(
-            id='depotserver1.uib.local',
-            opsiHostKey='19012334567845645678901232789012',
-            depotLocalUrl='file:///opt/pcbin/install',
-            depotRemoteUrl='smb://depotserver1.uib.local/opt_pcbin/install',
-            repositoryLocalUrl='file:///var/lib/opsi/repository',
-            repositoryRemoteUrl='webdavs://depotserver1.uib.local:4447/repository',
-            description='A depot',
-            notes='D€pot 1',
-            hardwareAddress=None,
-            ipAddress=None,
-            inventoryNumber='00000000002',
-            networkAddress='192.168.2.0/24',
-            maxBandwidth=10000
-        )
-        self.assertNotEquals(self.reference, obj2)
-
-    def testComparingToSomeDictFails(self):
-        self.assertNotEquals(self.reference, {"test": 123})
-
-
-class LocalbootProductTestCase(unittest.TestCase):
-    def testComparison(self):
-        obj1 = LocalbootProduct(
-            id                 = 'product2',
-            name               = u'Product 2',
-            productVersion     = '2.0',
-            packageVersion     = 'test',
-            licenseRequired    = False,
-            setupScript        = "setup.ins",
-            uninstallScript    = u"uninstall.ins",
-            updateScript       = "update.ins",
-            alwaysScript       = None,
-            onceScript         = None,
-            priority           = 0,
-            description        = None,
-            advice             = "",
-            productClassIds    = ['localboot-products'],
-            windowsSoftwareIds = ['{98723-7898adf2-287aab}', 'xxxxxxxx']
-        )
-        obj2 = LocalbootProduct(
-            id                 = 'product2',
-            name               = u'Product 2',
-            productVersion     = '2.0',
-            packageVersion     = 'test',
-            licenseRequired    = False,
-            setupScript        = "setup.ins",
-            uninstallScript    = u"uninstall.ins",
-            updateScript       = "update.ins",
-            alwaysScript       = None,
-            onceScript         = None,
-            priority           = 0,
-            description        = None,
-            advice             = "",
-            productClassIds    = ['localboot-products'],
-            windowsSoftwareIds = ['xxxxxxxx', '{98723-7898adf2-287aab}']
-        )
-
-        self.assertEquals(obj1, obj2)
