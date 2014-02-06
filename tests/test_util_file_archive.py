961749ecda48897dbcca60c1b554c3442554161a
Niko Wenselowski <n.wenselowski@uib.de>
2014-02-06 09:37:53 +0000

    Moving tests for archives into one file.

--- tests/test_util_file_archive.py
+++ tests/test_util_file_archive.py
@@ -3,8 +3,7 @@
 
 import unittest
 
-from OPSI.Util.File.Archive import Archive
-
+from OPSI.Util.File.Archive import Archive, PigzMixin, TarArchive
 
 
 class ArchiveFactoryTestCase(unittest.TestCase):
@@ -19,6 +18,32 @@ class ArchiveFactoryTestCase(unittest.TestCase):
     def testRaisingExceptionIfFiletypeCanNotBeDetermined(self):
         # Checking if the filetype for this python file can be guessed.
         self.assertRaises(Exception, Archive, __file__)
+
+
+class TarArchiveTestCase(unittest.TestCase):
+    def test_pigz_detection(self):
+        self.assertEqual(PigzMixin.is_pigz_available(),
+            TarArchive.is_pigz_available())
+
+
+class PigzMixinAppliedTestCase(unittest.TestCase):
+    def setUp(self):
+        class DumbArchive(PigzMixin):
+            pass
+
+        self.test_object = DumbArchive()
+
+    def tearDown(self):
+        del self.test_object
+
+    def test_having_mixin_methods(self):
+        self.assertTrue(hasattr(self.test_object, 'pigz_detected'))
+        self.assertTrue(hasattr(self.test_object, 'is_pigz_available'))
+
+    def test_mixin_methods_work(self):
+        self.assertEqual(PigzMixin.is_pigz_available(), self.test_object.pigz_detected)
+        self.assertEqual(PigzMixin.is_pigz_available(), self.test_object.is_pigz_available())
+
 
 if __name__ == '__main__':
     unittest.main()
