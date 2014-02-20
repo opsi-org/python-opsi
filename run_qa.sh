1e173fedadbe930d6ee9fdc8c85cbf9bf0dd84e7
Niko Wenselowski <n.wenselowski@uib.de>
2014-02-20 12:39:17 +0000

    Ignoring web2 and tests with flake8.

--- run_qa.sh
+++ run_qa.sh
@@ -3,10 +3,10 @@
 # This will create various files with QA information.
 # Please make sure the tools from requirements-qa.txt are installed
 echo "Running pylint"
-pylint --rcfile=pylintrc --ignore=web2 OPSI > pylint.txt || echo 'pylint did not finish with return code 0'
+pylint --rcfile=pylintrc OPSI > pylint.txt || echo 'pylint did not finish with return code 0'
 
 echo "Running flake8"
-flake8 --ignore=W191 --exclude=OPSI/web2/* OPSI/ > pep8.txt || echo 'pep8 did not finish with return code 0'
+flake8 --ignore=W191 --exclude=OPSI/web2/*,OPSI/tests/* OPSI/ > pep8.txt || echo 'pep8 did not finish with return code 0'
 
 echo "Running nosetests"
 nosetests --with-xunit --with-xcoverage --cover-package=OPSI tests/ || echo 'nosetests did not finish with return code 0'
