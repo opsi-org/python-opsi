9096956ca9e2d8b0f995f4ba52c05999e07391af
Niko Wenselowski <n.wenselowski@uib.de>
2014-02-27 21:53:54 +0000

    Seperating the testcase and the setup of the backend.
    Removing duplicate code parts.
    
    Indent with tabs.

--- tests/test_backend_extend_d_10_opsi.py
+++ tests/test_backend_extend_d_10_opsi.py
@@ -1,235 +1,169 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import grp
+from __future__ import absolute_import
+
 import os
-import pwd
-import shutil
-import tempfile
 import unittest
 
 from OPSI.Backend.BackendManager import BackendManager
 from OPSI.Object import (OpsiClient, LocalbootProduct, ProductOnClient,
-                         ProductDependency, OpsiDepotserver, ProductOnDepot,
-                         UnicodeConfig, ConfigState)
+						 ProductDependency, OpsiDepotserver, ProductOnDepot,
+						 UnicodeConfig, ConfigState)
+
+from .Backends.File import FileBackendMixin
 
 
-class BackendExtendedThroughOPSITestCase(unittest.TestCase):
-    BACKEND_SUBFOLDER = 'data'
+class ExtendedFileBackendMixin(FileBackendMixin):
+	def setUpBackend(self):
+		self._fileBackendConfig = {}
+		self._fileTempDir = self._copyOriginalBackendToTemporaryLocation()
 
-    def setUp(self):
-        self.TMP_CONFIG_DIR = self._copyOriginalBackendToTemporaryLocation()
+		self.backend = BackendManager(
+			backend='file',
+			backendconfigdir=os.path.join(self._fileTempDir, self.BACKEND_SUBFOLDER, 'backends'),
+			extensionconfigdir=os.path.join(self._fileTempDir, self.BACKEND_SUBFOLDER, 'backendManager', 'extend.d')
+		)
 
-        self.backendManager = BackendManager(
-            backend='file',
-            backendconfigdir=os.path.join(self.TMP_CONFIG_DIR, self.BACKEND_SUBFOLDER, 'backends'),
-            extensionconfigdir=os.path.join(self.TMP_CONFIG_DIR, self.BACKEND_SUBFOLDER, 'backendManager', 'extend.d')
-        )
+		self.backend.backend_createBase()
 
-        self.backendManager.backend_createBase()
 
-        self.fillBackend()
+class BackendExtendedThroughOPSITestCase(unittest.TestCase, ExtendedFileBackendMixin):
+	def setUp(self):
+		self.setUpBackend()
+		self.backendManager = self.backend
+		self.fillBackend()
 
-    @classmethod
-    def _copyOriginalBackendToTemporaryLocation(cls):
-        tempDir = tempfile.mkdtemp()
-        originalBackendDir = cls._getOriginalBackendLocation()
+	def fillBackend(self):
+		client = OpsiClient(
+			id='backend-test-1.vmnat.local',
+			description='Unittest Test client.'
+		)
 
-        shutil.copytree(originalBackendDir, os.path.join(tempDir, cls.BACKEND_SUBFOLDER))
+		depot = OpsiDepotserver(
+			id='depotserver1.some.test',
+			description='Test Depot',
+		)
 
-        cls._setupFileBackend(tempDir)
-        cls._patchDispatchConfig(tempDir)
+		self.backendManager.host_createObjects([client, depot])
 
-        return tempDir
+		firstProduct = LocalbootProduct('already_installed', '1.0', '1.0')
+		secondProduct = LocalbootProduct('to_install', '1.0', '1.0')
 
-    @staticmethod
-    def _getOriginalBackendLocation():
-        return os.path.normpath(
-            os.path.join(
-                os.path.dirname(__file__), '..', 'data'
-            )
-        )
+		prodDependency = ProductDependency(
+			productId=firstProduct.id,
+			productVersion=firstProduct.productVersion,
+			packageVersion=firstProduct.packageVersion,
+			productAction='setup',
+			requiredProductId=secondProduct.id,
+			# requiredProductVersion=secondProduct.productVersion,
+			# requiredPackageVersion=secondProduct.packageVersion,
+			requiredAction='setup',
+			requiredInstallationStatus='installed',
+			requirementType='after'
+		)
 
-    @classmethod
-    def _setupFileBackend(cls, targetDirectory):
-        cls._patchFileBackend(targetDirectory)
-        cls._createClientTemplateFolders(os.path.join(targetDirectory, 'baseDir'))
+		self.backendManager.product_createObjects([firstProduct, secondProduct])
+		self.backendManager.productDependency_createObjects([prodDependency])
 
-    @classmethod
-    def _patchFileBackend(cls, backendDirectory):
-        baseDir = os.path.join(backendDirectory, 'baseDir', 'config')
-        hostKeyDir = os.path.join(backendDirectory, 'keyFiles')
+		poc = ProductOnClient(
+			clientId=client.id,
+			productId=firstProduct.id,
+			productType=firstProduct.getType(),
+			productVersion=firstProduct.productVersion,
+			packageVersion=firstProduct.packageVersion,
+			installationStatus='installed',
+			actionResult='successful'
+		)
 
-        currentGroupId = os.getgid()
-        groupName = grp.getgrgid(currentGroupId)[0]
+		self.backendManager.productOnClient_createObjects([poc])
 
-        userName = pwd.getpwuid(os.getuid())[0]
+		firstProductOnDepot = ProductOnDepot(
+			productId=firstProduct.id,
+			productType=firstProduct.getType(),
+			productVersion=firstProduct.productVersion,
+			packageVersion=firstProduct.packageVersion,
+			depotId=depot.getId(),
+			locked=False
+		)
 
-        config_file = os.path.join(backendDirectory, cls.BACKEND_SUBFOLDER, 'backends', 'file.conf')
-        with open(config_file, 'w') as config:
-            new_configuration = """
-# -*- coding: utf-8 -*-
+		secondProductOnDepot = ProductOnDepot(
+			productId=secondProduct.id,
+			productType=secondProduct.getType(),
+			productVersion=secondProduct.productVersion,
+			packageVersion=secondProduct.packageVersion,
+			depotId=depot.getId(),
+			locked=False
+		)
 
-module = 'File'
-config = {{
-    "baseDir": u"{basedir}",
-    "hostKeyFile": u"{keydir}",
-    "fileGroupName": u"{groupName}",
-    "fileUserName": u"{userName}",
-}}
-""".format(basedir=baseDir, keydir=hostKeyDir, groupName=groupName, userName=userName)
+		self.backendManager.productOnDepot_createObjects([firstProductOnDepot, secondProductOnDepot])
 
-            config.write(new_configuration)
+		clientConfigDepotId = UnicodeConfig(
+			id=u'clientconfig.depot.id',
+			description=u'Depotserver to use',
+			possibleValues=[],
+			defaultValues=[depot.id]
+		)
 
-    @classmethod
-    def _createClientTemplateFolders(cls, targetDirectory):
-        templateDirectory = os.path.join(targetDirectory, 'config', 'templates')
-        os.makedirs(templateDirectory)
+		self.backendManager.config_createObjects(clientConfigDepotId)
 
-    @classmethod
-    def _patchDispatchConfig(cls, targetDirectory):
-        configDir = os.path.join(targetDirectory, 'data', 'backends')
+		clientDepotMappingConfigState = ConfigState(
+			configId=clientConfigDepotId.getId(),
+			objectId=client.getId(),
+			values=depot.getId()
+		)
 
-        with open(os.path.join(configDir, 'dispatch.conf'), 'w') as dpconf:
-            dpconf.write(
-"""
-.*                 : file
-"""
-)
+		self.backendManager.configState_createObjects(clientDepotMappingConfigState)
 
-    def fillBackend(self):
-        client = OpsiClient(
-            id='backend-test-1.vmnat.local',
-            description='Unittest Test client.'
-        )
+	def tearDown(self):
+		self.tearDownBackend()
 
-        depot = OpsiDepotserver(
-            id='depotserver1.some.test',
-            description='Test Depot',
-        )
+	def testBackendDoesNotCreateProductsOnClientsOnItsOwn(self):
+		pocs = self.backendManager.productOnClient_getObjects()
+		self.assertEqual(
+			1,
+			len(pocs),
+			'Expected to have only one ProductOnClient but got {n} instead: '
+			'{0}'.format(pocs, n=len(pocs))
+		)
 
-        self.backendManager.host_createObjects([client, depot])
-
-        firstProduct = LocalbootProduct('already_installed', '1.0', '1.0')
-        secondProduct = LocalbootProduct('to_install', '1.0', '1.0')
-
-        prodDependency = ProductDependency(
-            productId=firstProduct.id,
-            productVersion=firstProduct.productVersion,
-            packageVersion=firstProduct.packageVersion,
-            productAction='setup',
-            requiredProductId=secondProduct.id,
-            # requiredProductVersion=secondProduct.productVersion,
-            # requiredPackageVersion=secondProduct.packageVersion,
-            requiredAction='setup',
-            requiredInstallationStatus='installed',
-            requirementType='after'
-        )
-
-        self.backendManager.product_createObjects([firstProduct, secondProduct])
-        self.backendManager.productDependency_createObjects([prodDependency])
-
-        poc = ProductOnClient(
-            clientId=client.id,
-            productId=firstProduct.id,
-            productType=firstProduct.getType(),
-            productVersion=firstProduct.productVersion,
-            packageVersion=firstProduct.packageVersion,
-            installationStatus='installed',
-            actionResult='successful'
-        )
-
-        self.backendManager.productOnClient_createObjects([poc])
-
-        firstProductOnDepot = ProductOnDepot(
-            productId=firstProduct.id,
-            productType=firstProduct.getType(),
-            productVersion=firstProduct.productVersion,
-            packageVersion=firstProduct.packageVersion,
-            depotId=depot.getId(),
-            locked=False
-        )
-
-        secondProductOnDepot = ProductOnDepot(
-            productId=secondProduct.id,
-            productType=secondProduct.getType(),
-            productVersion=secondProduct.productVersion,
-            packageVersion=secondProduct.packageVersion,
-            depotId=depot.getId(),
-            locked=False
-        )
-
-        self.backendManager.productOnDepot_createObjects([firstProductOnDepot, secondProductOnDepot])
-
-        clientConfigDepotId = UnicodeConfig(
-            id=u'clientconfig.depot.id',
-            description=u'Depotserver to use',
-            possibleValues=[],
-            defaultValues=[depot.id]
-        )
-
-        self.backendManager.config_createObjects(clientConfigDepotId)
-
-        clientDepotMappingConfigState = ConfigState(
-            configId=clientConfigDepotId.getId(),
-            objectId=client.getId(),
-            values=depot.getId()
-        )
-
-        self.backendManager.configState_createObjects(clientDepotMappingConfigState)
-
-    def tearDown(self):
-        if os.path.exists(self.TMP_CONFIG_DIR):
-            shutil.rmtree(self.TMP_CONFIG_DIR)
-
-        del self.backendManager
-
-    def testBackendDoesNotCreateProductsOnClientsOnItsOwn(self):
-        pocs = self.backendManager.productOnClient_getObjects()
-        self.assertEqual(
-            1,
-            len(pocs),
-            'Expected to have only one ProductOnClient but got {n} instead: '
-            '{0}'.format(pocs, n=len(pocs))
-        )
-
-    def testSetProductActionRequestWithDependenciesSetsProductsToSetup(self):
-        """
-        An product action request should set product that are dependencies to \
+	def testSetProductActionRequestWithDependenciesSetsProductsToSetup(self):
+		"""
+		An product action request should set product that are dependencies to \
 setup even if they are already installed on a client.
-        """
-        self.backendManager.setProductActionRequestWithDependencies(
-            'to_install',
-            'backend-test-1.vmnat.local',
-            'setup'
-        )
+		"""
+		self.backendManager.setProductActionRequestWithDependencies(
+			'to_install',
+			'backend-test-1.vmnat.local',
+			'setup'
+		)
 
-        productsOnClient = self.backendManager.productOnClient_getObjects()
-        self.assertEqual(
-            2,
-            len(productsOnClient),
-            'Expected to have two ProductOnClients. Instead we got {n}: '
-            '{0}'.format(productsOnClient, n=len(productsOnClient))
-        )
+		productsOnClient = self.backendManager.productOnClient_getObjects()
+		self.assertEqual(
+			2,
+			len(productsOnClient),
+			'Expected to have two ProductOnClients. Instead we got {n}: '
+			'{0}'.format(productsOnClient, n=len(productsOnClient))
+		)
 
-        productThatShouldBeReinstalled = None
-        for poc in productsOnClient:
-            self.assertEqual(
-                'backend-test-1.vmnat.local',
-                poc.clientId,
-                'Wrong client id. Expected it to be "{0}" but got: '
-                '{1}'.format('backend-test-1.vmnat.local', poc.clientId)
-            )
+		productThatShouldBeReinstalled = None
+		for poc in productsOnClient:
+			self.assertEqual(
+				'backend-test-1.vmnat.local',
+				poc.clientId,
+				'Wrong client id. Expected it to be "{0}" but got: '
+				'{1}'.format('backend-test-1.vmnat.local', poc.clientId)
+			)
 
-            if poc.productId == 'already_installed':
-                productThatShouldBeReinstalled = poc
+			if poc.productId == 'already_installed':
+				productThatShouldBeReinstalled = poc
 
-        if productThatShouldBeReinstalled is None:
-            self.fail('Could not find a product "{0}" on the client.'.format('already_installed'))
+		if productThatShouldBeReinstalled is None:
+			self.fail('Could not find a product "{0}" on the client.'.format('already_installed'))
 
-        self.assertEquals(productThatShouldBeReinstalled.productId, 'already_installed')
-        self.assertEquals(productThatShouldBeReinstalled.actionRequest, 'setup')
+		self.assertEquals(productThatShouldBeReinstalled.productId, 'already_installed')
+		self.assertEquals(productThatShouldBeReinstalled.actionRequest, 'setup')
 
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
