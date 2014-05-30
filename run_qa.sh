3ea6d721a4f034b97548028827ed5224cb71077a
Niko Wenselowski <n.wenselowski@uib.de>
2014-05-30 09:20:54 +0000

    Renaming pylintrc.

--- run_qa.sh
+++ run_qa.sh
@@ -3,7 +3,7 @@
 # This will create various files with QA information.
 # Please make sure the tools from requirements-qa.txt are installed
 echo "Running pylint"
-pylint --rcfile=pylintrc OPSI > pylint.txt || echo 'pylint did not finish with return code 0'
+pylint --rcfile=.pylintrc OPSI > pylint.txt || echo 'pylint did not finish with return code 0'
 
 echo "Running flake8"
 flake8 --exit-zero OPSI/ > pep8.txt
