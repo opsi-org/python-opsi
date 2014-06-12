7f7a2f5aa05362f88f8350755aa5e8838cd2ac3b
Niko Wenselowski <n.wenselowski@uib.de>
2014-06-12 13:09:47 +0000

    First batch of tests for the SQL backend.

---
+++ tests/test_backend_sql.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+# This file is part of python-opsi.
+# Copyright (C) 2014 uib GmbH <info@uib.de>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Testing opsi SQL backend.
+
+:author: Niko Wenselowski <n.wenselowski@uib.de>
+:license: GNU Affero General Public License version 3
+"""
+
+import unittest
+
+import OPSI.Backend.SQL as sql
+
+
+class MySQLBackendWithoutConnectonTestCase(unittest.TestCase):
+    """
+    Testing the backend functions that do not require an connection
+    to an actual database.
+    """
+    def setUp(self):
+        self.backend = sql.SQLBackend()
+
+    def tearDown(self):
+        del self.backend
+
+
+class FilterToSQLTestCase(MySQLBackendWithoutConnectonTestCase):
+    def testCreatingFilter(self):
+        self.assertEquals('', self.backend._filterToSql())
+        self.assertEquals(u'(`lol` = 0)', self.backend._filterToSql({'lol': False}))
+
+    def testCreatingFilterHasParentheses(self):
+        self.assertTrue(self.backend._filterToSql({'lol': False}).startswith('('))
+        self.assertTrue(self.backend._filterToSql({'lol': False}).endswith(')'))
+
+    def testNoFilterForNoneValues(self):
+        result = self.backend._filterToSql({'a': False, 'b': None})
+        self.assertTrue('b' not in result)
+        self.assertTrue('NULL' not in result)
+        self.assertTrue('None' not in result)
+
+    def testBoolValueRepresentation(self):
+        self.assertTrue('0' in self.backend._filterToSql({'a': False}))
+        self.assertTrue('1' in self.backend._filterToSql({'a': True}))
+
+        self.assertEquals(
+            u'(`a` = 1) and (`b` = 0)',
+            self.backend._filterToSql({'a': True, 'b': False})
+        )
+
+    def testMultipleValuesAreAddedWithAnAnd(self):
+        self.assertTrue(
+            u' and ' in self.backend._filterToSql({'a': True, 'b': False})
+        )
+
+    def testNumberRepresentation(self):
+        self.assertEquals(u'(`a` = 1)', self.backend._filterToSql({'a': 1}))
+        self.assertEquals(u'(`b` = 2.3)', self.backend._filterToSql({'b': 2.3}))
+        self.assertEquals(u'(`c` = 4)', self.backend._filterToSql({'c': 4L}))
+
+
+if __name__ == '__main__':
+    unittest.main()
