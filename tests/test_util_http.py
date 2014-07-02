90a35b7cb60f59fd4ae7cecfe111cfcd10ca7efb
Niko Wenselowski <n.wenselowski@uib.de>
2014-07-02 09:20:09 +0000

    First test for OPSI.Util.HTTP

---
+++ tests/test_util_http.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+# This file is part of python-opsi.
+# Copyright (C) 2014 uib GmbH <info@uib.de>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Testing functionality of OPSI.Util.HTTP
+
+:author: Niko Wenselowski <n.wenselowski@uib.de>
+:license: GNU Affero General Public License version 3
+"""
+
+import random
+import os
+import unittest
+
+import OPSI.Util.HTTP
+
+
+class Hybi10EncodeTestCase(unittest.TestCase):
+    def testEndocingAndDecodingDoNotAlterString(self):
+        randstring = (
+            u'[{"operations": ["created", "deleted", "updated"], '
+            u'message_type": "register_for_object_events", '
+            u'"client_id": "nJ87nTA7Fph8n29C", '
+            u'"object_types": ["OpsiClient", "ProductOnClient", '
+            u'"BootConfiguration", "ProductOnDepot", "ConfigState"]}]'
+        )
+        randstring = randstring.encode('utf-8')
+        encoded = hybi10Encode(randstring)
+        decoded = hybi10Decode(encoded)
+
+        self.assertEquals(randstring, decoded)
+
+    def testEndocingAndDecodingDoNotAlterStringWithRandomInputs(self):
+        def string_generator(size):
+            return ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(size))
+
+        for i in range(1000):
+            randstring = string_generator(random.randint(1, 10000))
+            encoded = hybi10Encode(randstring)
+            decoded = hybi10Decode(encoded)
+
+            self.assertEquals(randstring, decoded)
+
+
+if __name__ == '__main__':
+    unittest.main()
