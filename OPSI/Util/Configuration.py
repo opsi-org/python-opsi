d27becf4283c0aba1a41325e52c0e19295bc9aca
Niko Wenselowski <n.wenselowski@uib.de>
2014-11-18 20:35:25 +0000

    Update header, use AGPLv3.

--- OPSI/Util/Configuration.py
+++ OPSI/Util/Configuration.py
@@ -1,24 +1,28 @@
-#!/usr/bin/python
+#! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# All rights reserved.
+# This file is part of python-opsi.
+# Copyright (C) 2013-2014 uib GmbH <info@uib.de>
 
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 2 as
-# published by the Free Software Foundation.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
 
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU Affero General Public License for more details.
 
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Configuration util.
 
-#   @copyright:
-#   @author: Christian Kampka <c.kampka@uib.de>
-#   @license: GNU General Public License version 2
+:author: Christian Kampka <c.kampka@uib.de>
+:author: Niko Wenselowski <n.wenselowski@uib.de>
+:license: GNU Affero General Public License version 3
+"""
 
 import os
 import sys
