531b25f9bd5041b214496396bb75593ca4b76c84
Niko Wenselowski <n.wenselowski@uib.de>
2015-01-23 16:27:24 +0000

    Only one statement per line and no unneeded parens.

--- OPSI/Backend/SQL.py
+++ OPSI/Backend/SQL.py
@@ -1243,8 +1243,10 @@ class SQLBackend(ConfigDataBackend):
 					modules[module] = True
 			else:
 				val = modules[module]
-				if (val == False): val = 'no'
-				if (val == True):  val = 'yes'
+				if val == False:
+					val = 'no'
+				elif val == True:
+					val = 'yes'
 			data += u'%s = %s\r\n' % (module.lower().strip(), val)
 		if not bool(publicKey.verify(md5(data).digest(), [long(modules['signature'])])):
 			logger.error(u"Failed to verify modules signature")
