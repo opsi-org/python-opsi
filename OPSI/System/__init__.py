a75d4447410ddd2aaef801de0f1ad8bbfef26399
Niko Wenselowski <n.wenselowski@uib.de>
2015-03-19 11:27:27 +0000

    Less wildcard importing.
    References #1127

--- OPSI/System/__init__.py
+++ OPSI/System/__init__.py
@@ -38,9 +38,12 @@ if os.name == 'posix':
 elif os.name == 'nt':
 	from Windows import *
 
-from OPSI.Types import *
+from OPSI.Types import (forceArchitecture, forceBool, forceDict, forceDomain,
+	forceFilename, forceFloat, forceFqdn, forceHostname, forceInt,
+	forceIPAddress, forceList, forceNetmask, forceOct, forceTime, forceUnicode,
+	forceUrl)
 
-__version__ = '4.0.6.1'
+__version__ = '4.0.6.3'
 
 
 class SystemHook(SystemSpecificHook):
