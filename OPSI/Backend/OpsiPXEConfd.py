6aab6049a25ec0f6f22d2e88faecb6c23449bd52
Niko Wenselowski <n.wenselowski@uib.de>
2015-03-19 10:10:41 +0000

    Adding missing imports.

--- OPSI/Backend/OpsiPXEConfd.py
+++ OPSI/Backend/OpsiPXEConfd.py
@@ -31,9 +31,10 @@ import threading
 import time
 
 from OPSI.Logger import Logger
+from OPSI.Objects import OpsiClient
 from OPSI.Types import forceInt, forceUnicode, forceHostId
 from OPSI.Types import BackendMissingDataError
-from OPSI.Backend.Backend import ConfigDataBackend, JSONRPCBackend
+from OPSI.Backend.Backend import OPSI_GLOBAL_CONF, ConfigDataBackend
 from OPSI.Backend.JSONRPC import JSONRPCBackend
 from OPSI.Util import getfqdn
 
