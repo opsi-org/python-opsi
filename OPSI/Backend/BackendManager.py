b89e835e68b4a0c97908b499ce34282041b010c1
Niko Wenselowski <n.wenselowski@uib.de>
2015-03-24 11:54:15 +0000

    Metadata bump.

--- OPSI/Backend/BackendManager.py
+++ OPSI/Backend/BackendManager.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # This file is part of python-opsi.
-# Copyright (C) 2006-2014 uib GmbH <info@uib.de>
+# Copyright (C) 2006-2015 uib GmbH <info@uib.de>
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -58,7 +58,7 @@ elif os.name == 'nt':
 	import win32net
 	import win32security
 
-__version__ = '4.0.6.2'
+__version__ = '4.0.6.3'
 
 logger = Logger()
 
