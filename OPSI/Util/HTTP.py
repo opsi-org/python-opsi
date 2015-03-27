ff05e5e72d1de2ab1bdcb69294783b07d2e539c1
Niko Wenselowski <n.wenselowski@uib.de>
2015-03-27 10:06:51 +0000

    Updated module header.
    Also: GPLv3

--- OPSI/Util/HTTP.py
+++ OPSI/Util/HTTP.py
@@ -1,37 +1,30 @@
-#!/usr/bin/python
+#! /usr/bin/env python
 # -*- coding: utf-8 -*-
+
+# This module is part of the desktop management solution opsi
+# Based on urllib3
+# (open pc server integration) http://www.opsi.org
+# Copyright (C) 2010 Andrey Petrov
+# Copyright (C) 2010-2015 uib GmbH <info@uib.de>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-   = = = = = = = = = = = = = = = = = =
-   =   opsi python library - HTTP    =
-   = = = = = = = = = = = = = = = = = =
+opsi python library - HTTP
 
-   This module is part of the desktop management solution opsi
-   Based on urllib3
-   (open pc server integration) http://www.opsi.org
-
-   Copyright (C) 2010 Andrey Petrov
-   Copyright (C) 2010-2014 uib GmbH
-
-   http://www.uib.de/
-
-   All rights reserved.
-
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License version 2 as
-   published by the Free Software Foundation.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
-   @copyright:	uib GmbH <info@uib.de>
-   @author: Jan Schneider <j.schneider@uib.de>
-   @license: GNU General Public License version 2
+:author: Jan Schneider <j.schneider@uib.de>
+:author: Niko Wenselowski <n.wenselowski@uib.de>
+:license: GNU Affero General Public License version 3
 """
 
 import base64
@@ -50,6 +43,7 @@ from OPSI.Types import forceBool, forceFilename, forceInt, forceUnicode, forceUn
 from OPSI.Types import OpsiTimeoutError, OpsiServiceVerificationError
 from OPSI.Logger import LOG_DEBUG, LOG_INFO, Logger
 from OPSI.Util import encryptWithPublicKeyFromX509CertificatePEMFile, randomString
+
 logger = Logger()
 
 
