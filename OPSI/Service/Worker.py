590dca2129d007b65f7c7fb4b5f9d263b9990a36
Niko Wenselowski <n.wenselowski@uib.de>
2015-04-27 08:41:32 +0000

    Easier to understand code.

--- OPSI/Service/Worker.py
+++ OPSI/Service/Worker.py
@@ -645,8 +645,9 @@ class MultiprocessWorkerOpsiJsonRpc(WorkerOpsiJsonRpc):
 				contentEncoding = self.request.headers.getHeader('content-encoding')[0].lower()
 			except Exception:
 				pass
-			d = self._callInstance.processQuery(self.query,
-				(contentEncoding == 'gzip') or (contentType and contentType.mediaType.startswith('gzip')))
+
+			gzipEnabled = (contentEncoding == 'gzip') or (contentType and contentType.mediaType.startswith('gzip'))
+			d = self._callInstance.processQuery(self.query, gzipEnabled)
 			d.addCallback(processResult)
 			return d
 
