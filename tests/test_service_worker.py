226314cd95c154e83907b33ec9db223d2de75631
Niko Wenselowski <n.wenselowski@uib.de>
2015-06-01 08:42:25 +0000

    Better tests.

--- tests/test_service_worker.py
+++ tests/test_service_worker.py
@@ -44,6 +44,14 @@ class FakeRequest(object):
 		self.headers = headers or FakeHeader()
 
 
+class FakeRPC(object):
+			def __init__(self, result=None):
+				self.result = result or None
+
+			def getResponse(self):
+				return self.result
+
+
 class WorkerOpsiJsonRpcTestCase(unittest.TestCase):
 
 	def setUp(self):
@@ -66,6 +74,53 @@ class WorkerOpsiJsonRpcTestCase(unittest.TestCase):
 		self.assertFalse(result.headers.hasHeader('content-encoding'))
 		self.assertEquals(['application/json;charset=utf-8'], result.headers.getRawHeaders('content-type'))
 		self.assertEquals('null', str(result.stream.read()))
+
+	def testHandlingMultipleRPCs(self):
+		"""
+		With multiple RPCs the results are returned in a list.
+
+		We do not use any compression in this testcase.
+		"""
+		class FakeRPC(object):
+			def __init__(self, result=None):
+				self.result = result or None
+
+			def getResponse(self):
+				return self.result
+
+		self.worker._rpcs = [FakeRPC(), FakeRPC(1), FakeRPC(u"FÄKE!"),
+							FakeRPC({"Narziss": "Morgen Nicht Geboren"})]
+
+		result = self.worker._generateResponse(None)
+		self.assertTrue(200, result.code)
+		self.assertTrue(result.headers.hasHeader('content-type'))
+		self.assertFalse(result.headers.hasHeader('content-encoding'))
+		self.assertEquals(['application/json;charset=utf-8'], result.headers.getRawHeaders('content-type'))
+		self.assertEquals('[null, 1, "F\xc3\x84KE!", {"Narziss": "Morgen Nicht Geboren"}]', str(result.stream.read()))
+
+	def testHandlingSingleResult(self):
+		"""
+		A single RPC result must not be returned in a list.
+		"""
+		self.worker._rpcs = [FakeRPC("Hallo Welt")]
+		result = self.worker._generateResponse(None)
+		self.assertTrue(200, result.code)
+		self.assertTrue(result.headers.hasHeader('content-type'))
+		self.assertFalse(result.headers.hasHeader('content-encoding'))
+		self.assertEquals(['application/json;charset=utf-8'], result.headers.getRawHeaders('content-type'))
+		self.assertEquals('"Hallo Welt"', str(result.stream.read()))
+
+	def testHandlingSingleResultConsistingOfList(self):
+		"""
+		If a single result is made the result is a list this list must not be unpacked.
+		"""
+		self.worker._rpcs = [FakeRPC(["Eins", "Zwei", "Drei"])]
+		result = self.worker._generateResponse(None)
+		self.assertTrue(200, result.code)
+		self.assertTrue(result.headers.hasHeader('content-type'))
+		self.assertFalse(result.headers.hasHeader('content-encoding'))
+		self.assertEquals(['application/json;charset=utf-8'], result.headers.getRawHeaders('content-type'))
+		self.assertEquals('["Eins", "Zwei", "Drei"]', str(result.stream.read()))
 
 
 if __name__ == '__main__':
