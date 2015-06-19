17338129e6db1f01f6b76af6753641dd88a67601
Niko Wenselowski <n.wenselowski@uib.de>
2015-06-19 15:31:35 +0000

    A little bit of test refactoring.

--- tests/test_acl.py
+++ tests/test_acl.py
@@ -234,138 +234,167 @@ class ACLTestCase(unittest.TestCase, HostsMixin, ProductsOnClientsMixin):
 
     def test_get_access_full(self):
         with getSQLiteBackend() as backend:
-            exBackend = ExtendedConfigDataBackend(backend)
-            self.backend = exBackend
+            self.backend = ExtendedConfigDataBackend(backend)
             self.setUpHosts()
             self.createHostsOnBackend()
 
             backend = BackendAccessControl(
-                    backend  = self.backend,
-                    username = self.configserver1.id,
-                    password = self.configserver1.opsiHostKey,
-                    acl      = [
-                        ['.*', [
+                backend=self.backend,
+                username=self.configserver1.id,
+                password=self.configserver1.opsiHostKey,
+                acl=[
+                        ['.*',
+                            [
                                 {'type': u'opsi_depotserver', 'ids': [], 'denyAttributes': [], 'allowAttributes': []}
-                            ]
                         ]
                     ]
+                ]
             )
+
             hosts = backend.host_getObjects()
-            self.assertEqual(len(self.hosts), len(hosts), u"Expected %s hosts, but got '%s' from backend" % (len(self.hosts), len(hosts)))
+            self.assertEqual(len(self.hosts), len(hosts),
+                u"Expected %s hosts, but got '%s' from backend" % (len(self.hosts), len(hosts))
+            )
+
             for host in hosts:
                 for h in self.hosts:
-                    if (h.id != host.id):
+                    if h.id != host.id:
                         continue
-                    self.assertEqual(h.opsiHostKey, host.opsiHostKey, u"Expected opsi host key %s, but got '%s' from backend" % (h.opsiHostKey, host.opsiHostKey))
+                    self.assertEqual(h.opsiHostKey, host.opsiHostKey,
+                        u"Expected opsi host key %s, but got '%s' from backend" % (h.opsiHostKey, host.opsiHostKey)
+                    )
 
-    def test_get_access_self(self):
+    def testOnlyAccessingSelfIsPossible(self):
         with getSQLiteBackend() as backend:
-            exBackend = ExtendedConfigDataBackend(backend)
-            self.backend = exBackend
+            self.backend = ExtendedConfigDataBackend(backend)
             self.setUpHosts()
             self.createHostsOnBackend()
 
             backend = BackendAccessControl(
-                    backend  = self.backend,
-                    username = self.configserver1.id,
-                    password = self.configserver1.opsiHostKey,
-                    acl      = [
-                        ['.*', [
+                backend=self.backend,
+                username=self.configserver1.id,
+                password=self.configserver1.opsiHostKey,
+                acl=[
+                        ['.*',
+                            [
                                 {'type': u'self', 'ids': [], 'denyAttributes': [], 'allowAttributes': []}
-                            ]
                         ]
                     ]
+                ]
             )
-            hosts = backend.host_getObjects()
-            self.assertEqual(1, len(hosts), u"Expected %s hosts, but found '%s' on backend" % (1, len(hosts)))
 
-    def test_get_access_deny_attributes(self):
+            hosts = backend.host_getObjects()
+            self.assertEqual(1, len(hosts),
+                u"Expected %s hosts, but found '%s' on backend" % (1, len(hosts))
+            )
+
+    def testDenyingAccessToSpecifiedAttributes(self):
         with getSQLiteBackend() as backend:
-            exBackend = ExtendedConfigDataBackend(backend)
-            self.backend = exBackend
+            self.backend = ExtendedConfigDataBackend(backend)
             self.setUpHosts()
             self.createHostsOnBackend()
 
-            denyAttributes = ['opsiHostKey', 'description']
+            denyAttributes = set(['opsiHostKey', 'description'])
             backend = BackendAccessControl(
-                    backend  = self.backend,
-                    username = self.configserver1.id,
-                    password = self.configserver1.opsiHostKey,
-                    acl      = [
-                        ['.*', [
+                backend=self.backend,
+                username=self.configserver1.id,
+                password=self.configserver1.opsiHostKey,
+                acl=[
+                        ['.*',
+                            [
                                 {'type': u'opsi_depotserver', 'ids': [], 'denyAttributes': denyAttributes, 'allowAttributes': []}
-                            ]
                         ]
                     ]
+                ]
             )
+
             hosts = backend.host_getObjects()
-            self.assertEqual(len(self.hosts), len(hosts), u"Expected %s hosts, but got '%s' from backend" % (len(self.hosts), len(hosts)))
+            self.assertEqual(len(self.hosts), len(hosts),
+                u"Expected %s hosts, but got '%s' from backend" % (len(self.hosts), len(hosts))
+            )
+
             for host in hosts:
-                for (attribute, value) in host.toHash().items():
+                for attribute, value in host.toHash().items():
                     if attribute in denyAttributes:
-                        self.assertEqual(value, None, u"Expected attribute '%s' to be None, but got '%s' from backend" % (attribute, value))
+                        self.assertEqual(value, None,
+                            u"Expected attribute '%s' to be None, but got '%s' from backend" % (attribute, value)
+                        )
 
     def test_get_access_allow_attributes(self):
         with getSQLiteBackend() as backend:
-            exBackend = ExtendedConfigDataBackend(backend)
-            self.backend = exBackend
+            self.backend = ExtendedConfigDataBackend(backend)
             self.setUpHosts()
             self.createHostsOnBackend()
 
-            allowAttributes = ['type', 'id', 'description', 'notes']
+            allowAttributes = set(['type', 'id', 'description', 'notes'])
             backend = BackendAccessControl(
-                    backend  = self.backend,
-                    username = self.configserver1.id,
-                    password = self.configserver1.opsiHostKey,
-                    acl      = [
-                        ['.*', [
+                backend=self.backend,
+                username=self.configserver1.id,
+                password=self.configserver1.opsiHostKey,
+                acl=[
+                        ['.*',
+                            [
                                 {'type': u'opsi_depotserver', 'ids': [], 'denyAttributes': [], 'allowAttributes': allowAttributes}
-                            ]
                         ]
                     ]
+                ]
             )
+
             hosts = backend.host_getObjects()
-            self.assertEqual(len(self.hosts), len(hosts), u"Expected %s hosts, but got '%s' from backend" % (len(self.hosts), len(hosts)))
+            self.assertEqual(len(self.hosts), len(hosts),
+                u"Expected %s hosts, but got '%s' from backend" % (len(self.hosts), len(hosts))
+            )
+
             for host in hosts:
-                for (attribute, value) in host.toHash().items():
+                for attribute, value in host.toHash().items():
                     if attribute not in allowAttributes:
-                        self.assertEqual(value, None, u"Expected attribute '%s' to be None, but got '%s' from backend" % (attribute, value))
+                        self.assertEqual(value, None,
+                            u"Expected attribute '%s' to be None, but got '%s' from backend" % (attribute, value)
+                        )
 
     def test_get_access_deny_attributes_and_self(self):
         with getSQLiteBackend() as backend:
-            exBackend = ExtendedConfigDataBackend(backend)
-            self.backend = exBackend
+            self.backend = ExtendedConfigDataBackend(backend)
             self.setUpHosts()
             self.createHostsOnBackend()
 
-            denyAttributes = ['opsiHostKey', 'description']
+            denyAttributes = set(['opsiHostKey', 'description'])
             backend = BackendAccessControl(
-                    backend  = self.backend,
-                    username = self.configserver1.id,
-                    password = self.configserver1.opsiHostKey,
-                    acl      = [
-                        ['.*', [
+                backend=self.backend,
+                username=self.configserver1.id,
+                password=self.configserver1.opsiHostKey,
+                acl=[
+                        ['.*',
+                            [
                                 {'type': u'opsi_depotserver', 'ids': [], 'denyAttributes': denyAttributes, 'allowAttributes': []},
                                 {'type': u'self', 'ids': [], 'denyAttributes': [], 'allowAttributes': []}
-                            ]
                         ]
                     ]
+                ]
             )
+
             hosts = backend.host_getObjects()
-            self.assertEqual(len(self.hosts), len(hosts), u"Expected %s hosts, but got '%s' from backend" % (len(self.hosts), len(hosts)))
+            self.assertEqual(len(self.hosts), len(hosts),
+                u"Expected %s hosts, but got '%s' from backend" % (len(self.hosts), len(hosts))
+            )
+
             for host in hosts:
-                if (host.id == self.configserver1.id):
-                    self.assertEqual(self.configserver1.opsiHostKey, host.opsiHostKey, u"Expected opsi host key %s, but got '%s' from backend" % (self.configserver1.opsiHostKey, host.opsiHostKey))
+                if host.id == self.configserver1.id:
+                    self.assertEqual(self.configserver1.opsiHostKey, host.opsiHostKey,
+                        u"Expected opsi host key %s, but got '%s' from backend" % (self.configserver1.opsiHostKey, host.opsiHostKey)
+                    )
                 else:
-                    for (attribute, value) in host.toHash().items():
+                    for attribute, value in host.toHash().items():
                         if attribute in denyAttributes:
-                            self.assertEqual(value, None, u"Expected attribute '%s' to be None, but got '%s' from backend" % (attribute, value))
+                            self.assertEqual(value, None,
+                                u"Expected attribute '%s' to be None, but got '%s' from backend" % (attribute, value)
+                            )
 
-
+    unittest.skipIf(not os.path.exists('/etc/opsi/modules'),
+        "Test requires valid modules file")
     def test_access_self_productOnClients(self):
         with getSQLiteBackend() as backend:
-            exBackend = ExtendedConfigDataBackend(backend)
-            self.backend = exBackend
+            self.backend = ExtendedConfigDataBackend(backend)
             self.setUpHosts()
             self.setUpProductOnClients()
 
@@ -374,37 +403,38 @@ class ACLTestCase(unittest.TestCase, HostsMixin, ProductsOnClientsMixin):
             self.backend.productOnClient_createObjects(self.productOnClients)
 
             for client in self.clients:
-                if (client.id == self.productOnClients[0].clientId):
+                if client.id == self.productOnClients[0].clientId:
                     break
 
             backend = BackendAccessControl(
-                    backend  = self.backend,
-                    username = client.id,
-                    password = client.opsiHostKey,
-                    acl      = [
-                        ['.*', [
+                backend=self.backend,
+                username=client.id,
+                password=client.opsiHostKey,
+                acl=[
+                        ['.*',
+                            [
                                 {'type': u'self', 'ids': [], 'denyAttributes': [], 'allowAttributes': []}
-                            ]
                         ]
                     ]
+                ]
             )
+
             productOnClients = backend.productOnClient_getObjects()
             for productOnClient in productOnClients:
-                self.assertEqual(client.id, productOnClient.clientId, u"Expected client id %s in productOnClient, but got client id '%s'" % (client.id, productOnClient.clientId))
+                self.assertEqual(client.id, productOnClient.clientId,
+                    u"Expected client id %s in productOnClient, but got client id '%s'" % (client.id, productOnClient.clientId)
+                )
 
             otherClientId = None
             for c in self.clients:
-                if (client.id != c.id):
+                if client.id != c.id:
                     otherClientId = c.id
                     break
 
             productOnClient = productOnClients[0].clone()
             productOnClient.clientId = otherClientId
-            try:
-                backend.productOnClient_updateObjects(productOnClient)
-                self.fail("Successfuly inserted productOnClient %s as user %s into backend. Access should have been denied." % (productOnClient, client.id))
-            except Exception, e:
-                pass
+
+            self.assertRaises(Exception, backend.productOnClient_updateObjects, productOnClient)
 
 
 if __name__ == '__main__':
