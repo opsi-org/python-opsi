3c8113186ec26929eed16fdb0276410b7243a589
Mathias Radtke <m.radtke@uib.de>
2015-07-22 13:26:07 +0000

    - added test to check if 'admin users' is avail. in opsi_depot share

--- tests/test_util_task_samba.py
+++ tests/test_util_task_samba.py
@@ -286,11 +286,38 @@ class SambaConfigureTest(unittest.TestCase):
 							break
 
 			self.assertTrue(found, 'Missing Admin Users in Share opsi_depot')
-			
 
+	def testCorrectOpsiDepotShareSamba4(self):
+		with workInTemporaryDirectory() as tempDir:
+			PathToSmbConf = os.path.join(tempDir, 'SMB_CONF')
+			with open(PathToSmbConf, 'w') as fakeSambaConfig:
+				fakeSambaConfig.write(u"[opsi_depot]\n")
+				fakeSambaConfig.write(u"   available = yes\n")
+				fakeSambaConfig.write(u"   comment = opsi depot share (ro)\n")
+				fakeSambaConfig.write(u"   path = /var/lib/opsi/depot\n")
+				fakeSambaConfig.write(u"   oplocks = no\n")
+				fakeSambaConfig.write(u"   follow symlinks = yes\n")
+				fakeSambaConfig.write(u"   level2 oplocks = no\n")
+				fakeSambaConfig.write(u"   writeable = no\n")
+				fakeSambaConfig.write(u"   invalid users = root\n")
 
+			with mock.patch('OPSI.Util.Task.Samba.isSamba4', lambda:True):
+				with mock.patch('OPSI.Util.Task.Samba.os.mkdir'):
+					Samba.configureSamba(PathToSmbConf)
 
-
+			with open(PathToSmbConf, 'r') as fakeSambaConfig:
+				found = False
+				opsi_depot = False
+				for line in fakeSambaConfig:
+					line = line.strip()
+					if '[opsi_depot]' in line:
+						opsi_depot = True
+					elif opsi_depot and 'admin users' in line:
+						break
+					elif opsi_depot and line.startswith('['):
+						opsi_depot = False
+				else:
+					self.fail('Did not find "admin users" in opsi_depot share')
 
 def main():
 	unittest.main()
