6cf5264ae44ac56c40fffaadb2ddf0f7763956c2
Mathias Radtke <m.radtke@uib.de>
2015-07-29 11:00:56 +0000

    - added header in Samba

--- OPSI/Util/Task/Samba.py
+++ OPSI/Util/Task/Samba.py
@@ -1,6 +1,27 @@
-#! /usr/bin/env python
-# Copyright (C) 2015 uib GmbH - http://www.uib.de
-# All rights reserved.
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+# This file is part of python-opsi.
+# Copyright (C) 2015 uib GmbH <info@uib.de>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Function and classes for Samba
+
+:author: Mathias Radtke <m.radtke@uib.de>
+:license: GNU Affero General Public License version 3
+"""
 
 import codecs
 import os
