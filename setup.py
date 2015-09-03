2a0f016a10e600991a0b6024b1ffb44b857389c5
Niko Wenselowski <n.wenselowski@uib.de>
2015-09-03 09:05:37 +0000

    Updated translations.

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ opsi python library - setup file
 from setuptools import setup, find_packages
 import os
 
-LANGUAGES = ['de', 'fr', 'da']
+LANGUAGES = ['da', 'de', 'es', 'fr', 'it', 'ru']
 
 VERSION = None
 with open(os.path.join("debian", "changelog")) as changelog:
