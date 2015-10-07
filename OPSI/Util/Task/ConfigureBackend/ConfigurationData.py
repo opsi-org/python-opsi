e572b219a79b95b25e0bebf66011d49565ce232b
Niko Wenselowski <n.wenselowski@uib.de>
2015-10-07 14:44:01 +0000

    The config for 'clientconfig.depot.drive' now also has the drives 'a:' and 'b:' present if it is created anew.

--- OPSI/Util/Task/ConfigureBackend/ConfigurationData.py
+++ OPSI/Util/Task/ConfigureBackend/ConfigurationData.py
@@ -140,9 +140,10 @@ default. Supply this if ``clientconfig.configserver.url`` or \
 				id=u'clientconfig.depot.drive',
 				description=u'Drive letter for depot share',
 				possibleValues=[
-					u'c:', u'd:', u'e:', u'f:', u'g:', u'h:', u'i:', u'j:',
-					u'k:', u'l:', u'm:', u'n:', u'o:', u'p:', u'q:', u'r:',
-					u's:', u't:', u'u:', u'v:', u'w:', u'x:', u'y:', u'z:',
+					u'a:', u'b:', u'c:', u'd:', u'e:', u'f:', u'g:', u'h:',
+					u'i:', u'j:', u'k:', u'l:', u'm:', u'n:', u'o:', u'p:',
+					u'q:', u'r:', u's:', u't:', u'u:', u'v:', u'w:', u'x:',
+					u'y:', u'z:',
 					u'dynamic'
 				],
 				defaultValues=[u'p:'],
