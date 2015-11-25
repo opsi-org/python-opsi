bdbf951a5c739eb2124e9fdb900bb2d6eb411b3b
Niko Wenselowski <n.wenselowski@uib.de>
2015-11-25 16:02:06 +0000

    Providing a test for backend cleaning.

---
+++ tests/test_util_task_cleanup_backend.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of python-opsi.
+# Copyright (C) 2015 uib GmbH <info@uib.de>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Testing backend cleaning.
+
+:author: Niko Wenselowski <n.wenselowski@uib.de>
+:license: GNU Affero General Public License version 3
+"""
+
+from __future__ import absolute_import
+
+import unittest
+
+from OPSI.Util.Task.CleanupBackend import cleanupBackend
+
+from .Backends import getTestBackend
+from .test_backend_replicator import fillBackend, checkIfBackendIsFilled
+
+
+class CleanupBackendTestCase(unittest.TestCase):
+    # TODO: we need checks to see what get's removed and what not.
+    # TODO: we also should provide some senseless data that will be removed!
+
+    def testCleanupBackend(self):
+        with getTestBackend(extended=True) as backend:
+            fillBackend(backend)
+
+            cleanupBackend(backend)
+            checkIfBackendIsFilled(backend)
+
+
+if __name__ == '__main__':
+    unittest.main()
