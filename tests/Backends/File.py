cf80c2c61b1fd8c6d6cfa50a697021f82ed1ea39
Niko Wenselowski <n.wenselowski@uib.de>
2015-12-04 10:44:32 +0000

    Will be solved differently in the future.

--- tests/Backends/File.py
+++ tests/Backends/File.py
@@ -50,10 +50,6 @@ class FileBackendMixin(BackendMixin):
         self._fileTempDir = self._copyOriginalBackendToTemporaryLocation()
 
         self.backend = ExtendedConfigDataBackend(FileBackend(**self._fileBackendConfig))
-        # TODO: Make use of a BackendManager Backend.
-        # This is to easily check if we have a file backend in the tests.
-        # With such a check we can easily skip tests.
-
         self.backend.backend_createBase()
 
     def _copyOriginalBackendToTemporaryLocation(self):
