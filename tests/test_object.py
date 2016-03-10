a3028b72723b2c5c7a1b13afbd8612db276640e5
Niko Wenselowski <n.wenselowski@uib.de>
2016-03-10 15:54:45 +0100

    Simplifying test.

--- tests/test_object.py
+++ tests/test_object.py
@@ -328,30 +328,27 @@ class MandatoryConstructorArgsTestCase(unittest.TestCase):
 
 
 class ProductTestCase(unittest.TestCase):
-    def setUp(self):
-        self.product = Product(
+
+    def testLongNameCanBeSetAndRead(self):
+        """
+        Namens with a length of more than 128 characters can are supported.
+        """
+        product = Product(
             id='new_prod',
             name='New Product for Tests',
             productVersion='1.0',
             packageVersion='1.0'
         )
 
-    def tearDown(self):
-        del self.product
-
-    def testLongNameCanBeSetAndRead(self):
-        """
-        Namens with a length of more than 128 characters can are supported.
-        """
         newName = (
             u'This is a very long name with 128 characters to test the '
             u'creation of long product names that should work now but '
             u'were limited b4'
         )
 
-        self.product.setName(newName)
+        product.setName(newName)
 
-        nameFromProd = self.product.getName()
+        nameFromProd = product.getName()
 
         self.assertEqual(newName, nameFromProd)
         self.assertEqual(128, len(nameFromProd))
