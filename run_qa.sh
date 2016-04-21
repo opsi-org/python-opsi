f53a2aa798a704502618c6abc436b5811a150531
Niko Wenselowski <n.wenselowski@uib.de>
2016-04-21 14:25:38 +0200

    Running tests by calling the script for testing.

--- run_qa.sh
+++ run_qa.sh
@@ -3,11 +3,7 @@
 # This will create various files with QA information.
 # Please make sure the tools from requirements-qa.txt are installed
 echo "Running tests"
-if [ -e coverage.xml ]; then
-	rm coverage.xml
-fi
-
-py.test --junitxml=testreport.xml --cov-config .coveragerc --cov OPSI --cov-report xml --quiet tests/
+./run_tests.sh
 
 echo "Running pylint"
 pylint --rcfile=.pylintrc OPSI > pylint.txt || echo 'pylint did not finish with return code 0'
