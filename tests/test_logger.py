1b2e206bd246d73903eb3dfc6df293dc24a1c520
Niko Wenselowski <der@nik0.de>
2016-04-23 18:48:54 +0200

    Fixing the test.

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -190,7 +190,7 @@ def testLogLevelIsShownInOutput(logger, message, logLevel):
 			value = messageBuffer.getvalue()
 			assert value.startswith("[{0:d}]".format(logLevel))
 			assert message in value
-			assert value.endswith('message')
+			assert value.endswith(message)
 
 
 def testConfidentialStringsAreNotLogged(logger):
