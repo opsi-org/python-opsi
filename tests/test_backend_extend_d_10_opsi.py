1a1d61785116d04636e03cf7a8caa70460da9e08
Niko Wenselowski <n.wenselowski@uib.de>
2016-06-29 11:31:37 +0200

    Refactoring the client / depot setup into a function.

--- tests/test_backend_extend_d_10_opsi.py
+++ tests/test_backend_extend_d_10_opsi.py
@@ -40,34 +40,7 @@ def prefilledBackendManager(backendManager):
 
 
 def fillBackend(backend):
-	client = OpsiClient(
-		id='backend-test-1.vmnat.local',
-		description='Unittest Test client.'
-	)
-
-	depot = OpsiDepotserver(
-		id='depotserver1.some.test',
-		description='Test Depot',
-	)
-
-	backend.host_createObjects([client, depot])
-
-	clientConfigDepotId = UnicodeConfig(
-		id=u'clientconfig.depot.id',
-		description=u'Depotserver to use',
-		possibleValues=[],
-		defaultValues=[depot.id]
-	)
-
-	backend.config_createObjects(clientConfigDepotId)
-
-	clientDepotMappingConfigState = ConfigState(
-		configId=clientConfigDepotId.getId(),
-		objectId=client.getId(),
-		values=depot.getId()
-	)
-
-	backend.configState_createObjects(clientDepotMappingConfigState)
+	client, depot = createClientAndDepot(backend)
 
 	firstProduct = LocalbootProduct('to_install', '1.0', '1.0')
 	secondProduct = LocalbootProduct('already_installed', '1.0', '1.0')
@@ -119,6 +92,39 @@ def fillBackend(backend):
 	)
 
 	backend.productOnDepot_createObjects([firstProductOnDepot, secondProductOnDepot])
+
+
+def createClientAndDepot(backend):
+	client = OpsiClient(
+		id='backend-test-1.vmnat.local',
+		description='Unittest Test client.'
+	)
+
+	depot = OpsiDepotserver(
+		id='depotserver1.some.test',
+		description='Test Depot',
+	)
+
+	backend.host_createObjects([client, depot])
+
+	clientConfigDepotId = UnicodeConfig(
+		id=u'clientconfig.depot.id',
+		description=u'Depotserver to use',
+		possibleValues=[],
+		defaultValues=[depot.id]
+	)
+
+	backend.config_createObjects(clientConfigDepotId)
+
+	clientDepotMappingConfigState = ConfigState(
+		configId=clientConfigDepotId.getId(),
+		objectId=client.getId(),
+		values=depot.getId()
+	)
+
+	backend.configState_createObjects(clientDepotMappingConfigState)
+
+	return client, depot
 
 
 def testBackendDoesNotCreateProductsOnClientsOnItsOwn(prefilledBackendManager):
