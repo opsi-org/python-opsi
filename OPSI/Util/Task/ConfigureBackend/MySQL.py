cf88ce1221cfad55669205d989abb35d5d4a5fc8
Mathias Radtke <m.radtke@uib.de>
2016-07-04 12:18:33 +0200

    added missing "'"

--- OPSI/Util/Task/ConfigureBackend/MySQL.py
+++ OPSI/Util/Task/ConfigureBackend/MySQL.py
@@ -134,7 +134,7 @@ def initializeDatabase(dbAdminUser, dbAdminPass, config,
 		db.query(u'USE {database};'.format(**config))
 		db.query(
 			(
-				u'GRANT ALL ON {1[database]}.* TO {1[username]}@\'{0}\'
+				u'GRANT ALL ON {1[database]}.* TO {1[username]}@\'{0}\''
 				u'IDENTIFIED BY \'{1[password]}\'').format(
 					host,
 					config,
