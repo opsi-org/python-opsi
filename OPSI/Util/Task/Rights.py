f8a4bb2d2b4a0d70d5cf5515c5dd4814e291ae2c
Niko Wenselowski <n.wenselowski@uib.de>
2016-07-19 14:34:35 +0200

    Avoid duplicate output of path.

--- OPSI/Util/Task/Rights.py
+++ OPSI/Util/Task/Rights.py
@@ -133,7 +133,7 @@ def setRights(path=u'/'):
 			continue
 
 		LOGGER.notice(u"Setting rights on directory {0!r}", startPath)
-		LOGGER.debug2(u"Current setting: startPath={path}, uid={uid}, gid={gid}", path=startPath, uid=rights.uid, gid=rights.gid)
+		LOGGER.debug2(u"Current setting: uid={uid}, gid={gid}", uid=rights.uid, gid=rights.gid)
 		chown(startPath, rights.uid, rights.gid)
 		os.chmod(startPath, rights.directories)
 		for filepath in findFiles(startPath, prefix=startPath, returnLinks=rights.correctLinks, excludeFile=re.compile("(.swp|~)$")):
