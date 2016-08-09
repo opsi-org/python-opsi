184a7aac9af5841d30c939aaaeec9c8a962efe70
Niko Wenselowski <der@nik0.de>
2016-08-09 23:58:21 +0200

    Migrating DHCPDConf-Tests.

--- tests/test_backend_dhcpd.py
+++ tests/test_backend_dhcpd.py
@@ -34,39 +34,6 @@ from OPSI.Types import BackendIOError
 from .Backends.DHCPD import DHCPDConfMixin
 
 
-class DHCPDConfFileTestCase(unittest.TestCase, DHCPDConfMixin):
-    def setUp(self):
-        self.setUpDHCPDConf()
-
-    def tearDown(self):
-        self.tearDownDHCPDConf()
-
-    def testAddingHostsToConfig(self):
-        """
-        Adding hosts to a DHCPDConf.
-
-        If this fails on your machine with a message that 127.x.x.x is refused
-        as network address please correct your hostname settings.
-        """
-        dhcpdConf = self.dhcpdConf
-        dhcpdConf.parse()
-
-        dhcpdConf.addHost('TestclienT', '0001-21-21:00:00', '192.168.99.112', '192.168.99.112', None)
-        dhcpdConf.addHost('TestclienT2', '00:01:09:08:99:11', '192.168.99.113', '192.168.99.113', {"next-server": "192.168.99.2", "filename": "linux/pxelinux.0/xxx?{}"})
-
-        self.assertNotEqual(None, dhcpdConf.getHost('TestclienT2'))
-        self.assertEqual(None, dhcpdConf.getHost('notthere'))
-
-    def testGeneratingConfig(self):
-        dhcpdConf = self.dhcpdConf
-        dhcpdConf.parse()
-
-        dhcpdConf.addHost('TestclienT', '0001-21-21:00:00', '192.168.99.112', '192.168.99.112', None)
-        dhcpdConf.addHost('TestclienT2', '00:01:09:08:99:11', '192.168.99.113', '192.168.99.113', {"next-server": "192.168.99.2", "filename": "linux/pxelinux.0/xxx?{}"})
-
-        dhcpdConf.generate()
-
-
 class DHCPBackendTestCase(unittest.TestCase, DHCPDConfMixin):
     def setUp(self):
         self.setUpDHCPDConf()
