fed60fa065c917a8aaeb898fa2847bd17f472e6e
Niko Wenselowski <n.wenselowski@uib.de>
2016-09-09 14:20:38 +0200

    Use the correct version.

--- OPSI/System/Posix.py
+++ OPSI/System/Posix.py
@@ -55,7 +55,7 @@ from OPSI.Types import OpsiVersionError
 from OPSI.Object import *
 from OPSI.Util import objectToBeautifiedText, removeUnit
 
-__version__ = '4.0.7.16'
+__version__ = '4.0.7.20'
 
 logger = Logger()
 
