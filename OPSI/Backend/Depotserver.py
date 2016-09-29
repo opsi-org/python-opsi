7865eb606bf321721ac15238acb7919d3fe2b0d1
Niko Wenselowski <n.wenselowski@uib.de>
2016-09-29 18:18:09 +0200

    Releasing this as 4.0.7.25.

--- OPSI/Backend/Depotserver.py
+++ OPSI/Backend/Depotserver.py
@@ -41,7 +41,7 @@ from OPSI.Util import (compareVersions, getfqdn, md5sum, librsyncSignature,
 	librsyncPatchFile, librsyncDeltaFile, removeDirectory)
 from OPSI.Util.File import ZsyncFile
 
-__version__ = '4.0.7.22'
+__version__ = '4.0.7.25'
 
 logger = Logger()
 
