65054414720943acbb6b63ccdc151ba190064982
Niko Wenselowski <n.wenselowski@uib.de>
2016-12-06 09:46:55 +0100

    Better module description.

--- tests/test_backend_backend.py
+++ tests/test_backend_backend.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-Testing basic backends.
+Testing basic backend functionality.
 
 :author: Niko Wenselowski <n.wenselowski@uib.de>
 :license: GNU Affero General Public License version 3
