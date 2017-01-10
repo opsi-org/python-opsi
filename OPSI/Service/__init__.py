d49242d125c185e4590202b035c2d8b862fd1dec
Niko Wenselowski <n.wenselowski@uib.de>
2017-01-10 13:54:31 +0100

    Add documentation.

--- OPSI/Service/__init__.py
+++ OPSI/Service/__init__.py
@@ -35,13 +35,30 @@ logger = Logger()
 
 
 class SSLContext(object):
-	def __init__(self, sslServerKeyFile, sslServerCertFile, acceptedCiphers=None):
+	def __init__(self, sslServerKeyFile, sslServerCertFile, acceptedCiphers=''):
+		"""
+		Create a context for the usage of SSL in twisted.
+
+		:param sslServerCertFile: Path to the certificate file.
+		:type sslServerCertFile: str
+		:param sslServerKeyFile: Path to the key file.
+		:type sslServerKeyFile: str
+		:param acceptedCiphers: A string defining what ciphers should \
+be accepted. Please refer to the OpenSSL documentation on how such a \
+string should be composed. No limitation will be done if an empty value \
+is set.
+		:type acceptedCiphers: str
+		"""
 		self._sslServerKeyFile = sslServerKeyFile
 		self._sslServerCertFile = sslServerCertFile
 		self._acceptedCiphers = acceptedCiphers
 
 	def getContext(self):
-		''' Create an SSL context. '''
+		'''
+		Create an SSL context.
+
+		:returntype: OpenSSL.SSL.Context
+		'''
 
 		# Test if server certificate and key file exist.
 		if not os.path.isfile(self._sslServerKeyFile):
