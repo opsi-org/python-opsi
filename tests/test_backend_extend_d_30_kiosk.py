56ff682e2b0f796c18915d8ece99cb9a10686700
Niko Wenselowski <n.wenselowski@uib.de>
2017-04-24 17:44:29 +0200

    Adding a first test.

---
+++ tests/test_backend_extend_d_30_kiosk.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# This file is part of python-opsi.
+# Copyright (C) 2017 uib GmbH <info@uib.de>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Tests for the kiosk client method.
+
+:author: Niko Wenselowski <n.wenselowski@uib.de>
+:license: GNU Affero General Public License version 3
+"""
+
+import pytest
+
+from OPSI.Types import BackendMissingDataError
+
+
+def testGettingInfoForNonExistingClient(backendManager):
+    with pytest.raises(BackendMissingDataError):
+        backendManager.getKioskProductInfosForClient('foo.bar.baz')
