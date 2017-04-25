dcec9fd9a84d4128a4c316bde22f7cfa2c3035d3
Niko Wenselowski <n.wenselowski@uib.de>
2017-04-25 18:38:29 +0200

    Update header.

--- OPSI/Util/AMP.py
+++ OPSI/Util/AMP.py
@@ -1,33 +1,32 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+# This module is part of the desktop management solution opsi
+
+# (open pc server integration) http://www.opsi.org
+
+# Copyright (C) 2010 Andrey Petrov
+# Copyright (C) 2010-2017 uib GmbH
+
+# http://www.uib.de/
+
+# All rights reserved.
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 """
-   This module is part of the desktop management solution opsi
-
-   (open pc server integration) http://www.opsi.org
-
-   Copyright (C) 2010 Andrey Petrov
-   Copyright (C) 2010-2016 uib GmbH
-
-   http://www.uib.de/
-
-   All rights reserved.
-
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License version 2 as
-   published by the Free Software Foundation.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
-   @copyright:	uib GmbH <info@uib.de>
-   @author: Christian Kampka <c.kampka@uib.de>
-   @license: GNU General Public License version 2
+:copyright:	uib GmbH <info@uib.de>
+:author: Christian Kampka <c.kampka@uib.de>
+:license: GNU General Public License version 2
 """
 
 import os
