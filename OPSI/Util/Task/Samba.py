474a9b8703979b75a39a92983a38bd563590c251
Niko Wenselowski <n.wenselowski@uib.de>
2017-06-28 15:36:58 +0200

    Better description.

--- OPSI/Util/Task/Samba.py
+++ OPSI/Util/Task/Samba.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-Function and classes for Samba
+Configuration of Samba for the use with opsi.
 
 :author: Mathias Radtke <m.radtke@uib.de>
 :author: Niko Wenselowski <n.wenselowski@uib.de>
