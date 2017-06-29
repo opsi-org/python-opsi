816a32da8fb512bbe168c1b720382a3af6bce447
Niko Wenselowski <n.wenselowski@uib.de>
2017-06-29 15:35:39 +0200

    Always warn about an old depot directory.

--- OPSI/Util/Task/InitializeBackend.py
+++ OPSI/Util/Task/InitializeBackend.py
@@ -165,10 +165,11 @@ def _setupDepotDirectory():
 	if not os.path.exists(depotDir):
 		try:
 			os.mkdir(depotDir)
-			if os.path.exists("/opt/pcbin/install"):
-				logger.warning(u"You have an old depot configuration. Using /opt/pcbin/install is depracted, please use /var/lib/opsi/depot instead.")
 		except Exception as error:
 			logger.warning(u"Failed to create depot directory '%s': %s" % (depotDir, error))
+
+	if os.path.exists("/opt/pcbin/install"):
+		logger.warning(u"You have an old depot configuration. Using /opt/pcbin/install is depracted, please use /var/lib/opsi/depot instead.")
 
 
 def _setupWorkbenchDirectory():
