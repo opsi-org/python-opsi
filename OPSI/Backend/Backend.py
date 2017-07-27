816a8166d3876a61d1aa0a61b9f1baf11bd7c77b
Niko Wenselowski <n.wenselowski@uib.de>
2017-07-27 14:19:54 +0200

    Releasing this as 4.0.7.45-1.

--- OPSI/Backend/Backend.py
+++ OPSI/Backend/Backend.py
@@ -65,7 +65,7 @@ from OPSI.Util import (blowfishEncrypt, blowfishDecrypt, compareVersions,
 from OPSI.Util.File import ConfigFile
 import OPSI.SharedAlgorithm
 
-__version__ = '4.0.7.28'
+__version__ = '4.0.7.45'
 
 logger = Logger()
 OPSI_VERSION_FILE = u'/etc/opsi/version'
