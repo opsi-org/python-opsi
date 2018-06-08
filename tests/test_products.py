c1b18664d90c6095dabd8df78b22451ed535af15
Niko Wenselowski <n.wenselowski@uib.de>
2018-06-08 10:58:18 +0200

    Having only one test and letting the fixture change between ascii and unicode.

--- tests/test_products.py
+++ tests/test_products.py
@@ -643,46 +643,26 @@ def testLongProductName(extendedConfigDataBackend):
     assert newName == backendProduct.name
 
 
-@pytest.fixture(scope='session')
-def asciiChangelog():
-    changelog = '''opsi-winst/opsi-script (4.11.5.13) stable; urgency=low
+@pytest.fixture(
+    scope='session',
+    params=[False, True],
+    ids=['ascii', 'unicode'])
+def changelog(request):
+    if request.param:
+        # unicode
+        changelog = u'''opsi-winst/opsi-script (4.11.5.13) stable; urgency=low
 
 * do not try to run non existing external sub sections
+* Jetzt ausf\xfchren oder sp\xe4ter
 
 -- Detlef Oertel <d.oertel@uib.de>  Thu,  21 Aug 2015:15:00:00 +0200
 
 '''
-
-    changelog = changelog * 555
-
-    assert len(changelog.strip()) > 65535  # Limit for `TEXT` in MySQL / MariaDB
-
-    return asciiChangelog
-
-
-@pytest.mark.requiresModulesFile  # because of SQLite backend...
-def testLongChangelogOnProductCanBeHandled(extendedConfigDataBackend, asciiChangelog):
-    product = LocalbootProduct(id='freiheit', productVersion=1, packageVersion=1)
-    product.setChangelog(asciiChangelog)
-    assert product.getChangelog() == asciiChangelog
-
-    extendedConfigDataBackend.product_createObjects(product)
-
-    productFromBackend = extendedConfigDataBackend.product_getObjects(id=product.id)[0]
-    changelogFromBackend = productFromBackend.getChangelog()
-
-    assert len(changelogFromBackend) > 1
-    assert len(changelogFromBackend) > 63000  # Leaving some room...
-
-    assert asciiChangelog[:2048] == changelogFromBackend[:2048]
-
-
-@pytest.fixture(scope='session')
-def unicodeChangelog():
-    changelog = u'''opsi-winst/opsi-script (4.11.5.13) stable; urgency=low
+    else:
+        # ASCII
+        changelog = '''opsi-winst/opsi-script (4.11.5.13) stable; urgency=low
 
 * do not try to run non existing external sub sections
-* Jetzt ausf\xfchren oder sp\xe4ter
 
 -- Detlef Oertel <d.oertel@uib.de>  Thu,  21 Aug 2015:15:00:00 +0200
 
@@ -696,10 +676,10 @@ def unicodeChangelog():
 
 
 @pytest.mark.requiresModulesFile  # because of SQLite backend...
-def testLongUnicodeChangelogOnProductCanBeHandled(extendedConfigDataBackend, unicodeChangelog):
+def testLongChangelogOnProductCanBeHandled(extendedConfigDataBackend, changelog):
     product = LocalbootProduct(id='freiheit', productVersion=1, packageVersion=1)
-    product.setChangelog(unicodeChangelog)
-    assert product.getChangelog() == unicodeChangelog
+    product.setChangelog(changelog)
+    assert product.getChangelog() == changelog
 
     extendedConfigDataBackend.product_createObjects(product)
 
@@ -709,7 +689,7 @@ def testLongUnicodeChangelogOnProductCanBeHandled(extendedConfigDataBackend, uni
     assert len(changelogFromBackend) > 1
     assert len(changelogFromBackend) > 63000  # Leaving some room...
 
-    assert unicodeChangelog[:2048] == changelogFromBackend[:2048]
+    assert changelog[:2048] == changelogFromBackend[:2048]
 
 
 @pytest.mark.requiresModulesFile  # because of SQLite backend...
