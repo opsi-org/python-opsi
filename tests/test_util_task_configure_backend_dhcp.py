0e2f98da8926826cf2af8918e5c521cd97ba502d
Niko Wenselowski <der@nik0.de>
2018-07-04 14:03:11 +0200

    Mark another test as endless.

--- tests/test_util_task_configure_backend_dhcp.py
+++ tests/test_util_task_configure_backend_dhcp.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # This file is part of python-opsi.
-# Copyright (C) 2015-2017 uib GmbH <info@uib.de>
+# Copyright (C) 2015-2018 uib GmbH <info@uib.de>
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -26,6 +26,8 @@ from __future__ import absolute_import
 
 import os
 from contextlib import contextmanager
+
+import pytest
 
 import OPSI.Util.Task.ConfigureBackend as backendConfigUtils
 from OPSI.Util import md5sum
@@ -81,6 +83,7 @@ subnet 192.168.0.0 netmask 255.255.0.0 {
         assert any("next-server" in line for line in target), "next-server not fonud in new file."
 
 
+@pytest.mark.endless
 def testConfiguringDHCPDBackendWithEmptyFile(tempDir):
     filename = 'dhcpd_test.conf'
     with open(filename, 'x'):
