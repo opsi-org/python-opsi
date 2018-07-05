40e8bff09121beb28a6493274425ea9cb3ce4966
Niko Wenselowski <n.wenselowski@uib.de>
2018-07-05 14:43:15 +0200

    We need bytes.

--- OPSI/Util/File/Opsi/__init__.py
+++ OPSI/Util/File/Opsi/__init__.py
@@ -1271,7 +1271,7 @@ element of the tuple is replace with the second element.
 		info = tarfile.TarInfo(name="%s/checksums" % self.CONTROL_DIR)
 		info.size = size
 
-		self.addfile(info, string)
+		self.addfile(info, string.encode())
 
 	def _addSysInfoFile(self):
 		string = StringIO()
