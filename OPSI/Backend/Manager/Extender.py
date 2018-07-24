091f0911eb40d46481fe6f181936066381627219
Niko Wenselowski <n.wenselowski@uib.de>
2018-07-24 13:54:07 +0200

    Change to the Python 3 attributes.

--- OPSI/Backend/Manager/Extender.py
+++ OPSI/Backend/Manager/Extender.py
@@ -75,7 +75,7 @@ class BackendExtender(ExtendedBackend):
 				if methodName.startswith('_'):
 					continue
 				logger.debug2(u"Extending {0} with instancemethod: {1!r}", self._backend.__class__.__name__, methodName)
-				new_function = types.FunctionType(functionRef.func_code, functionRef.func_globals, functionRef.func_code.co_name)
+				new_function = types.FunctionType(functionRef.__code__, functionRef.__globals__, functionRef.__code__.__name__)
 				new_method = types.MethodType(new_function, self)
 				setattr(self, methodName, new_method)
 
