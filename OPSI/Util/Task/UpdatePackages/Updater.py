286bf96ce9197ef4e88bba769da1172ed33d66d5
Niko Wenselowski <n.wenselowski@uib.de>
2018-07-30 16:42:18 +0200

    Change docstring.

--- OPSI/Util/Task/UpdatePackages/Updater.py
+++ OPSI/Util/Task/UpdatePackages/Updater.py
@@ -18,7 +18,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-Components for updating packages.
+Component for handling packages updates.
 
 :copyright: uib GmbH <info@uib.de>
 :author: Niko Wenselowski <n.wenselowski@uib.de>
