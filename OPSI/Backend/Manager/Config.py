2ac2f155f9c8fafc2f33cbda7b6c871ab01f97b3
Niko Wenselowski <der@nik0.de>
2018-08-22 11:38:10 +0200

    Use tabs.

--- OPSI/Backend/Manager/Config.py
+++ OPSI/Backend/Manager/Config.py
@@ -31,24 +31,24 @@ from OPSI.Exceptions import BackendConfigurationError
 
 
 def loadBackendConfig(path):
-    """
-    Load the backend configuration at `path`.
-    :param path: Path to the configuration file to load.
-    :type path: str
-    :rtype: dict
-    """
-    if not os.path.exists(path):
-        raise BackendConfigurationError(u"Backend config file '%s' not found" % path)
+	"""
+	Load the backend configuration at `path`.
+	:param path: Path to the configuration file to load.
+	:type path: str
+	:rtype: dict
+	"""
+	if not os.path.exists(path):
+		raise BackendConfigurationError(u"Backend config file '%s' not found" % path)
 
-    moduleGlobals = {
-        'config': {},  # Will be filled after loading
-        'module': '',  # Will be filled after loading
-        'os': os,
-        'socket': socket,
-        'sys': sys,
-    }
+	moduleGlobals = {
+		'config': {},  # Will be filled after loading
+		'module': '',  # Will be filled after loading
+		'os': os,
+		'socket': socket,
+		'sys': sys,
+	}
 
-    with open(path) as configFile:
-        exec(configFile.read(), moduleGlobals)
+	with open(path) as configFile:
+		exec(configFile.read(), moduleGlobals)
 
-    return moduleGlobals
+	return moduleGlobals
