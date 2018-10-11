500611f6539ed292f7b7f3f204080859890c236c
Niko Wenselowski <n.wenselowski@uib.de>
2018-10-11 14:00:07 +0200

    Even more raw strings.

--- OPSI/Util/File/__init__.py
+++ OPSI/Util/File/__init__.py
@@ -670,7 +670,7 @@ class InfFile(ConfigFile):
 	hdaudioDeviceRegex = re.compile(r'HDAUDIO\\\.*VEN_([\da-fA-F]+)&DEV_([\da-fA-F]+)', re.IGNORECASE)
 	usbDeviceRegex = re.compile(r'USB.*VID_([\da-fA-F]+)&PID_([\da-fA-F]+)', re.IGNORECASE)
 	acpiDeviceRegex = re.compile(r'ACPI\\(\S+)_-_(\S+)', re.IGNORECASE)
-	varRegex = re.compile(r'\%([^\%]+)\%')
+	varRegex = re.compile(r'%([^%]+)%')
 	classRegex = re.compile(r'class\s*=')
 
 	def __init__(self, filename, lockFailTimeout=2000):
