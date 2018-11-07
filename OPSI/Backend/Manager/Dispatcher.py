dc9bf4648b3a98dd5a2ab47f99ca1f796f8c4301
Niko Wenselowski <der@nik0.de>
2018-11-07 13:46:17 +0100

    Remove leftover BackendExtender.
    Imports should import from OPSI.Backend.Manager.Extender.

--- OPSI/Backend/Manager/Dispatcher.py
+++ OPSI/Backend/Manager/Dispatcher.py
@@ -24,8 +24,6 @@ Dispatching backend calls to multiple backends.
 :license: GNU Affero General Public License version 3
 """
 
-from __future__ import absolute_import
-
 import importlib
 import inspect
 import os
@@ -33,13 +31,12 @@ import re
 import types
 
 from OPSI.Backend.Base import (
-	Backend, ConfigDataBackend, ExtendedBackend, getArgAndCallString)
+	Backend, ConfigDataBackend, getArgAndCallString)
 from OPSI.Exceptions import BackendConfigurationError
 from OPSI.Logger import Logger
 from OPSI.Types import forceList
 from OPSI.Util.File.Opsi import BackendDispatchConfigFile
 
-from .AccessControl import BackendAccessControl
 from .Config import loadBackendConfig
 
 __all__ = ('BackendDispatcher', )
@@ -225,62 +222,3 @@ class BackendDispatcher(Backend):
 
 	def dispatcher_getBackendNames(self):
 		return self._backends.keys()
-
-
-class BackendExtender(ExtendedBackend):
-	def __init__(self, backend, **kwargs):
-		if not isinstance(backend, ExtendedBackend) and not isinstance(backend, BackendDispatcher):
-			if not isinstance(backend, BackendAccessControl) or (not isinstance(backend._backend, ExtendedBackend) and not isinstance(backend._backend, BackendDispatcher)):
-				raise TypeError("BackendExtender needs instance of ExtendedBackend or BackendDispatcher as backend, got %s" % backend.__class__.__name__)
-
-		ExtendedBackend.__init__(self, backend, overwrite=kwargs.get('overwrite', True))
-
-		self._extensionConfigDir = None
-		self._extensionClass = None
-
-		for (option, value) in kwargs.items():
-			option = option.lower()
-			if option == 'extensionconfigdir':
-				self._extensionConfigDir = value
-			elif option == 'extensionclass':
-				self._extensionClass = value
-
-		self.__createExtensions()
-
-	def __createExtensions(self):
-		if self._extensionClass:
-			for methodName, functionRef in inspect.getmembers(self._extensionClass, inspect.isfunction):
-				if methodName.startswith('_'):
-					continue
-				logger.debug2(u"Extending {0} with instancemethod: {1!r}", self._backend.__class__.__name__, methodName)
-				new_function = types.FunctionType(functionRef.__code__, functionRef.__globals__, functionRef.__name__)
-				new_method = types.MethodType(new_function, self)
-				setattr(self, methodName, new_method)
-
-		if self._extensionConfigDir:
-			if not os.path.exists(self._extensionConfigDir):
-				logger.error(u"No extensions loaded: extension directory {0!r} does not exist".format(self._extensionConfigDir))
-				return
-
-			try:
-				confFiles = (
-					os.path.join(self._extensionConfigDir, filename)
-					for filename in sorted(os.listdir(self._extensionConfigDir))
-					if filename.endswith('.conf')
-				)
-
-				for confFile in confFiles:
-					try:
-						logger.info(u"Reading config file '%s'" % confFile)
-						with open(confFile) as confFileHandle:
-							exec(confFileHandle.read())
-					except Exception as execError:
-						logger.logException(execError)
-						raise RuntimeError(u"Error reading file {0!r}: {1}".format(confFile, execError))
-
-					for key, val in locals().items():
-						if isinstance(val, types.FunctionType):   # TODO: find a better way
-							logger.debug2(u"Extending %s with instancemethod: '%s'" % (self._backend.__class__.__name__, key))
-							setattr(self, key, types.MethodType(val, self))
-			except Exception as error:
-				raise BackendConfigurationError(u"Failed to read extensions from '%s': %s" % (self._extensionConfigDir, error))
