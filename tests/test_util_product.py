1a920653fd12ff7ae2360affca0df4b58182d769
Niko Wenselowski <n.wenselowski@uib.de>
2018-11-22 12:17:17 +0100

    Import mock from our helper module.

--- tests/test_util_product.py
+++ tests/test_util_product.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # This file is part of python-opsi.
-# Copyright (C) 2014-2017 uib GmbH <info@uib.de>
+# Copyright (C) 2014-2018 uib GmbH <info@uib.de>
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -24,14 +24,13 @@ Testing the OPSI.Util.Product module.
 
 from __future__ import absolute_import
 
-import mock
 import os
 import re
 import tempfile
 
 import OPSI.Util.Product as Product
 
-from .helpers import cd
+from .helpers import cd, mock
 
 import pytest
 
