491766af7fd67abf98cf35bb69deaa6d809d0998
Niko Wenselowski <n.wenselowski@uib.de>
2019-02-01 11:19:51 +0100

    Added basic handling for an .opsirc file.

---
+++ OPSI/Util/File/Opsi/Opsirc.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+
+# This module is part of the desktop management solution opsi
+# (open pc server integration) http://www.opsi.org
+# Copyright (C) 2019 uib GmbH <info@uib.de>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Handling an .opsirc file.
+
+An .opsirc file contains information about how to connect to an
+opsi API.
+
+An example::
+
+	address = https://opsimain.domain.local:4447/rpc
+	username = myname
+	password = topsecret
+
+
+None of these settings are mandatory.
+
+The file should be encoded as utf-8.
+
+:copyright: uib GmbH <info@uib.de>
+:author: Niko Wenselowski <n.wenselowski@uib.de>
+:license: GNU Affero General Public License version 3
+"""
+
+import codecs
+import os
+
+from OPSI.Logger import Logger
+from OPSI.Types import forceUnicode, forceUrl
+
+
+logger = Logger()
+
+
+def readOpsirc(filename):
+	"""
+	Read the configuration file and parse it for usable information.
+
+	:param filename: The path of the file to read.
+	:type filename: str
+	:returns: Settings read from the file. Possible keys are `username`,\
+`password` and `address`.
+	:rtype: {str: str}
+	"""
+	if not os.path.exists(filename):
+		logger.debug(u".opsirc file {} does not exist.".format(filename))
+		return {}
+
+	return _parseConfig(filename)
+
+
+def _parseConfig(filename):
+	config = {}
+	with codecs.open(filename, mode='r', encoding='utf-8') as opsircfile:
+		for line in opsircfile:
+			line = line.strip()
+			if line.startswith(('#', ';')) or not line:
+				continue
+
+			try:
+				key, value = line.split('=', 1)
+			except ValueError:
+				logger.debug2(u"Unable to split line {!r}".format(line))
+				continue
+
+			key = key.strip()
+			value = value.strip()
+
+			if key == 'address':
+				config[key] = forceUrl(value)
+			elif key in ('username', 'password'):
+				config[key] = forceUnicode(value)
+			else:
+				logger.debug(u"Ignoring unknown key {}".format(key))
+
+	return config
