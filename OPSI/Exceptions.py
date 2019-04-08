0e2b78efe415d495e5f54c62fb48ba7487647954
Niko Wenselowski <n.wenselowski@uib.de>
2019-04-08 14:25:22 +0200

    Make sure that we are compatible to the basic Exception.

--- OPSI/Exceptions.py
+++ OPSI/Exceptions.py
@@ -50,6 +50,7 @@ class OpsiError(Exception):
 	ExceptionShortDescription = "Opsi error"
 
 	def __init__(self, message=''):
+		super().__init__(message)
 		self._message = forceUnicode(message)
 
 	def __str__(self):
