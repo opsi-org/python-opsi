14bb0cb476b9c8f716446b19631d9eea7d16fea1
Niko Wenselowski <n.wenselowski@uib.de>
2019-04-30 13:12:32 +0200

    Make the comparison independent from casing.

--- OPSI/Util/File/Opsi/__init__.py
+++ OPSI/Util/File/Opsi/__init__.py
@@ -1494,7 +1494,7 @@ element of the tuple is replace with the second element.
 							currentError = p.stderr.readline().strip()
 							if currentError:
 								lastErrors.append(currentError)
-								if "Using a password on the command line interface can be insecure." not in currentError:
+								if "using a password on the command line interface can be insecure." not in currentError.lower():
 									collectedErrors.append(currentError)
 						except Exception:
 							continue
