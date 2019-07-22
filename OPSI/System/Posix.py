f0f2f66d22ad598c13b7d27a9a2552ad0a379a77
Niko Wenselowski <n.wenselowski@uib.de>
2019-07-22 11:47:25 +0200

    Read only the required parts.

--- OPSI/System/Posix.py
+++ OPSI/System/Posix.py
@@ -3011,7 +3011,10 @@ class Distribution(object):
 		self.distribution, self._version, self.id = distribution_information
 		self.distribution = self.distribution.strip()
 
-		osType, self.hostname, self.kernel, self.detailedVersion, self.arch, processor = platform.uname()
+		self.hostname = platform.node()
+		self.kernel = platform.release()
+		self.detailedVersion = platform.version()
+		self.arch = platform.machine()
 
 		self.distributor = self._getDistributor()
 
