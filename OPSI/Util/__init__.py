521e45fb40f2be9219647b4c1a555d19b33c364f
Niko Wenselowski <der@nik0.de>
2019-09-04 18:27:30 +0200

    We want our key.

--- OPSI/Util/__init__.py
+++ OPSI/Util/__init__.py
@@ -553,6 +553,9 @@ def blowfishEncrypt(key, cleartext):
 	:raises BlowfishError: In case things go wrong.
 	:rtype: str
 	"""
+	if not key:
+		raise BlowfishError("Missing key")
+
 	cleartext = forceUnicode(cleartext)
 	key = forceUnicode(key).encode()
 
@@ -581,6 +584,9 @@ def blowfishDecrypt(key, crypt):
 	:raises BlowfishError: In case things go wrong.
 	:rtype: str
 	"""
+	if not key:
+		raise BlowfishError("Missing key")
+
 	key = forceUnicode(key).encode()
 	crypt = bytes.fromhex(crypt)
 
