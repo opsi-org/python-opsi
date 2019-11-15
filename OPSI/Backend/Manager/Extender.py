8c3d0395a67f37210a57abdd58f8c04d35001fe5
Niko Wenselowski <der@nik0.de>
2019-11-15 13:55:28 +0100

    Correct reference.

--- OPSI/Backend/Manager/Extender.py
+++ OPSI/Backend/Manager/Extender.py
@@ -101,7 +101,7 @@ class BackendExtender(ExtendedBackend):
 @lru_cache(maxsize=None)
 def _getExtensionFiles(directory):
 	if not os.path.exists(directory):
-		logger.error(u"No extensions loaded: extension directory {0!r} does not exist".format(self._extensionConfigDir))
+		logger.error(u"No extensions loaded: extension directory {0!r} does not exist".format(directory))
 		return
 
 	return [
