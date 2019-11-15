56401723a6e6fa31e388dacaa6cc7b9d0242f190
Niko Wenselowski <der@nik0.de>
2019-11-15 13:56:10 +0100

    Consistent return type.

--- OPSI/Backend/Manager/Extender.py
+++ OPSI/Backend/Manager/Extender.py
@@ -99,10 +99,10 @@ class BackendExtender(ExtendedBackend):
 
 
 @lru_cache(maxsize=None)
-def _getExtensionFiles(directory):
+def _getExtensionFiles(directory) -> list:
 	if not os.path.exists(directory):
 		logger.error(u"No extensions loaded: extension directory {0!r} does not exist".format(directory))
-		return
+		return []
 
 	return [
 		os.path.join(directory, filename)
