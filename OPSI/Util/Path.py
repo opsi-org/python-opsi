5210ba58d2cc9e4e0e736320ed1629e87ccc5ad1
Niko Wenselowski <der@nik0.de>
2019-12-02 17:48:28 +0100

    Indetation should be done with tabs.

--- OPSI/Util/Path.py
+++ OPSI/Util/Path.py
@@ -32,11 +32,11 @@ from contextlib import contextmanager
 
 @contextmanager
 def cd(path: str):
-    'Change the current directory to `path` as long as the context exists.'
+	'Change the current directory to `path` as long as the context exists.'
 
-    currentDir = os.getcwd()
-    os.chdir(path)
-    try:
-        yield
-    finally:
-        os.chdir(currentDir)
+	currentDir = os.getcwd()
+	os.chdir(path)
+	try:
+		yield
+	finally:
+		os.chdir(currentDir)
