473b799ac405eaf55a781b54158f09ae59006ff6
Jan Schneider <j.schneider@uib.de>
2020-03-11 20:32:34 +0100

    Move user specific values from BackendAccessControl to UserStore which allows to store these values in a session

--- OPSI/Backend/Manager/AccessControl.py
+++ OPSI/Backend/Manager/AccessControl.py
@@ -29,6 +29,7 @@ import inspect
 import os
 import re
 import types
+from typing import List
 from functools import lru_cache
 
 from OPSI.Backend.Base import (
@@ -56,10 +57,18 @@ elif os.name == 'nt':
 	from .Authentication.NT import authenticate
 	from .Authentication.NT import readGroups
 
-__all__ = ('BackendAccessControl', )
+__all__ = ('BackendAccessControl', 'BackendAccessControlManager')
 
 logger = Logger()
 
+class UserStore:
+	def __init__(self):
+		self.username = None
+		self.password = None
+		self.userGroups = set()
+		self.forceGroups = None
+		self.host = None
+		self.authenticated = False
 
 class BackendAccessControl:
 
@@ -67,23 +76,14 @@ class BackendAccessControl:
 
 		self._backend = backend
 		self._context = backend
-		self._username = None
-		self._password = None
 		self._acl = None
 		self._aclFile = None
-		self._userGroups = set()
-		self._forceGroups = None
-		self._host = None
-		self._authenticated = False
 		self._pamService = None
-
+		self._user_store = UserStore()
+		
+		kwargs = {k.lower(): v for k, v in kwargs.items()}
 		for (option, value) in kwargs.items():
-			option = option.lower()
-			if option == 'username':
-				self._username = value
-			elif option == 'password':
-				self._password = value
-			elif option == 'acl':
+			if option == 'acl':
 				self._acl = value
 			elif option == 'aclfile':
 				self._aclFile = value
@@ -92,61 +92,18 @@ class BackendAccessControl:
 				self._pamService = value
 			elif option in ('context', 'accesscontrolcontext'):
 				self._context = value
-			elif option == 'forcegroups':
-				if value is not None:
-					self._forceGroups = forceUnicodeList(value)
+			elif option in ('user_store', 'userstore'):
+				self._user_store = value
 
-		if not self._username:
-			raise BackendAuthenticationError(u"No username specified")
-		if not self._password:
-			raise BackendAuthenticationError(u"No password specified")
 		if not self._backend:
 			raise BackendAuthenticationError(u"No backend specified")
 		if isinstance(self._backend, BackendAccessControl):
 			raise BackendConfigurationError(u"Cannot use BackendAccessControl instance as backend")
-
-		try:
-			if re.search(r'^[^.]+\.[^.]+\.\S+$', self._username):
-				# Username starts with something like hostname.domain.tld:
-				# Assuming it is a host passing his FQDN as username
-				logger.debug(u"Trying to authenticate by opsiHostKey...")
-				self._username = self._username.lower()
-
-				try:
-					host = self._context.host_getObjects(id=self._username)
-				except AttributeError as aerr:
-					logger.debug(u"{0!r}", aerr)
-					raise BackendUnaccomplishableError(u"Passed backend has no method 'host_getObjects', cannot authenticate host '%s'" % self._username)
-
-				try:
-					self._host = host[0]
-				except IndexError as ierr:
-					logger.debug(u"{0!r}", ierr)
-					raise BackendMissingDataError(u"Host '%s' not found in backend %s" % (self._username, self._context))
-
-				if not self._host.opsiHostKey:
-					raise BackendMissingDataError(u"OpsiHostKey not found for host '%s'" % self._username)
-
-				logger.confidential(u"Client {0!r}, key sent {1!r}, key stored {2!r}", self._username, self._password, self._host.opsiHostKey)
-
-				if self._password != self._host.opsiHostKey:
-					raise BackendAuthenticationError(u"OpsiHostKey authentication failed for host '%s': wrong key" % self._host.id)
-
-				logger.info(u"OpsiHostKey authentication successful for host {0!r}", self._host.id)
-			else:
-				# System user trying to log in with username and password
-				logger.debug(u"Trying to authenticate by operating system...")
-				self._authenticateUser()
-				# Authentication did not throw exception => authentication successful
-				logger.info(u"Operating system authentication successful for user '%s', groups '%s'" % (self._username, ','.join(self._userGroups)))
-		except Exception as e:
-			raise BackendAuthenticationError(forceUnicode(e))
-
+		
 		self._createInstanceMethods()
 		if self._aclFile:
 			self.__loadACLFile()
-		self._authenticated = True
-
+		
 		if not self._acl:
 			self._acl = [[r'.*', [{'type': u'sys_group', 'ids': [OPSI_ADMIN_GROUP], 'denyAttributes': [], 'allowAttributes': []}]]]
 
@@ -154,8 +111,72 @@ class BackendAccessControl:
 		for i, (pattern, acl) in enumerate(self._acl):
 			self._acl[i] = (re.compile(pattern), acl)
 
+		if kwargs.get('username') and kwargs.get('password'):
+			self.authenticate(kwargs['username'], kwargs['password'], kwargs.get('forcegroups'))
+
+	@property
+	def user_store(self):
+		if callable(self._user_store):
+			return self._user_store()
+		return self._user_store
+
+	@user_store.setter
+	def user_store(self, user_store):
+		self._user_store = user_store
+	
+	def authenticate(self, username: str, password: str, forceGroups: List[str] = None):
+		self.user_store.authenticated = False
+		self.user_store.username = username
+		self.user_store.password = password
+		self.user_store.forceGroups = None
+		if forceGroups is not None:
+			self.user_store.forceGroups = forceUnicodeList(forceGroups)
+		
+		if not self.user_store.username:
+			raise BackendAuthenticationError("No username specified")
+		if not self.user_store.password:
+			raise BackendAuthenticationError("No password specified")
+		try:
+			if re.search(r'^[^.]+\.[^.]+\.\S+$', self.user_store.username):
+				# Username starts with something like hostname.domain.tld:
+				# Assuming it is a host passing his FQDN as username
+				logger.debug(u"Trying to authenticate by opsiHostKey...")
+				self.user_store.username = self.user_store.username.lower()
+
+				try:
+					host = self._context.host_getObjects(id=self.user_store.username)
+				except AttributeError as aerr:
+					logger.debug(u"{0!r}", aerr)
+					raise BackendUnaccomplishableError(u"Passed backend has no method 'host_getObjects', cannot authenticate host '%s'" % self.user_store.username)
+
+				try:
+					self.user_store.host = host[0]
+				except IndexError as ierr:
+					logger.debug(u"{0!r}", ierr)
+					raise BackendMissingDataError(u"Host '%s' not found in backend %s" % (self.user_store.username, self._context))
+
+				if not self.user_store.host.opsiHostKey:
+					raise BackendMissingDataError(u"OpsiHostKey not found for host '%s'" % self.user_store.username)
+
+				logger.confidential(u"Client {0!r}, key sent {1!r}, key stored {2!r}", self.user_store.username, self.user_store.password, self.user_store.host.opsiHostKey)
+
+				if self.user_store.password != self.user_store.host.opsiHostKey:
+					raise BackendAuthenticationError(u"OpsiHostKey authentication failed for host '%s': wrong key" % self.user_store.host.id)
+
+				logger.info(u"OpsiHostKey authentication successful for host {0!r}", self.user_store.host.id)
+			else:
+				# System user trying to log in with username and password
+				logger.debug(u"Trying to authenticate by operating system...")
+				self._authenticateUser()
+				# Authentication did not throw exception => authentication successful
+				logger.info(u"Operating system authentication successful for user '%s', groups '%s'" % (self.user_store.username, ','.join(self.user_store.userGroups)))
+		except Exception as e:
+			raise BackendAuthenticationError(forceUnicode(e))
+		
+		self.user_store.authenticated = True
+	
 	def accessControl_authenticated(self):
-		return self._authenticated
+		return self.user_store.authenticated
 
 	def accessControl_userIsAdmin(self):
 		return self._isMemberOfGroup(OPSI_ADMIN_GROUP) or self._isOpsiDepotserver()
@@ -214,52 +235,52 @@ class BackendAccessControl:
 			raise NotImplementedError("Sorry, operating system '%s' not supported yet!" % os.name)
 
 		try:
-			authFunc(self._username, self._password)
+			authFunc(self.user_store.username, self.user_store.password)
 
-			if self._forceGroups is not None:
-				self._userGroups = set(self._forceGroups)
-				logger.info(u"Forced groups for user {!r}: {}", self._username, ', '.join(self._userGroups))
+			if self.user_store.forceGroups is not None:
+				self.user_store.userGroups = set(self.user_store.forceGroups)
+				logger.info(u"Forced groups for user {!r}: {}", self.user_store.username, ', '.join(self.user_store.userGroups))
 			else:
-				self._userGroups = readGroups(self._username)
+				self.user_store.userGroups = readGroups(self.user_store.username)
 		except Exception as error:
-			raise BackendAuthenticationError(u"PAM authentication failed for user '%s': %s" % (self._username, error))
+			raise BackendAuthenticationError(u"PAM authentication failed for user '%s': %s" % (self.user_store.username, error))
 
 	def _isMemberOfGroup(self, ids):
 		for groupId in forceUnicodeList(ids):
-			if groupId in self._userGroups:
+			if groupId in self.user_store.userGroups:
 				return True
 		return False
 
 	def _isUser(self, ids):
-		return forceBool(self._username in forceUnicodeList(ids))
+		return forceBool(self.user_store.username in forceUnicodeList(ids))
 
 	def _isOpsiDepotserver(self, ids=[]):
-		if not self._host or not isinstance(self._host, OpsiDepotserver):
+		if not self.user_store.host or not isinstance(self.user_store.host, OpsiDepotserver):
 			return False
 		if not ids:
 			return True
 
 		for hostId in forceUnicodeList(ids):
-			if hostId == self._host.id:
+			if hostId == self.user_store.host.id:
 				return True
 		return False
 
 	def _isOpsiClient(self, ids=[]):
-		if not self._host or not isinstance(self._host, OpsiClient):
+		if not self.user_store.host or not isinstance(self.user_store.host, OpsiClient):
 			return False
 
 		if not ids:
 			return True
 
-		return forceBool(self._host.id in forceUnicodeList(ids))
+		return forceBool(self.user_store.host.id in forceUnicodeList(ids))
 
 	def _isSelf(self, **params):
 		if not params:
 			return False
 		for (param, value) in params.items():
-			if isinstance(value, types.ClassType) and issubclass(value, Object) and (value.id == self._username):
+			if isinstance(value, types.ClassType) and issubclass(value, Object) and (value.id == self.user_store.username):
 				return True
-			if param in ('id', 'objectId', 'hostId', 'clientId', 'serverId', 'depotId') and (value == self._username):
+			if param in ('id', 'objectId', 'hostId', 'clientId', 'serverId', 'depotId') and (value == self.user_store.username):
 				return True
 		return False
 
@@ -315,19 +336,19 @@ class BackendAccessControl:
 
 		logger.debug("Method {0!r} using acls: {1}", methodName, acls)
 		if granted is True:
-			logger.debug(u"Full access to method {0!r} granted to user {1!r} by acl {2!r}", methodName, self._username, acls[0])
+			logger.debug(u"Full access to method {0!r} granted to user {1!r} by acl {2!r}", methodName, self.user_store.username, acls[0])
 			newKwargs = kwargs
 		elif granted is False:
-			raise BackendPermissionDeniedError(u"Access to method '%s' denied for user '%s'" % (methodName, self._username))
+			raise BackendPermissionDeniedError(u"Access to method '%s' denied for user '%s'" % (methodName, self.user_store.username))
 		else:
-			logger.debug(u"Partial access to method {0!r} granted to user {1!r} by acls {2!r}", methodName, self._username, acls)
+			logger.debug(u"Partial access to method {0!r} granted to user {1!r} by acls {2!r}", methodName, self.user_store.username, acls)
 			try:
 				newKwargs = self._filterParams(kwargs, acls)
 				if not newKwargs:
 					raise BackendPermissionDeniedError(u"No allowed param supplied")
 			except Exception as error:
 				logger.logException(error, LOG_INFO)
-				raise BackendPermissionDeniedError(u"Access to method '%s' denied for user '%s': %s" % (methodName, self._username, error))
+				raise BackendPermissionDeniedError(u"Access to method '%s' denied for user '%s': %s" % (methodName, self.user_store.username, error))
 
 		logger.debug2("newKwargs: {0}", newKwargs)
 
@@ -392,7 +413,7 @@ class BackendAccessControl:
 						except KeyError:
 							pass
 
-					if not objectId or objectId != self._username:
+					if not objectId or objectId != self.user_store.username:
 						continue
 
 				if acl.get('allowAttributes'):
