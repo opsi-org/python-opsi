ec34bd78f925cdbc68941877fdbbda3fbcc7df3f
Jan Schneider <j.schneider@uib.de>
2020-05-25 12:03:25 +0200

    Wrong os

--- OPSI/System/Posix.py
+++ OPSI/System/Posix.py
@@ -921,6 +921,20 @@ output will be returned.
 	exitCode = 0
 	result = []
 
+	sp_env = os.environ.copy()
+	if getattr(sys, 'frozen', False):
+		# Running in pyinstaller / frozen
+		lp_orig = sp_env.get("LD_LIBRARY_PATH_ORIG")
+		if lp_orig is not None:
+			# Restore the original, unmodified value
+			logger.debug("Setting original LD_LIBRARY_PATH '%s' in env for subprocess" % lp_orig)
+			sp_env["LD_LIBRARY_PATH"] = lp_orig
+		else:
+			# This happens when LD_LIBRARY_PATH was not set.
+			# Remove the env var as a last resort
+			logger.debug("Removing LD_LIBRARY_PATH from env for subprocess")
+			sp_env.pop("LD_LIBRARY_PATH", None)
+	
 	startTime = time.time()
 	try:
 		logger.info(u"Executing: %s" % cmd)
