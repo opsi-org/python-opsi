7a4730af40d4521dc5440ce3d269b754db482a6d
Jan Schneider <j.schneider@uib.de>
2020-06-09 00:40:50 +0200

    pytest: ignore manual_tests

--- run_tests.sh
+++ run_tests.sh
@@ -3,4 +3,4 @@ if [ -e coverage.xml ]; then
 	rm coverage.xml
 fi
 
-py.test --junitxml=testreport.xml --cov-config .coveragerc --cov OPSI --cov-report xml -v tests/
+py.test --junitxml=testreport.xml --cov-config .coveragerc --cov OPSI --cov-report xml -v tests/ --ignore=tests/manual_tests
