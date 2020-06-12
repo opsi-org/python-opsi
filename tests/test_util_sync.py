300681684b10bfb699e727fc02e249fa0477bbb9
Jan Schneider <j.schneider@uib.de>
2020-06-12 15:03:00 +0200

    Replace python-librsync and duplicity by librsync

--- tests/test_util_sync.py
+++ tests/test_util_sync.py
@@ -26,15 +26,14 @@ import codecs
 import os
 import os.path
 import shutil
+import random
+import string
 from itertools import combinations_with_replacement
 
 import pytest
 
-try:
-	from OPSI.Util.Sync import librsyncDeltaFile, librsyncSignature, librsyncPatchFile
-	importFailed = False
-except ImportError:
-	importFailed = True
+importFailed = False
+from OPSI.Util.Sync import librsyncDeltaFile, librsyncSignature, librsyncPatchFile
 
 
 @pytest.fixture
@@ -47,47 +46,73 @@ def librsyncTestfile():
 
 @pytest.mark.skipif(importFailed, reason="Import failed.")
 def testLibrsyncSignatureBase64Encoded(librsyncTestfile):
-	assert 'cnMBNgAACAAAAAAI/6410IBmvH1GKbBN\n' == librsyncSignature(librsyncTestfile)
+	assert b'cnMBNgAACAAAAAAI/6410IBmvH1GKbBN\n' == librsyncSignature(librsyncTestfile)
 
 
 @pytest.mark.skipif(importFailed, reason="Import failed.")
 def testLibrsyncSignatureCreation(librsyncTestfile):
 	signature = librsyncSignature(librsyncTestfile, base64Encoded=False)
-	assert 'rs\x016\x00\x00\x08\x00\x00\x00\x00\x08\xff\xae5\xd0\x80f\xbc}F)\xb0M' == signature
+	assert b'rs\x016\x00\x00\x08\x00\x00\x00\x00\x08\xff\xae5\xd0\x80f\xbc}F)\xb0M' == signature
 
 
 @pytest.mark.skipif(importFailed, reason="Import failed.")
 def testLibrsyncDeltaFileCreation(librsyncTestfile, tempDir):
-	signature = librsyncSignature(librsyncTestfile, base64Encoded=False)
 	deltafile = os.path.join(tempDir, 'delta')
-
+	oldfile = os.path.join(tempDir, 'old')
+	with open(oldfile, "wb") as f:
+		f.write(b"olddata")
+	signature = librsyncSignature(oldfile, base64Encoded=False)
+	
 	librsyncDeltaFile(librsyncTestfile, signature.strip(), deltafile)
 	assert os.path.exists(deltafile), "No delta file was created"
 
-	expectedDelta = 'rs\x026F\x00\x04\x8a\x00'
-	with open(deltafile, "r") as f:
+	expectedDelta = b"rs\x026B\x04\x8a"
+	with open(librsyncTestfile, "rb") as f:
+		expectedDelta += f.read()
+	expectedDelta += b"\x00"
+	
+	with open(deltafile, "rb") as f:
 		assert expectedDelta == f.read()
 
+@pytest.mark.skipif(importFailed, reason="Import failed.")
+def testLibrsyncDeltaSize(librsyncTestfile, tempDir):
+	baseFile = os.path.join(tempDir, 'base')
+	oldfile = os.path.join(tempDir, 'old')
+	deltaFile = os.path.join(tempDir, 'base.delta')
+	size = 1 * 1024 * 1024 # 1MiB
+
+	data = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(size))
+	with open(baseFile, "w") as f:
+		f.write(data)
+	with open(oldfile, "w") as f:
+		f.write(data[:int(size/2)])
+	
+	signature = librsyncSignature(oldfile, False)
+	librsyncDeltaFile(baseFile, signature, deltaFile)
+	delta_size = os.path.getsize(deltaFile)
+	assert delta_size == 524398
 
 @pytest.mark.skipif(importFailed, reason="Import failed.")
 def testLibrsyncPatchFileDoesNotAlterIfUnneeded(librsyncTestfile, tempDir):
 	baseFile = librsyncTestfile
-	signature = librsyncSignature(baseFile, False)
-
+	oldfile = os.path.join(tempDir, 'old')
 	deltaFile = os.path.join(tempDir, 'base.delta')
+
+	shutil.copy(baseFile, oldfile)
+	signature = librsyncSignature(oldfile, False)
 	librsyncDeltaFile(baseFile, signature, deltaFile)
 
 	assert os.path.exists(deltaFile)
-	expectedDelta = "rs\x026F\x00\x04\x8a\x00"
+	expectedDelta = b"rs\x026F\x00\x04\x8a\x00"
 	with open(deltaFile, "rb") as f:
 		assert expectedDelta == f.read()
+	
+	newfile = os.path.join(tempDir, 'newFile.txt')
+	librsyncPatchFile(oldfile, deltaFile, newfile)
+	assert os.path.exists(newfile)
 
-	newFile = os.path.join(tempDir, 'newFile.txt')
-	librsyncPatchFile(baseFile, deltaFile, newFile)
-	assert os.path.exists(newFile)
-
-	with open(newFile, "r") as newF:
-		with open(baseFile, "r") as baseF:
+	with open(newfile, "rb") as newF:
+		with open(baseFile, "rb") as baseF:
 			assert baseF.readlines() == newF.readlines()
 
 
@@ -107,29 +132,29 @@ def testLibrsyncPatchFileCreatesNewFileBasedOnDelta(librsyncTestfile, tempDir):
 	deltaFileForNewFile = os.path.join(tempDir, 'newDelta.delta')
 	librsyncDeltaFile(newFile, signature, deltaFileForNewFile)
 	expectedDelta = (
-		'rs\x026B\x04\xb8Die NASA konnte wieder ein Funksignal der '
-		'Sonde New Horizons empfangen. Damit scheint sicher, dass '
-		'das Man\xc3\xb6ver ein Erfolg war und nun jede Menge Daten '
-		'zu erwarten sind. Bis die alle auf der Erde sind, wird es '
-		'aber dauern.\n\nDie NASA feiert eine "historische Nacht": '
-		'Die Sonde New Horizons ist am Zwergplaneten Pluto '
-		'vorbeigeflogen und hat kurz vor drei Uhr MESZ wieder Kontakt '
-		'mit der Erde aufgenommen. Jubel, rotwei\xc3\x9fblaue '
-		'F\xc3\xa4hnchen und stehende Ovationen pr\xc3\xa4gten die '
-		'Stimmung im John Hopkins Labor in Maryland. Digital stellten '
-		'sich prominente Gratulanten ein, von Stephen Hawking mit '
-		'einer Videobotschaft bis zu US-Pr\xc3\xa4sident Barack Obama '
-		'per Twitter.\n\n"Hallo Welt"\n\nDas erste Funksignal New '
-		'Horizons nach dem Vorbeiflug am Pluto brachte noch keine '
-		'wissenschaftlichen Ergebnisse oder neue Fotos, sondern '
-		'Telemetriedaten der Sonde selbst. Das war so geplant. '
-		'Aus diesen Informationen geht hervor, dass es New Horizons '
-		'gut geht, dass sie ihren Kurs h\xc3\xa4lt und die '
-		'vorausberechnete Menge an Speichersektoren belegt ist. '
-		'Daraus schlie\xc3\x9fen die Verantwortlichen der NASA, dass '
-		'auch tats\xc3\xa4chlich wissenschaftliche Informationen im '
-		'geplanten Ausma\xc3\x9f gesammelt wurden.\n\nUnd diese Zeile '
-		'hier macht den Unterschied.\n\x00')
+		b'rs\x026B\x04\xb8Die NASA konnte wieder ein Funksignal der '
+		b'Sonde New Horizons empfangen. Damit scheint sicher, dass '
+		b'das Man\xc3\xb6ver ein Erfolg war und nun jede Menge Daten '
+		b'zu erwarten sind. Bis die alle auf der Erde sind, wird es '
+		b'aber dauern.\n\nDie NASA feiert eine "historische Nacht": '
+		b'Die Sonde New Horizons ist am Zwergplaneten Pluto '
+		b'vorbeigeflogen und hat kurz vor drei Uhr MESZ wieder Kontakt '
+		b'mit der Erde aufgenommen. Jubel, rotwei\xc3\x9fblaue '
+		b'F\xc3\xa4hnchen und stehende Ovationen pr\xc3\xa4gten die '
+		b'Stimmung im John Hopkins Labor in Maryland. Digital stellten '
+		b'sich prominente Gratulanten ein, von Stephen Hawking mit '
+		b'einer Videobotschaft bis zu US-Pr\xc3\xa4sident Barack Obama '
+		b'per Twitter.\n\n"Hallo Welt"\n\nDas erste Funksignal New '
+		b'Horizons nach dem Vorbeiflug am Pluto brachte noch keine '
+		b'wissenschaftlichen Ergebnisse oder neue Fotos, sondern '
+		b'Telemetriedaten der Sonde selbst. Das war so geplant. '
+		b'Aus diesen Informationen geht hervor, dass es New Horizons '
+		b'gut geht, dass sie ihren Kurs h\xc3\xa4lt und die '
+		b'vorausberechnete Menge an Speichersektoren belegt ist. '
+		b'Daraus schlie\xc3\x9fen die Verantwortlichen der NASA, dass '
+		b'auch tats\xc3\xa4chlich wissenschaftliche Informationen im '
+		b'geplanten Ausma\xc3\x9f gesammelt wurden.\n\nUnd diese Zeile '
+		b'hier macht den Unterschied.\n\x00')
 
 	with open(deltaFileForNewFile, "rb") as f:
 		assert expectedDelta == f.read()
