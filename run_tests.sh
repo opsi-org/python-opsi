3716c37979158d24b7c96835e93dcf4db9440014
Jan Schneider <j.schneider@uib.de>
2020-06-13 17:18:34 +0200

    Work on test reporting

--- run_tests.sh
+++ run_tests.sh
@@ -3,4 +3,4 @@ if [ -e coverage.xml ]; then
 	rm coverage.xml
 fi
 
-py.test --junitxml=testreport.xml --cov-config .coveragerc --cov OPSI --cov-report xml -v tests/ --ignore=tests/manual_tests
+py.test --junitxml=testreport.xml --cov-config .coveragerc --cov OPSI --cov-report term --cov-report xml -v tests/ --ignore=tests/manual_tests
