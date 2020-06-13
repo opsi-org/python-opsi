05ac3d3ca7d42c79f3aa5d831ab1f7dc75e4acaf
Jan Schneider <j.schneider@uib.de>
2020-06-13 20:48:00 +0200

    Set junit_family to new default in pytest 6.0

--- run_tests.sh
+++ run_tests.sh
@@ -3,4 +3,4 @@ if [ -e coverage.xml ]; then
 	rm coverage.xml
 fi
 
-py.test --junitxml=testreport.xml --cov-config .coveragerc --cov OPSI --cov-report term --cov-report xml -v tests/ --ignore=tests/manual_tests
+py.test --junit_family=xunit2 --junitxml=testreport.xml --cov-config .coveragerc --cov OPSI --cov-report term --cov-report xml -v tests/ --ignore=tests/manual_tests
