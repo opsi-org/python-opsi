ed2143d46e8d6e6d6f4569e7ab97efde606f91b6
Jan Schneider <j.schneider@uib.de>
2020-06-24 17:26:43 +0200

    print exitcode

--- run_tests.sh
+++ run_tests.sh
@@ -4,4 +4,8 @@ if [ -e coverage.xml ]; then
 fi
 
 py.test -o junit_family=xunit2 --junitxml=testreport-OPSI.xml --cov-append --cov OPSI --cov-report term --cov-report xml -v tests/testOPSI
+echo "exitcode $?"
+
 py.test -o junit_family=xunit2 --junitxml=testreport-opsicommon.xml --cov-append --cov opsicommon --cov-report term --cov-report xml -v tests/testopsicommon
+echo "exitcode $?"
+
