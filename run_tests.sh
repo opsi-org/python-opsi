1bcf9eed6830c19393a4c43f2376a81d76a780e1
Jan Schneider <j.schneider@uib.de>
2020-06-24 16:38:07 +0200

    Append to coverage.xml, remove empty config file

--- run_tests.sh
+++ run_tests.sh
@@ -3,5 +3,5 @@ if [ -e coverage.xml ]; then
 	rm coverage.xml
 fi
 
-py.test -o junit_family=xunit2 --junitxml=testreport-OPSI.xml --cov-config .coveragerc --cov OPSI --cov-report term --cov-report xml -v tests/testOPSI
-py.test -o junit_family=xunit2 --junitxml=testreport-opsicommon.xml --cov-config .coveragerc --cov opsicommon --cov-report term --cov-report xml -v tests/testopsicommon
+py.test -o junit_family=xunit2 --junitxml=testreport-OPSI.xml --cov-append --cov OPSI --cov-report term --cov-report xml -v tests/testOPSI
+py.test -o junit_family=xunit2 --junitxml=testreport-opsicommon.xml --cov-append --cov opsicommon --cov-report term --cov-report xml -v tests/testopsicommon
