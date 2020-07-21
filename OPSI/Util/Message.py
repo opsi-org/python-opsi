e1f83b788449fcd4be46cf5e1e68479faa74caee
Jan Schneider <j.schneider@uib.de>
2020-07-21 12:11:27 +0200

    [chg] return listening status

--- OPSI/Util/Message.py
+++ OPSI/Util/Message.py
@@ -678,6 +678,7 @@ class NotificationServer(threading.Thread, SubjectsObserver):
 	def start_and_wait(timeout=30):
 		self.start()
 		self._running_event.wait(timeout)
+		return self._listening
 	
 	def _stopListeningCompleted(self, result):
 		self._listening = False
