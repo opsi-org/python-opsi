f717cbebff5b5d997a1567011cdf6adf897e361a
Fabian Kalweit <f.kalweit@uib.de>
2020-11-03 16:30:43 +0100

    [chg] dont catch exceptions in intsall_ca

--- opsicommon/ssl/linux.py
+++ opsicommon/ssl/linux.py
@@ -18,30 +18,24 @@ import os
 __all__ = ["install_ca"]
 
 def install_ca(ca_file):
-	
-	try:
-		if isCentOS() or isRHEL():
-			# /usr/share/pki/ca-trust-source/anchors/
-			system_cert_path = "/etc/pki/ca-trust/source/anchors"
-			cmd = "update-ca-trust"
-		elif isDebian() or isUbuntu():
-			system_cert_path = "/usr/local/share/ca-certificates"
-			cmd = "update-ca-certificates"
-		elif isOpenSUSE() or isSLES():
-			system_cert_path = "/usr/share/pki/trust/anchors"
-			cmd = "update-ca-certificates"
-		else:
-			logger.error("Failed to set system cert path!")
-			raise 
 
-		with open(ca_file, "r") as file:
-			ca_file_content = file.read()
-		ca = crypto.load_certificate(crypto.FILETYPE_PEM, ca_file_content)
-		
-		cert_file = f"{ca.get_subject().commonName.replace(' ', '_')}.crt"
-		copyfile(ca_file, os.path.join(system_cert_path, cert_file))
-		output = execute(cmd)
+	if isCentOS() or isRHEL():
+		# /usr/share/pki/ca-trust-source/anchors/
+		system_cert_path = "/etc/pki/ca-trust/source/anchors"
+		cmd = "update-ca-trust"
+	elif isDebian() or isUbuntu():
+		system_cert_path = "/usr/local/share/ca-certificates"
+		cmd = "update-ca-certificates"
+	elif isOpenSUSE() or isSLES():
+		system_cert_path = "/usr/share/pki/trust/anchors"
+		cmd = "update-ca-certificates"
+	else:
+		logger.error("Failed to set system cert path!")
+		raise 
 
-	except Exception as e:
-		logger.error("ERROR: %s", e)
+	with open(ca_file, "r") as file:
+		ca = crypto.load_certificate(crypto.FILETYPE_PEM,  file.read())
 	
+	cert_file = f"{ca.get_subject().commonName.replace(' ', '_')}.crt"
+	copyfile(ca_file, os.path.join(system_cert_path, cert_file))
+	output = execute(cmd)
