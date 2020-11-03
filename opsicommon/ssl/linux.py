5372f8316ce85e672e54854ef02eab86754195bb
Fabian Kalweit <f.kalweit@uib.de>
2020-11-03 15:50:48 +0100

    [chg] rm devel output

--- opsicommon/ssl/linux.py
+++ opsicommon/ssl/linux.py
@@ -23,16 +23,13 @@ def install_ca(ca_file):
 	
 	try:
 		if isCentOS() or isRHEL():
-			logger.devel("CENTOS/RHEL")
 			# /usr/share/pki/ca-trust-source/anchors/
 			system_cert_path = "/etc/pki/ca-trust/source/anchors"
 			cmd = "update-ca-trust"
 		elif isDebian() or isUbuntu():
-			logger.devel("DEBIAN/UBUNTU")
 			system_cert_path = "/usr/local/share/ca-certificates"
 			cmd = "update-ca-certificates"
 		elif isOpenSUSE() or isSLES():
-			logger.devel("SUSE")
 			system_cert_path = "/usr/share/pki/trust/anchors"
 			cmd = "update-ca-certificates"
 		else:
@@ -44,10 +41,8 @@ def install_ca(ca_file):
 		ca = crypto.load_certificate(crypto.FILETYPE_PEM, ca_file_content)
 		
 		cert_file = f"{ca.get_subject().commonName.replace(' ', '_')}.crt"
-		logger.devel(cert_file)
 		copyfile(ca_file, os.path.join(system_cert_path, cert_file))
 		output = execute(cmd)
-		logger.devel(output)
 
 	except Exception as e:
 		logger.error("ERROR: %s", e)
