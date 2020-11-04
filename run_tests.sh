cad8867ef835159a08045514a0385e6035dd66e3
Fabian Kalweit <f.kalweit@uib.de>
2020-11-04 15:33:11 +0100

    [chg] gitlab run ssl test

--- run_tests.sh
+++ run_tests.sh
@@ -3,9 +3,9 @@ if [ -e coverage.xml ]; then
 	rm coverage.xml
 fi
 
-py.test -o junit_family=xunit2 --junitxml=testreport-OPSI.xml --cov-append --cov OPSI --cov-report term --cov-report xml -v tests/testOPSI
-echo "exitcode $?"
+# py.test -o junit_family=xunit2 --junitxml=testreport-OPSI.xml --cov-append --cov OPSI --cov-report term --cov-report xml -v tests/testOPSI
+# echo "exitcode $?"
 
-py.test -o junit_family=xunit2 --junitxml=testreport-opsicommon.xml --cov-append --cov opsicommon --cov-report term --cov-report xml -v tests/testopsicommon
+py.test -o junit_family=xunit2 --junitxml=testreport-opsicommon.xml --cov-append --cov opsicommon --cov-report term --cov-report xml -v tests/testopsicommon/test_ssl.py
 echo "exitcode $?"
 
