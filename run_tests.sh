7dfa06964667db7aadf84ab6cce836ffa70415c7
Jan Schneider <j.schneider@uib.de>
2021-03-29 15:38:11 +0200

    [chg] Set pytest log lvel to warning

--- run_tests.sh
+++ run_tests.sh
@@ -3,5 +3,5 @@ if [ -e coverage.xml ]; then
 	rm coverage.xml
 fi
 
-py.test -o junit_family=xunit2 --junitxml=testreport-OPSI.xml --cov-append --cov OPSI --cov-report term --cov-report xml -v tests/testOPSI
-py.test -o junit_family=xunit2 --junitxml=testreport-opsicommon.xml --cov-append --cov opsicommon --cov-report term --cov-report xml -v tests/testopsicommon
+py.test --log-level=WARNING -o junit_family=xunit2 --junitxml=testreport-OPSI.xml --cov-append --cov OPSI --cov-report term --cov-report xml -v tests/testOPSI
+py.test --log-level=WARNING -o junit_family=xunit2 --junitxml=testreport-opsicommon.xml --cov-append --cov opsicommon --cov-report term --cov-report xml -v tests/testopsicommon
