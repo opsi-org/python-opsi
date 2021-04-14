4b3976f7d8b91346a3f875cc979349f8a5afafe1
doerrer <n.doerrer@uib.de>
2021-04-14 09:23:50 +0200

    [chg] skip all tests except logging_tests for debugging

--- run_tests.sh
+++ run_tests.sh
@@ -2,5 +2,6 @@
 [ -e coverage.xml ] && rm coverage.xml
 set -e
 
-py.test --log-level=WARNING -o junit_family=xunit2 --junitxml=testreport-OPSI.xml --cov-append --cov OPSI --cov-report term --cov-report xml -v tests/testOPSI
-py.test --log-level=WARNING -o junit_family=xunit2 --junitxml=testreport-opsicommon.xml --cov-append --cov opsicommon --cov-report term --cov-report xml -v tests/testopsicommon
+#py.test --log-level=WARNING -o junit_family=xunit2 --junitxml=testreport-OPSI.xml --cov-append --cov OPSI --cov-report term --cov-report xml -v tests/testOPSI
+#py.test --log-level=WARNING -o junit_family=xunit2 --junitxml=testreport-opsicommon.xml --cov-append --cov opsicommon --cov-report term --cov-report xml -v tests/testopsicommon
+py.test --log-level=WARNING -o junit_family=xunit2 --junitxml=testreport-opsicommon.xml --cov-append --cov opsicommon --cov-report term --cov-report xml -v tests/testopsicommon/test_logging.py
