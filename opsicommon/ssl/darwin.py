e70e9b8be9274054259f6cc09e17b0ac045fabb0
Jan Schneider <j.schneider@uib.de>
2021-06-28 14:58:51 +0200

    [new] Implement install_ca and remove_ca for macOS

--- opsicommon/ssl/darwin.py
+++ opsicommon/ssl/darwin.py
@@ -6,14 +6,41 @@
 This file is part of opsi - https://www.opsi.org
 """
 
+import os
+import tempfile
+
 from OpenSSL import crypto
+from OPSI.System import execute
+
+from opsicommon.logging import logger
 
 __all__ = ["install_ca", "remove_ca"]
 
 
 def install_ca(ca_cert: crypto.X509):
-	raise NotImplementedError("Not implemented on macOS")
+	logger.info("Installing CA '%s' into system store", ca_cert.get_subject().CN)
+
+	pem_file = tempfile.NamedTemporaryFile(mode="wb", delete=False, encoding="ascii")
+	pem_file.write(crypto.dump_certificate(crypto.FILETYPE_PEM, ca_cert))
+	pem_file.close()
+	try:
+		execute(f'security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain "{pem_file.name}"')
+	finally:
+		os.remove(pem_file.name)
 
 
 def remove_ca(subject_name: str) -> bool:
-	raise NotImplementedError("Not implemented on macOS")
+
+	pem = execute(f'security find-certificate -p -c "{subject_name}" /Library/Keychains/System.keychain').strip()
+	if not pem:
+		logger.info("CA '%s' not found, nothing to remove", subject_name)
+		return
+
+	logger.info("Removing CA '%s'", subject_name)
+	pem_file = tempfile.NamedTemporaryFile(mode="wb", delete=False, encoding="ascii")
+	pem_file.write(crypto.dump_certificate(crypto.FILETYPE_PEM, pem))
+	pem_file.close()
+	try:
+		execute(f'security remove-trusted-cert -d "{pem_file.name}"')
+	finally:
+		os.remove(pem_file.name)
