6a4ac448f33569202e9b4c2deff8f5fa09305463
Jan Schneider <j.schneider@uib.de>
2021-07-05 13:52:13 +0200

    [chg] ignore test_logging.py

--- run_tests.sh
+++ run_tests.sh
@@ -2,5 +2,12 @@
 [ -e coverage.xml ] && rm coverage.xml
 set -e
 
-py.test --log-level=WARNING -o junit_family=xunit2 --junitxml=testreport-OPSI.xml --cov-append --cov OPSI --cov-report term --cov-report xml -v tests/testOPSI
-py.test --log-level=WARNING -o junit_family=xunit2 --junitxml=testreport-opsicommon.xml --cov-append --cov opsicommon --cov-report term --cov-report xml -v tests/testopsicommon
+py.test -v --log-level=WARNING \
+	-o junit_family=xunit2 --junitxml=testreport-OPSI.xml \
+	--cov-append --cov OPSI --cov-report term --cov-report xml \
+	tests/testOPSI
+py.test -v --log-level=WARNING \
+	-o junit_family=xunit2 --junitxml=testreport-opsicommon.xml \
+	--cov-append --cov opsicommon --cov-report term --cov-report xml\
+	--ignore tests/testopsicommon/test_logging.py \
+	tests/testopsicommon
