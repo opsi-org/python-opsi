8fbdcda177cdce74cfd9c2315fa2a06690e6869d
doerrer <n.doerrer@uib.de>
2021-10-11 15:36:34 +0200

    [chg] removed some more legacy logger calls

--- OPSI/Util/Task/UpdatePackages/Config.py
+++ OPSI/Util/Task/UpdatePackages/Config.py
@@ -20,7 +20,7 @@ from OPSI.Types import (
 	forceHostId, forceInt, forceProductId, forceUnicode, forceUrl
 )
 
-from opsicommon.logging import logger, secret_filter
+from opsicommon.logging import logger, secret_filter, logging_config
 
 from .Exceptions import (
 	ConfigurationError, MissingConfigurationValueError, RequiringBackendError
@@ -125,9 +125,9 @@ overriden based on values in configuration file.
 							config["packageDir"] = forceFilename(value.strip())
 						elif option.lower() == 'logfile':
 							value = forceFilename(value.strip())
-							logger.setLogFile(value)
+							logging_config(log_file=value)
 						elif option.lower() == 'loglevel':
-							logger.setFileLevel(forceInt(value.strip()))
+							logging_config(file_level=forceInt(value.strip()))
 						elif option.lower() == 'timeout':
 							# TODO: find a better way!
 							socket.setdefaulttimeout(float(value.strip()))
