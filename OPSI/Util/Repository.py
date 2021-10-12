e60d19bf83e29b0ae2b5a1ed87fa1f1f26ce0096
doerrer <n.doerrer@uib.de>
2021-10-12 14:52:17 +0200

    [new] added default no_proxy entries for localhost:4447 and 127.0.0.1:4447

--- OPSI/Util/Repository.py
+++ OPSI/Util/Repository.py
@@ -982,7 +982,7 @@ class HTTPRepository(Repository):  # pylint: disable=too-many-instance-attribute
 					if key in os.environ:
 						del os.environ[key]
 			no_proxy = [x.strip() for x in os.environ.get("no_proxy", "").split(",") if x.strip()]
-			no_proxy.extend(["localhost", "127.0.0.1", "ip6-localhost", "::1"])
+			no_proxy.extend(["localhost", "localhost:4447", "127.0.0.1", "127.0.0.1:4447", "ip6-localhost", "::1"])
 			os.environ["no_proxy"] = ",".join(set(no_proxy))
 		else:
 			# Do not use a proxy
