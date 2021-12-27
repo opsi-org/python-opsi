4a7381dffe2fad5e4f83946e3a5f8b29dd6bf9de
Jan Schneider <j.schneider@uib.de>
2021-12-27 15:32:59 +0100

    [chg] Cleanup tests

--- run_tests.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-[ -e coverage.xml ] && rm coverage.xml
-set -e
-
-py.test -v --log-level=warning \
-	-o junit_family=xunit2 --junitxml=testreport-OPSI.xml \
-	--cov-append --cov OPSI --cov-report term --cov-report xml \
-	tests
