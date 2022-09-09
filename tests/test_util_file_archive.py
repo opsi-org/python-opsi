499c02b6d8b82d66bda870feeff20eb5f67e8edc
Jan Schneider <j.schneider@uib.de>
2022-09-09 08:45:07 +0200

    [fix] Fix test

--- tests/test_util_file_archive.py
+++ tests/test_util_file_archive.py
@@ -10,19 +10,19 @@ import os
 
 import pytest
 
-from OPSI.Util.File.Archive import getFileType, Archive, PigzMixin, TarArchive
+from OPSI.Util.File.Archive import Archive, TarArchive, getFileType, is_pigz_available
 
 from .helpers import mock
 
 
 def testArchiveFactoryRaisesExceptionOnUnknownFormat():
 	with pytest.raises(Exception):
-		Archive('no_filename', format='unknown')
+		Archive("no_filename", format="unknown")
 
 
 @pytest.mark.parametrize("fileFormat", ["tar", "cpio"])
 def testCreatingArchive(fileFormat):
-	Archive('no_file', format=fileFormat)
+	Archive("no_file", format=fileFormat)
 
 
 def testRaisingExceptionIfFiletypeCanNotBeDetermined():
@@ -30,31 +30,9 @@ def testRaisingExceptionIfFiletypeCanNotBeDetermined():
 		Archive(__file__)
 
 
-def testPigzDetectionOnTarArchive():
-	assert PigzMixin.is_pigz_available() == TarArchive.is_pigz_available()
-
-
-@pytest.fixture
-def dumbArchive():
-	class DumbArchive(PigzMixin):
-		pass
-
-	yield DumbArchive()
-
-
-def testPigzMixinProvidesMethods(dumbArchive):
-	assert hasattr(dumbArchive, 'pigz_detected')
-	assert hasattr(dumbArchive, 'is_pigz_available')
-
-
-def testPigzMixinMethods(dumbArchive):
-	assert PigzMixin.is_pigz_available() == dumbArchive.pigz_detected
-	assert PigzMixin.is_pigz_available() == dumbArchive.is_pigz_available()
-
-
 def testDisablingPigz(dumbArchive):
 	"""
 	Disabling the usage of pigz by setting PIGZ_ENABLED to False.
 	"""
-	with mock.patch('OPSI.Util.File.Archive.PIGZ_ENABLED', False):
-		assert dumbArchive.is_pigz_available() is False
+	with mock.patch("OPSI.Util.File.Archive.PIGZ_ENABLED", False):
+		assert is_pigz_available() is False
