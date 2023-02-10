2d6641e38cec5af894be4636051d50f312de1147
Jan Schneider <j.schneider@uib.de>
2023-02-10 13:11:58 +0000

    [fix] pylint

--- OPSI/Util/Task/Rights.py
+++ OPSI/Util/Task/Rights.py
@@ -25,7 +25,7 @@ from OPSI.Config import (
 	OPSI_ADMIN_GROUP,
 	OPSICONFD_USER,
 )
-from OPSI.System.Posix import getLocalFqdn, isCentOS, isOpenSUSE, isRHEL, isSLES, isUCS
+from OPSI.System.Posix import getLocalFqdn, isOpenSUSE, isSLES
 from opsicommon.logging import get_logger
 from opsicommon.utils import Singleton
 
