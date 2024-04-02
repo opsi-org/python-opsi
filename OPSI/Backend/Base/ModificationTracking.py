35cd709beaedc5713d752bce719157f389625230
Jan Schneider <j.schneider@uib.de>
2024-04-02 17:52:11 +0200

    [fix] Mark methods with @no_export in ModificationTrackingBackend

--- OPSI/Backend/Base/ModificationTracking.py
+++ OPSI/Backend/Base/ModificationTracking.py
@@ -8,28 +8,29 @@ Backend that tracks modifications.
 
 from opsicommon.logging import get_logger
 
+from OPSI.Backend import no_export
+
 from .Extended import ExtendedBackend
 
-__all__ = (
-	'ModificationTrackingBackend', 'BackendModificationListener'
-)
+__all__ = ("ModificationTrackingBackend", "BackendModificationListener")
 
 
 logger = get_logger("opsi.general")
 
 
 class ModificationTrackingBackend(ExtendedBackend):
-
 	def __init__(self, backend, overwrite=True):
 		ExtendedBackend.__init__(self, backend, overwrite=overwrite)
 		self._createInstanceMethods()
 		self._backendChangeListeners = []
 
+	@no_export
 	def addBackendChangeListener(self, backendChangeListener):
 		if backendChangeListener in self._backendChangeListeners:
 			return
 		self._backendChangeListeners.append(backendChangeListener)
 
+	@no_export
 	def removeBackendChangeListener(self, backendChangeListener):
 		if backendChangeListener not in self._backendChangeListeners:
 			return
@@ -44,25 +45,22 @@ class ModificationTrackingBackend(ExtendedBackend):
 				logger.error(err)
 
 	def _executeMethod(self, methodName, **kwargs):
-		logger.debug(
-			"ModificationTrackingBackend %s: executing %s on backend %s",
-			self, methodName, self._backend
-		)
+		logger.debug("ModificationTrackingBackend %s: executing %s on backend %s", self, methodName, self._backend)
 		meth = getattr(self._backend, methodName)
 		result = meth(**kwargs)
 		action = None
-		if '_' in methodName:
-			action = methodName.split('_', 1)[1]
+		if "_" in methodName:
+			action = methodName.split("_", 1)[1]
 
-		if action in ('insertObject', 'updateObject', 'deleteObjects'):
+		if action in ("insertObject", "updateObject", "deleteObjects"):
 			value = list(kwargs.values())[0]
-			if action == 'insertObject':
-				self._fireEvent('objectInserted', value)
-			elif action == 'updateObject':
-				self._fireEvent('objectUpdated', value)
-			elif action == 'deleteObjects':
-				self._fireEvent('objectsDeleted', value)
-			self._fireEvent('backendModified')
+			if action == "insertObject":
+				self._fireEvent("objectInserted", value)
+			elif action == "updateObject":
+				self._fireEvent("objectUpdated", value)
+			elif action == "deleteObjects":
+				self._fireEvent("objectsDeleted", value)
+			self._fireEvent("backendModified")
 
 		return result
 
