c2bb6f6c353cbf0bcb3db3a789a690cd9bca49d0
Jan Schneider <j.schneider@uib.de>
2026-03-16 14:28:17 +0100

    [fix] Fix import

--- opsi/opsiservice/server/__init__.py
+++ opsi/opsiservice/server/__init__.py
@@ -1,3 +1,8 @@
+# This file is part of the device management solution OPSI http://www.opsi.org
+# Copyright (c) 2026-2026 uib GmbH <info@uib.de>
+# This code is owned by the uib GmbH, Mainz, Germany (uib.de). All rights reserved.
+# License: AGPL-3.0-only
+
 from ._common import get_opsiconfd_config
 
 __all__ = ["get_opsiconfd_config"]
--- opsi/system/time/__init__.py
+++ opsi/system/time/__init__.py
@@ -1,3 +1,8 @@
+# This file is part of the device management solution OPSI http://www.opsi.org
+# Copyright (c) 2026-2026 uib GmbH <info@uib.de>
+# This code is owned by the uib GmbH, Mainz, Germany (uib.de). All rights reserved.
+# License: AGPL-3.0-only
+
 from opsi.exception import OperatingSystemUnsupportedError
 from opsi.system.info import get_system, is_linux, is_macos, is_windows
 
