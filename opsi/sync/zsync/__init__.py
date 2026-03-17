c4c241b01eb0a8f16d1af3f349fcf4ba1ec98713
Jan Schneider <j.schneider@uib.de>
2026-03-17 13:15:44 +0100

    [fix] Correct header

--- opsi/sync/zsync/__init__.py
+++ opsi/sync/zsync/__init__.py
@@ -1,5 +1,5 @@
 # This file is part of the device management solution OPSI http://www.opsi.org
-# Copyright (c) 2026-2026 uib GmbH <info@uib.de>
+# Copyright (c) 2020-2026 uib GmbH <info@uib.de>
 # This code is owned by the uib GmbH, Mainz, Germany (uib.de). All rights reserved.
 # License: AGPL-3.0-only
 
