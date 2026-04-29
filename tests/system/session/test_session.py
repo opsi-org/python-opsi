b67e8ff15dd37724df70897b39d971ea7110cda3
doerrer <n.doerrer@uib.de>
2026-04-29 09:29:16 +0200

    [fix] fixed sessions test for linux

--- tests/system/session/test_session.py
+++ tests/system/session/test_session.py
@@ -1,3 +1,4 @@
+from opsi.system.info import is_linux
 from opsi.system.session import get_sessions
 
 
@@ -5,6 +6,8 @@ def test_get_sessions() -> None:
 	sessions = get_sessions()
 	assert isinstance(sessions, list)
 	print(sessions)
+	if is_linux():
+		return  # Might run in a headless environment, so we can't reliably test for sessions
 	assert sessions
 	assert sessions[0].id
 	assert sessions[0].desktop
