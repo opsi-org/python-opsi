03b0ee06ec0e9dc3e010d7494db7b92d55b85e9a
doerrer <n.doerrer@uib.de>
2026-04-30 16:07:03 +0200

    [chg] changed structure to be in line with the rest of python-opsi

--- opsi/system/session/__init__.py
+++ opsi/system/session/__init__.py
@@ -1,18 +1,13 @@
-import os
-from opsi.system.info import is_linux, is_macos, is_windows
-from ._common import DisplaySession
+from opsi.exception import OperatingSystemUnsupportedError
+from opsi.system.info import is_linux, is_macos, is_windows, get_system
+
+if is_linux():
+	from ._linux import get_display_sessions
+elif is_macos():
+	from ._macos import get_display_sessions
+elif is_windows():
+	from ._windows import get_display_sessions
+else:
+	raise OperatingSystemUnsupportedError(f"{get_system()} not supported")
 
 __all__ = ["get_display_sessions"]
-
-
-def get_display_sessions(*, protocol: str | None = None, user: str | None = None) -> list[DisplaySession]:
-	if is_windows():
-		from ._windows import get_display_sessions as _get_display_sessions
-	elif is_linux():
-		from ._linux import get_display_sessions as _get_display_sessions
-	elif is_macos():
-		return [DisplaySession(id=1, desktop="default", user=os.getenv("USER") or "")]
-	else:
-		raise RuntimeError("Unsupported operating system")
-
-	return _get_display_sessions(protocol=protocol, user=user)
