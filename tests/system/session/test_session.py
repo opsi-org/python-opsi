54a5696ab2735390db44aa01e07f493e06f8a5f9
Jan Schneider <j.schneider@uib.de>
2026-05-04 17:01:29 +0200

    [chg] Add test

--- tests/system/session/test_session.py
+++ tests/system/session/test_session.py
@@ -6,7 +6,7 @@
 import pytest
 
 from opsi.system.info import is_linux, is_windows
-from opsi.system.session import get_display_sessions
+from opsi.system.session import get_console_session, get_display_sessions
 
 
 @pytest.mark.parametrize("one_session_per_user", [True, False])
@@ -30,3 +30,18 @@ def test_get_display_sessions(one_session_per_user: bool) -> None:
 		if one_session_per_user:
 			assert session.user not in users
 		users.add(session.user)
+
+
+def test_get_console_session() -> None:
+	session = get_console_session()
+	if not session and is_linux():
+		pytest.skip("No console session found, might be running in a headless Linux environment")
+
+	assert session
+	assert session.id
+	assert session.desktop
+	assert session.user
+
+	if is_windows():
+		assert session.windows_state is not None
+		assert session.windows_protocol is not None
